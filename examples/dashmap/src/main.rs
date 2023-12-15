80fbb71866980d21cd514e868322487f8e98960e
S John CD <john-cd@users.noreply.github.com>
2023-12-15 01:24:04 +0000

    feat: add dashmap example and concur. data structure page

--- examples/dashmap/src/main.rs
+++ examples/dashmap/src/main.rs
@@ -1,12 +1,3 @@
-# Concurrent Data Structures
-
-## Dashmap
-
-[Dashmap]( https://docs.rs/dashmap/5.3.3/dashmap/struct.DashMap.html# ) is an implementation of a concurrent associative array/hashmap in Rust.
-
-DashMap tries to be a direct replacement for `RwLock<HashMap<K, V>>`.
-
-```rust,ignore
 use dashmap::DashMap;
 use std::sync::Arc;
 use std::thread;
@@ -64,18 +55,9 @@ fn main() {
 
     // Access the final state of the map from the main thread
     println!("final count: {}", map.iter().count());
+
 }
-```
 
-## Bounded Multi-producer Multi-consumer Queue
 
-```rust,ignore
-use crossbeam_queue::ArrayQueue;
 
-let q = ArrayQueue::new(2);
 
-assert_eq!(q.push('a'), Ok(()));
-assert_eq!(q.push('b'), Ok(()));
-assert_eq!(q.push('c'), Err('c'));
-assert_eq!(q.pop(), Some('a'));
-```
