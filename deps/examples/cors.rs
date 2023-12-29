e9862d0e1b9dde486c2565bccfc22c59a51949f5
S John CD <john-cd@users.noreply.github.com>
2023-12-29 17:08:45 +0000

    formatting

--- deps/examples/cors.rs
+++ deps/examples/cors.rs
@@ -1,10 +1,10 @@
-use http::{Request, Response, Method, header};
-use http_body_util::Full;
 use bytes::Bytes;
-use tower::{ServiceBuilder, ServiceExt, Service};
-use tower_http::cors::{Any, CorsLayer};
+use http::{header, Method, Request, Response};
+use http_body_util::Full;
 use std::convert::Infallible;
 use std::error::Error;
+use tower::{Service, ServiceBuilder, ServiceExt};
+use tower_http::cors::{Any, CorsLayer};
 
 async fn handle(_request: Request<Full<Bytes>>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::default()))
@@ -18,23 +18,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // allow requests from any origin
         .allow_origin(Any);
 
-    let mut service = ServiceBuilder::new()
-        .layer(cors)
-        .service_fn(handle);
+    let mut service = ServiceBuilder::new().layer(cors).service_fn(handle);
 
     let request = Request::builder()
         .header(header::ORIGIN, "https://example.com")
         .body(Full::default())
         .unwrap();
 
-    let response = service
-        .ready()
-        .await?
-        .call(request)
-        .await?;
+    let response = service.ready().await?.call(request).await?;
 
     assert_eq!(
-        response.headers().get(header::ACCESS_CONTROL_ALLOW_ORIGIN).unwrap(),
+        response
+            .headers()
+            .get(header::ACCESS_CONTROL_ALLOW_ORIGIN)
+            .unwrap(),
         "*",
     );
     Ok(())
