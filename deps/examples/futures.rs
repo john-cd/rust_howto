0487acf7bb5c0cddbb3df4d42475a9e7633e65fd
S John CD <john-cd@users.noreply.github.com>
2023-12-29 03:55:02 +0000

    move futures examples to separate .rs files

---
+++ deps/examples/futures.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)]
+
+use futures::{
+    future::FutureExt, // for `.fuse()`
+    pin_mut,
+    select,
+};
+
+async fn task_one() {
+    /* ... */
+}
+async fn task_two() {
+    /* ... */
+}
+
+async fn race_tasks() {
+    let t1 = task_one().fuse();
+    let t2 = task_two().fuse();
+
+    pin_mut!(t1, t2);
+
+    select! {
+        () = t1 => println!("task one completed first"),
+        () = t2 => println!("task two completed first"),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    race_tasks().await;
+}
---
+++ deps/examples/futures3.rs
@@ -0,0 +1,28 @@
+use futures::future::FutureExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let future_of_1 = async { 1 };
+
+    // Map this future’s output to a (possibly) different type, returning a new future of the resulting type.
+    let new_future = future_of_1.map(|x| x + 3);
+
+    // Chain on a computation for when a future finished, passing the result of the future to the provided closure f.
+    let future_of_7 = new_future.then(|x| async move { x + 3 });
+    assert_eq!(future_of_7.await, 7);
+
+    // Conditional `Either` future
+    let x = 6;
+    let future = if x > 10 {
+        async { true }.left_future()
+    } else {
+        async { false }.right_future()
+    };
+    assert_eq!(future.await, false);
+
+    // Flatten nested futures
+    let nested_future = async { async { 1 } };
+    let future = nested_future.flatten();
+    assert_eq!(future.await, 1);
+    Ok(())
+}
