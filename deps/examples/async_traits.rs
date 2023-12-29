064becf7c87bda63eaabffd0cd556822832c7e4f
S John CD <john-cd@users.noreply.github.com>
2023-12-29 03:15:27 +0000

    move async trait examples to separate .rs files

---
+++ deps/examples/async_traits.rs
@@ -0,0 +1,27 @@
+struct MyHealthChecker;
+
+trait HealthCheck {
+    async fn check(&mut self) -> bool;   // <- async fn defined in a Trait
+}
+
+impl HealthCheck for MyHealthChecker {
+    async fn check(&mut self) -> bool {  // async fn implementation in the associated impl block
+        do_async_op().await
+    }
+}
+
+async fn do_health_check(mut hc: impl HealthCheck) {
+    if !hc.check().await {               // use as normal
+        log_health_check_failure().await;
+    }
+}
+
+async fn do_async_op() -> bool { true }
+
+async fn log_health_check_failure() {}
+
+#[tokio::main]
+async fn main() {
+    let hc = MyHealthChecker;
+    do_health_check(hc).await;
+}
