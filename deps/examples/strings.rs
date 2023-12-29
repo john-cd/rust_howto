810fe7a04ef1964e90bc2da09c93eb6494991863
S John CD <john-cd@users.noreply.github.com>
2023-12-29 20:22:29 +0000

    Move string examples to separate .rs files

---
+++ deps/examples/strings.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut s1 = String::from("hello");     // Unicode, not ASCII
+    s1.push_str(", world!");            // This kind of string can be mutated
+    s1.clear();                         // this empties the String, making it equal to ""
+
+    let s2 = "initial contents".to_string();  // alternative init from string literals - available on any type that implements the Display trait
+
+    let s3 = s1 + &s2;                  // concat: note s1 has been moved here and can no longer be used
+    // ERROR let s = format!("{s1}-{s2}-{s3}");
+
+    let _s: &str = &s3[0..4]; // string slice -  contains the first 4 bytes of the string.
+    // Caution: If we were to try to slice only part of a unicode character’s bytes, Rust would panic at runtime
+
+    // Iterate
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
+}
---
+++ deps/examples/strings2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    println!("x = {x} and y + 2 = {}", y + 2);
+}
