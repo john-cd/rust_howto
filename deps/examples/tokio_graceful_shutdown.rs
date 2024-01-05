85819ad85141f5425c9005d93645f1a8f5201492
S John CD <john-cd@users.noreply.github.com>
2024-01-04 23:27:09 -0800

    Fix tokio example

--- deps/examples/tokio_graceful_shutdown.rs
+++ deps/examples/tokio_graceful_shutdown.rs
@@ -1,40 +1,38 @@
-// use tokio::time::{sleep, Duration};
-// use tokio_graceful_shutdown::{SubsystemBuilder, SubsystemHandle, Toplevel};
+use tokio::time::{sleep, Duration};
+use tokio_graceful_shutdown::{SubsystemBuilder, SubsystemHandle, Toplevel};
 
-// async fn countdown() {
-//     for i in (1..=5).rev() {
-//         tracing::info!("Shutting down in: {}", i);
-//         sleep(Duration::from_millis(1000)).await;
-//     }
-// }
+async fn countdown() {
+    for i in (1..=5).rev() {
+        tracing::info!("Shutting down in: {}", i);
+        sleep(Duration::from_millis(1000)).await;
+    }
+}
 
-// async fn countdown_subsystem(subsys: SubsystemHandle) -> std::result::Result<(), _>{
-//     tokio::select! {
-//         _ = subsys.on_shutdown_requested() => {
-//             tracing::info!("Countdown cancelled.");
-//         },
-//         _ = countdown() => {
-//             subsys.request_shutdown();
-//         }
-//     };
-//     Ok(())
-// }
+async fn countdown_subsystem(subsys: SubsystemHandle) -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    tokio::select! {
+        _ = subsys.on_shutdown_requested() => {
+            tracing::info!("Countdown cancelled.");
+        },
+        _ = countdown() => {
+            subsys.request_shutdown();
+        }
+    };
+    Ok(())
+}
 
-// #[tokio::main]
-// async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-//     // Init logging
-//     tracing_subscriber::fmt()
-//         .with_max_level(tracing::Level::TRACE)
-//         .init();
+#[tokio::main]
+async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    // Init logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
 
-//     // Setup and execute subsystem tree
-//     Toplevel::new(|s| async move {
-//         s.start(SubsystemBuilder::new("Countdown", countdown_subsystem));
-//     })
-//     .catch_signals() // signals the Toplevel object to listen for SIGINT/SIGTERM/Ctrl+C
-//     .handle_shutdown_requests(Duration::from_millis(1000)) // collects all the return values of the subsystems, determines the global error state
-//     .await
-//     .map_err(|e| { e.into() })
-// }
-
-fn main() {}
+    // Setup and execute subsystem tree
+    Toplevel::new(|s| async move {
+        s.start(SubsystemBuilder::new("Countdown", countdown_subsystem));
+    })
+    .catch_signals() // signals the Toplevel object to listen for SIGINT/SIGTERM/Ctrl+C
+    .handle_shutdown_requests(Duration::from_millis(1000)) // collects all the return values of the subsystems, determines the global error state
+    .await
+    .map_err(|e| e.into())
+}
