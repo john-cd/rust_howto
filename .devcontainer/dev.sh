65089d8d37fc26a82c5b709e54237e32a3632cf6
S John CD <john-cd@users.noreply.github.com>
2024-01-12 14:02:28 -0800

    Switch to slim image

--- .devcontainer/dev.sh
+++ .devcontainer/dev.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
+# NO set -e on purpose
+set -x
+set -v
+
+if [ ! -d /code ]; then
+  echo "/code is not accessible somehow. CHECK BIND MOUNT."
+  exec "$@"
+  exit 0
+fi
+
 echo "-------------------------------------------"
 echo "Format the code"
-cargo fmt --all
+just fmtall
 
 echo "Fetch the dependencies"
 cargo fetch
 
-echo "Build all examples"
-cargo build --workspace --all-targets
+echo "Build the book"
+just build
+
+echo "Build code"
+just buildall
 
 echo "Toolchain info:"
 rustup check
 echo "-------------------------------------------"
 
 # Do not remove.
-# This is what will cause the dockerfile CMD e.g. sleep infinity to run.
+# This is what will run any dockerfile CMD.
 exec "$@"
+
+exit 0
