60d71cb6942d531404c762a42fbd24038ff3aeee
S John CD <john-cd@users.noreply.github.com>
2024-01-16 19:40:03 +0000

    Bind mount issue documentation

--- .devcontainer/dev.sh
+++ .devcontainer/dev.sh
@@ -4,10 +4,20 @@
 set -x
 set -v
 
+## Check whether Docker has failed to bind mount the development laptop's project folder to /code
+## You may also verify that the bind mount was created correctly using `docker inspect <container_name>`. Look for the Mounts section.
+## Bind mount failures may happen with Dev Drives under Win 11. In this case, store your code in WSL e.g. ~/code/rust_howto/
+## and then use `code .` from the WSL prompt in that folder.
 if [ ! -d /code/.devcontainer/ ]; then
   echo "ERROR: CHECK BIND MOUNT"
   exec "$@"
-  exit 0
+  exit 111
+fi
+
+if [ ! -f /code/justfile ]; then
+  echo "ERROR: CHECK BIND MOUNT"
+  exec "$@"
+  exit 112
 fi
 
 echo "-------------------------------------------"
