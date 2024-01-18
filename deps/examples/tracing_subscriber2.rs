48f30e6e955db33dae50e03e4445af529bc3e9c5
S John CD <john-cd@users.noreply.github.com>
2024-01-18 09:12:34 +0000

    Disable two examples for now

--- deps/examples/tracing_subscriber2.rs
+++ deps/examples/tracing_subscriber2.rs
@@ -1,13 +1,13 @@
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
+// use tracing_subscriber::layer::SubscriberExt;
+// use tracing_subscriber::util::SubscriberInitExt;
 
 fn main() {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::filter::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| {
-                "myproj=debug,axum=debug,tower_http=debug,mongodb=debug".into()
-            }),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+//     tracing_subscriber::registry()
+//         .with(tracing_subscriber::fmt::layer())
+//         .with(tracing_subscriber::filter::EnvFilter::new(
+//             std::env::var("RUST_LOG").unwrap_or_else(|_| {
+//                 "myproj=debug,axum=debug,tower_http=debug,mongodb=debug".into()
+//             }),
+//         ))
+//         .init();
 }
--- deps/examples/tracing_subscriber3.rs
+++ deps/examples/tracing_subscriber3.rs
@@ -1,15 +1,15 @@
-use tracing_subscriber::filter::EnvFilter;
-use tracing_subscriber::fmt;
-use tracing_subscriber::prelude::*;
+// use tracing_subscriber::filter::EnvFilter;
+// use tracing_subscriber::fmt;
+// use tracing_subscriber::prelude::*;
 
 fn main() {
-    let fmt_layer = fmt::layer().with_target(false);
-    let filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("info"))
-        .unwrap();
+    // let fmt_layer = fmt::layer().with_target(false);
+    // let filter_layer = EnvFilter::try_from_default_env()
+    //     .or_else(|_| EnvFilter::try_new("info"))
+    //     .unwrap();
 
-    tracing_subscriber::registry()
-        .with(filter_layer)
-        .with(fmt_layer)
-        .init();
+    // tracing_subscriber::registry()
+    //     .with(filter_layer)
+    //     .with(fmt_layer)
+    //     .init();
 }
