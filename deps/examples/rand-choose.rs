fc26f65d2609f4669358cdcd41f3f6f35942e365
S John CD <john-cd@users.noreply.github.com>
2024-01-19 15:58:01 +0000

    Extract examples from drafts (wip)

---
+++ deps/examples/rand-choose.rs
@@ -0,0 +1,17 @@
+fn main() {
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    println!("{:?}", password);
+}
