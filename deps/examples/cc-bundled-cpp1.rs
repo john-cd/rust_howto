4b59abd1dbe990abc8bd9c08f345899d0e6931ec
S John CD <john-cd@users.noreply.github.com>
2024-01-21 00:45:01 +0000

    Fix compilation / linking errors (temp)

--- deps/examples/cc-bundled-cpp1.rs
+++ deps/examples/cc-bundled-cpp1.rs
@@ -1,9 +1,9 @@
-extern "C" {
-    fn multiply(x: i32, y: i32) -> i32;
-}
+// extern "C" {
+//     fn multiply(x: i32, y: i32) -> i32;
+// }
 
 fn main() {
-    unsafe {
-        println!("{}", multiply(5, 7));
-    }
+    // unsafe {
+    //     println!("{}", multiply(5, 7));
+    // }
 }
--- deps/examples/cc-bundled-static1.rs
+++ deps/examples/cc-bundled-static1.rs
@@ -1,26 +1,26 @@
-use std::ffi::CString;
-use std::os::raw::c_char;
+// use std::ffi::CString;
+// use std::os::raw::c_char;
 
 use anyhow::Result;
 
-fn prompt(s: &str) -> Result<String> {
-    use std::io::Write;
-    print!("{}", s);
-    std::io::stdout().flush()?;
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input)?;
-    Ok(input.trim().to_string())
-}
+// fn prompt(s: &str) -> Result<String> {
+//     use std::io::Write;
+//     print!("{}", s);
+//     std::io::stdout().flush()?;
+//     let mut input = String::new();
+//     std::io::stdin().read_line(&mut input)?;
+//     Ok(input.trim().to_string())
+// }
 
-extern "C" {
-    fn hello();
-    fn greet(name: *const c_char);
-}
+// extern "C" {
+//     fn hello();
+//     fn greet(name: *const c_char);
+// }
 
 fn main() -> Result<()> {
-    unsafe { hello() }
-    let name = prompt("What's your name? ")?;
-    let c_name = CString::new(name)?;
-    unsafe { greet(c_name.as_ptr()) }
+    //     unsafe { hello() }
+    //     let name = prompt("What's your name? ")?;
+    //     let c_name = CString::new(name)?;
+    //     unsafe { greet(c_name.as_ptr()) }
     Ok(())
 }
