6ed01ac95d377c6eef85dcee1a72f6b5a57db665
S John CD <john-cd@users.noreply.github.com>
2024-01-25 23:25:25 +0000

    finish "publish"

--- publish/src/main.rs
+++ publish/src/main.rs
@@ -5,11 +5,20 @@ use cargo_toml::Manifest;
 fn main() -> Result<(), Box<dyn Error>> {
     println!("This crate is a placeholder. It does nothing useful.\n");
 
+    // Let's print parts of the Cargo.toml manifest.
+
     let cargo_toml_bytes = include_bytes!("../Cargo.toml");
     let manifest = Manifest::from_slice(cargo_toml_bytes)?;
+    // Cargo.toml refers to the workspace ../Cargo.toml,
+    // therefore some fields are not available until
+    // `cargo package` rewrites it with full details.
+    let some_fields_unavailable = manifest.needs_workspace_inheritance();
 
     if let Some(package) = manifest.package {
         println!("Name: {}\n", package.name);
+        if some_fields_unavailable {
+            return Ok(());
+         }
         println!(
             "Description: {}\n",
             package.description.unwrap_or_default().get()?
