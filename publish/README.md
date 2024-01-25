161c5b071f56b4726b647be17dadc319b585d02a
S John CD <john-cd@users.noreply.github.com>
2024-01-25 19:39:54 +0000

    Add cargo metadata; create "publish" package

---
+++ publish/README.md
@@ -0,0 +1,8 @@
+# Rust How-To
+
+**A documentation summary and a compendium of snippets and recipes for the Rust language and ecosystem.**
+**Everything you need for day-to-day Rust coding, all in one place.**
+
+The "Rust How-To" book is deployed on <https://john-cd.com/rust_howto>
+
+GitHub repo: <https://john-cd.github.io/rust_howto>
---
+++ publish/src/main.rs
@@ -0,0 +1,35 @@
+
+use std::error::Error;
+
+use cargo_toml::Manifest;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("This crate is a placeholder. It does nothing useful.\n");
+    
+    let cargo_toml_bytes = include_bytes!("../Cargo.toml");
+    let manifest = Manifest::from_slice(cargo_toml_bytes)?;
+
+    if let Some(package) = manifest.package {
+        println!("Name: {}\n", package.name);
+        println!(
+            "Description: {}\n",
+            package.description.unwrap_or_default().get()?
+        );
+        println!("Authors: {:?}\n", package.authors.get()?);
+        println!(
+            "Homepage: {}\n",
+            package.homepage.unwrap_or_default().get()?
+        );
+        println!(
+            "Repository: {}\n",
+            package.repository.unwrap_or_default().get()?
+        );
+        println!(
+            "Documentation: {}\n",
+            package.documentation.unwrap_or_default().get()?
+        );
+        println!("Keywords: {:?}\n", package.keywords.get()?);
+        println!("Categories: {:?}\n", package.categories.get()?);
+    }
+    Ok(())
+}
