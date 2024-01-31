ff440f21d933ab4b80e18adb415a2b49e88bdfb6
John CD <johncd222@gmail.com>
2024-01-31 19:13:14 +0000

    mdbook-utils: move to a separate repo

--- mdbook-utils/README.md
+++
@@ -1,5 +0,0 @@
-# mdBook Utilities
-
-This package contains tools to manage links, reference definitions, code examples in Markdown files, especially `mdbook` source directories."
-
-Run the tool without arguments to display the the list of commands.
--- mdbook-utils/src/fs/mod.rs
+++
@@ -1,9 +0,0 @@
-/// Functions that use the filesystem: create or check existence of
-/// directories, read multiple files from a directory, etc
-mod dir;
-mod find_markdown_files;
-mod read_files;
-
-pub use dir::*;
-pub use find_markdown_files::*;
-pub(crate) use read_files::*;
