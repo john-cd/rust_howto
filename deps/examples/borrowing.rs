c7a686006fa869f27001768f149eeca6b80fd988
John <john-cd@users.noreply.github.com>
2024-02-09 17:19:48 +0000

    Cargo clippy

--- deps/examples/borrowing.rs
+++ deps/examples/borrowing.rs
@@ -3,7 +3,7 @@ fn main() {
 
     let _len = calculate_length(&s1); // pass an immutable reference to s1
 
-    fn calculate_length(s: &String) -> usize {
+    fn calculate_length(s: &str) -> usize {
         s.len()
     } // Here, s goes out of scope. But because it does not have ownership of what
     // it refers to, s1 is not dropped.
