c30cdac80254609c9aa7ac07741625eb38979ccd
John <john-cd@users.noreply.github.com>
2024-10-28 19:15:51 +0000

    Code formatting

--- deps/tests/multimap.rs
+++ deps/tests/multimap.rs
@@ -1,11 +1,12 @@
-use multimap::MultiMap;
 use anyhow::Result;
-use crates_io_api::SyncClient;
 use crates_io_api::Category;
+use crates_io_api::SyncClient;
+use multimap::MultiMap;
 
-// Calls the crates.io API client and retrieve the categories a given crate belongs to.
+// Calls the crates.io API client and retrieve the categories a given crate
+// belongs to.
 fn get_categories_for_crate(crate_name: &str) -> Result<Vec<Category>> {
-        let client = SyncClient::new(
+    let client = SyncClient::new(
         "my-user-agent (my-contact@domain.com)",
         std::time::Duration::from_millis(1000), // rate limit interval
     )?;
@@ -19,7 +20,7 @@ fn test() -> Result<()> {
     let crate_names = vec!["toml", "config", "nom", "pest"];
 
     let mut m: MultiMap<String, &str> = MultiMap::new();
-    for  name in crate_names {
+    for name in crate_names {
         for cat in get_categories_for_crate(name)? {
             // There can be multiple crates in the same category
             // A multimap allows multiple values for the same key
@@ -28,7 +29,10 @@ fn test() -> Result<()> {
     }
 
     // Get all values for a given key
-    println!("List of crates in the `config` category: {:?}", m.get_vec("config"));
+    println!(
+        "List of crates in the `config` category: {:?}",
+        m.get_vec("config")
+    );
 
     // Or iterate over all keys and the key's vector
     for (cat, names) in m.iter_all() {
