515222ea008f66b60779a473659f55cd2825b2bc
John <john-cd@users.noreply.github.com>
2024-10-31 22:06:22 +0000

    code formatting

--- tools/src/templates/category_badge.rs
+++ tools/src/templates/category_badge.rs
@@ -2,7 +2,8 @@ use anyhow::Result;
 use serde::Serialize;
 
 /// Category badge
-pub static CAT_BADGE: &str = r"[![cat-{slug}][cat-{slug}-badge]][cat-{slug}]\{\{hi:{category}}} ";
+pub static CAT_BADGE: &str =
+    r"[![cat-{slug}][cat-{slug}-badge]][cat-{slug}]\{\{hi:{category}}} ";
 
 /// create_category_badge
 ///
