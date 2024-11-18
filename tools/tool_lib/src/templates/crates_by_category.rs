b04e1403e167476fd29df1b88f643c56d713b314
John <john-cd@users.noreply.github.com>
2024-11-18 21:22:08 +0000

    cargo doc warning fix

--- tools/tool_lib/src/templates/crates_by_category.rs
+++ tools/tool_lib/src/templates/crates_by_category.rs
@@ -11,15 +11,15 @@ struct Context<'a> {
     crate_names: Vec<&'a str>,
 }
 
-/// create_category_and_crates
-/// Returns one line in the "crates organized by category" table:
-/// | <category badge> | <description> | <crate badge> <crate badge>... |
-///
-/// category: category name e.g. Encoding
-/// slug: category slug used by the crates.io website e.g. encoding
-/// description: description of the category
-/// crate_names: list of the names of the crates to include in the category (per
-/// lib.rs)
+// create_category_and_crates
+// Returns one line in the "crates organized by category" table:
+// | <category badge> | <description> | <crate badge> <crate badge>... |
+//
+// category: category name e.g. Encoding
+// slug: category slug used by the crates.io website e.g. encoding
+// description: description of the category
+// crate_names: list of the names of the crates to include in the category (per
+// lib.rs)
 pub fn create_category_and_crates(
     category: &str,
     slug: &str,
