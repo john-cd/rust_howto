d6aaf7addb9852f7bd687217d075f6789f123e68
John <john-cd@users.noreply.github.com>
2024-11-29 23:32:46 +0000

    add `other` folder and example stubs

---
+++ deps/tests/other/cross_platform/crux.rs
@@ -0,0 +1,9 @@
+// ANCHOR: example
+fn main() {
+}
+// ANCHOR_END: example
+
+#[test]
+fn test() {
+    main();
+}
---
+++ deps/tests/other/main.rs
@@ -0,0 +1,8 @@
+//! Rather than running all tests in separate processes (by keeping them in
+//! separate files in deps/tests), we group all the tests in each section of the
+//! book in the same crate.
+//!
+//! All examples in this folder MUST be listed as module below or they won't be
+//! discovered / run.
+
+mod cross_platform;
