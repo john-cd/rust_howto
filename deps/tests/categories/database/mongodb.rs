8754905835424fd8ccf1b2222776d4e151a8bde2
John <john-cd@users.noreply.github.com>
2024-12-13 21:13:07 +0000

    add mongodb example

--- deps/tests/categories/database/mongodb.rs
+++ deps/tests/categories/database/mongodb.rs
@@ -1,12 +1,45 @@
 // ANCHOR: example
-fn main() {
-    // TODO
-    todo!();
+use mongodb::{Client, bson::doc};
+use serde::{Serialize, Deserialize};
+use dotenvy::dotenv;
+use std::env;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    name: String,
+    age: i32,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Read the .env file, if present
+    dotenv().ok();
+
+    // Example: mongodb://root:passwd@server:27017/
+    let mongo_uri = env::var("MONGO_URI")?;
+    let client = Client::with_uri_str(&mongo_uri).await?;
+    let db = client.database("test_db");
+    let collection = db.collection::<User>("users");
+
+    let user = User { name: String::from("Alice"), age: 30 };
+    collection.insert_one(user).await?;
+
+    let filter = doc! { "name": "Alice" };
+    if let Some(user) = collection.find_one(filter).await? {
+        println!("Found user: {:?}", user);
+    } else {
+        println!("User not found");
+    }
+
+    Ok(())
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
-    main();
+    unsafe {
+        // Refer to the compose*.yaml files
+        env::set_var("MONGO_URI", "mongodb://mongoadmin:mysecretpassword@rust_howto_dev-mongodb-1:27017/");
+    }
+    main().unwrap();
 }
