e436bbe053e6cdf5d18426d5b57173d5976b7c27
John CD <john-cd@users.noreply.github.com>
2024-12-15 11:28:37 -0800

    add `Result` example

--- deps/tests/standard_library/result.rs
+++ deps/tests/standard_library/result.rs
@@ -1,12 +1,30 @@
 // ANCHOR: example
+use std::fs::File;
+use std::io::Read;
+use std::io::{self};
+
+fn read_file(file_path: &str) -> Result<String, io::Error> {
+    // If opening the file fails, return with an Err
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    // If reading the file fails, return with an Err
+    file.read_to_string(&mut contents)?;
+    // Happy path: return the contents of the file as a String
+    Ok(contents)
+}
+
 fn main() {
-    // TODO P0
-    todo!();
+    let file_path = "example.txt";
+
+    // Handle the `Result`
+    match read_file(file_path) {
+        Ok(contents) => println!("File content: \n{}", contents),
+        Err(e) => eprintln!("An error occurred: {}", e),
+    }
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
