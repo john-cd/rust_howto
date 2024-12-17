9cb8cd28e98ffce2cbe8723ff75b42404df8fee2
John <john-cd@users.noreply.github.com>
2024-12-17 07:33:47 +0000

    fix #149

--- deps/tests/categories/config/dotenvy.rs
+++ deps/tests/categories/config/dotenvy.rs
@@ -6,7 +6,12 @@ use anyhow::Result;
 fn main() -> Result<()> {
     // Load environment variables from .env file.
     // Fails if .env file not found, not readable or invalid.
+    // If variables with the same names already exist in the environment,
+    // their values will be preserved. If multiple declarations for the
+    // same environment variable exist in your .env file, the first one is
+    // applied.
     dotenvy::dotenv()?;
+    // OR: dotenvy::dotenv().ok();
 
     for (key, value) in env::vars() {
         println!("{key}: {value}");
@@ -21,4 +26,3 @@ fn test() -> Result<()> {
     main()?;
     Ok(())
 }
-// TODO P0 .ok()
