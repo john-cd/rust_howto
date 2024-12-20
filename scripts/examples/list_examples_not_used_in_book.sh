752f0095d1b13d0dd66e6bb0bb71720830273098
John <john-cd@users.noreply.github.com>
2024-12-20 21:24:27 +0000

    fix: update script to list examples in the correct directory

--- scripts/examples/list_examples_not_used_in_book.sh
+++ scripts/examples/list_examples_not_used_in_book.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 set -eu
 
-# List examples (.rs files) in `deps/tests` that are not included in the book markdown.
+# List examples (.rs files) in `tests` that are not included in the book markdown.
 # Usage: /code/scripts/examples/list_examples_not_used_in_book.sh
 
 root="/code/"
 
-grep -Proh '\{\{#include .+?\.rs(:.+?)?\}\}' "${root}src" "${root}drafts" | sed -E 's~\{\{#include .+/([._a-zA-Z0-9]+?\.rs)(:.+?)?\}\}~\1~' | sort -u > /tmp/examples_in_markdown.txt
-find "${root}deps/tests" -type f -name "*.rs" -exec basename {} \; | sed '/main.rs/d; /mod.rs/d' | sort -u > /tmp/examples_in_deps.txt
+grep -Proh '\{\{#include .+?\.rs(:.+?)?\}\}' "${root}src" "${root}drafts" | sed -E 's~\{\{#include [./]*([^.]+/[._a-zA-Z0-9]+?\.rs)(:.+?)?\}\}~\1~' | sort -u > /tmp/examples_in_markdown.txt
+find "${root}crates/ex/" -type f -name "*.rs" -exec realpath --relative-to=${root} {} \; | sed '/lib.rs/d; /main.rs/d; /mod.rs/d; /build.rs/d; /cat.rs/d; /lang.rs/d; /std.rs/d; /topic.rs/d' | sort -u > /tmp/examples_in_deps.txt
 comm -13 /tmp/examples_in_markdown.txt /tmp/examples_in_deps.txt
 
-# The script matches e.g. {{#include ../../../deps/tests/cats/development_tools_debugging/type_name_of_val.rs:example}} and extracts the file names
+# The script matches e.g. {{#include ../../../crates/ex/categories/d/tests/development_tools_debugging/type_name_of_val.rs:example}} and extracts the file names
 # then compare to the list of test files in deps
 # A few files e.g. `main.rs` and `mod.rs` are not true examples and should not be included into the book.
 
