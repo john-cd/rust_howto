afcd3d63d9063b9d9275fad7d38c04eb89cb8c6e
John <john-cd@users.noreply.github.com>
2024-12-22 03:53:44 +0000

    feat: add directories dependency and add data/config directory retrieval example. close #760

--- crates/ex/categories/efghijkl/tests/filesystem/directories.rs
+++ crates/ex/categories/efghijkl/tests/filesystem/directories.rs
@@ -1,12 +1,43 @@
 // ANCHOR: example
+use std::path::PathBuf;
+
+use anyhow::Result;
+use anyhow::anyhow;
+use directories::ProjectDirs;
+
+pub fn get_data_dir() -> Result<PathBuf> {
+    let directory = if let Ok(s) = std::env::var("APP_DATA_DIR") {
+        PathBuf::from(s)
+    } else if let Some(proj_dirs) =
+        ProjectDirs::from("com", "Foo Corp", "Bar App")
+    {
+        proj_dirs.data_local_dir().to_path_buf()
+    } else {
+        return Err(anyhow!("Unable to find the data directory"));
+    };
+    Ok(directory)
+}
+
+pub fn get_config_dir() -> Result<PathBuf> {
+    let directory = if let Ok(s) = std::env::var("APP_CONFIG_DIR") {
+        PathBuf::from(s)
+    } else if let Some(proj_dirs) =
+        ProjectDirs::from("com", "Foo Corp", "Bar App")
+    {
+        proj_dirs.config_local_dir().to_path_buf()
+    } else {
+        return Err(anyhow!("Unable to find the config directory"));
+    };
+    Ok(directory)
+}
+
 fn main() {
-    todo!();
+    println!("{:?}", get_data_dir());
+    println!("{:?}", get_config_dir());
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
-// [P0](https://github.com/john-cd/rust_howto/issues/760)
