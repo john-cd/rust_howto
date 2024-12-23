16f18ac20da5a837a8f296822fd93a74fd13fefc
John <john-cd@users.noreply.github.com>
2024-12-23 06:13:59 +0000

    clean up RefCell example; close #894

--- crates/ex/standard_library/tests/other/refcell.rs
+++ crates/ex/standard_library/tests/other/refcell.rs
@@ -1,13 +1,6 @@
 // ANCHOR: example
 use std::cell::RefCell;
 
-// The `RefCell` type in Rust is used for _interior mutability_,
-// a pattern that allows you to mutate data even when there are immutable
-// references to it. `RefCell` dynamically borrow checks at runtime,
-// unlike Rust's standard borrowing rules (checks at compile time).
-// Attempts to violate borrowing rules (like having multiple mutable borrows)
-// will cause a panic at runtime. `RefCell` is single-threaded.
-// The corresponding `Sync` version of `RefCell<T>` is `RwLock<T>`.
 fn main() {
     // Create a RefCell containing a vector of integers
     let data = RefCell::new(vec![1, 2, 3, 4, 5]);
@@ -16,20 +9,31 @@ fn main() {
     {
         let borrowed_data = data.borrow();
         println!("Borrowed (immutable): {:?}", borrowed_data);
-    } // The immutable borrow ends here
+
+        // We can borrow the data immutably again
+        let _borrowed_data2 = data.borrow();
+    } // The immutable borrow ends here (when the `Ref` guard returned by `borrow` exits scope.)
 
     // Borrow the data mutably and modify it
     {
         let mut borrowed_data = data.borrow_mut();
         borrowed_data.push(6);
         println!("Borrowed (mutable): {:?}", borrowed_data);
+
+        // We can't borrow the data again while it's borrowed mutably
+        assert!(data.try_borrow().is_err());
+        assert!(data.try_borrow_mut().is_err());
     } // The mutable borrow ends here
 
     // Borrow the data immutably again to check the modification
     {
-        let borrowed_data = data.borrow();
-        println!("Borrowed (immutable again): {:?}", borrowed_data);
+        if let Ok(borrowed_data) = data.try_borrow() {
+            println!("Borrowed (immutable again): {:?}", borrowed_data);
+        }
     }
+
+    // We can also consume the `RefCell`, returning the wrapped value.
+    let _data = data.into_inner();
 }
 // ANCHOR_END: example
 
@@ -37,4 +41,3 @@ fn main() {
 fn test() {
     main();
 }
-// [P0](https://github.com/john-cd/rust_howto/issues/894)
