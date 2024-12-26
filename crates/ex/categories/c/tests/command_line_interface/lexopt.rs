a4ceb2c71f7bdaa6be72cc74d4dffdd1bee01592
John <john-cd@users.noreply.github.com>
2024-12-26 21:24:50 +0000

    add lexopt example (wip) #678

--- crates/ex/categories/c/tests/command_line_interface/lexopt.rs
+++ crates/ex/categories/c/tests/command_line_interface/lexopt.rs
@@ -1,12 +1,45 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use lexopt::prelude::*;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/678)
+// fn main() -> anyhow::Result<()> {
+//     let mut parser = lexopt::Parser::from_env();
+//     let mut name = String::new();
+//     let mut age = 0;
+
+//     while let Some(arg) = parser.next().unwrap() {
+//         match arg {
+//             lexopt::Arg::Long("name") => {
+//                 name = parser.value()?.to_string_lossy().to_string();
+//             }
+//             lexopt::Arg::Long("age") => {
+//                 age = parser.value()?.parse().expect("Invalid age");
+//             }
+//             lexopt::Arg::Value(val) => {
+//                 eprintln!("Unexpected argument: {:?}", val);
+//             }
+//             _ => {
+//                 eprintln!("Unexpected flag or option");
+//             }
+//         }
+//     }
+
+//     println!("Name: {}", name);
+//     println!("Age: {}", age);
+//     Ok(())
+// }
+
+// #[test]
+// fn test() {
+//     // Simulate command-line arguments
+//     unsafe {
+//         std::env::set_var("CARGO_BIN_EXE_lexopt_example", "lexopt_example");
+//         std::env::set_var(
+//             "CARGO_BIN_EXE_lexopt_example",
+//             "--name John --age 30",
+//         );
+//     }
+//     main().unwrap();
+// }
+// // ANCHOR_END: example
+// // TODO finish
+// // [P1](https://github.com/john-cd/rust_howto/issues/678)
