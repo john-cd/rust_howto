012b2d22cdbbb5f16d9aea600e2c6c5397304f1e
John <john-cd@users.noreply.github.com>
2024-12-30 17:45:51 +0000

    add stakker example (WIP) #94

--- crates/ex/categories/c/tests/concurrency/stakker.rs
+++ crates/ex/categories/c/tests/concurrency/stakker.rs
@@ -1,12 +1,37 @@
 // ANCHOR: example
-fn main() {
-    todo!();
-}
+
+// Stakker is an actor-based system for Rust, which makes it great for building
+// concurrent, distributed applications.
+// use stakker::Deferrer;
+// use stakker::Stakker;
+// use stakker::StopCause;
+// use stakker::actor;
+// use stakker::stop;
+
+// struct Hello;
+
+// impl Hello {
+//     fn greet(&self) {
+//         println!("Hello, world!");
+//     }
+// }
+
+// fn main() {
+//     let mut stakker = Stakker::new(Deferrer::new());
+
+//     actor!(stakker, Hello::greet(), async {
+//         let actor = Hello;
+//         actor.greet();
+//         stop!(actor);
+//     });
+
+//     stakker.run(10_000_000);
+// }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
+#[ignore = "not implemented"]
 fn test() {
-    main();
+    // main();
 }
 // [add stakker example](https://github.com/john-cd/rust_howto/issues/94)
