4425421bd2d5257935cb9e4b42349ab24e8f62e8
John <john-cd@users.noreply.github.com>
2025-01-01 10:12:35 +0000

    code example placeholder (#754)

--- crates/ex/categories/efghijkl/tests/encoding/monostate.rs
+++ crates/ex/categories/efghijkl/tests/encoding/monostate.rs
@@ -1,12 +1,34 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use monostate::MonoState;
+// use std::sync::Arc;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/754)
+// monostate is a crate in Rust that allows you to create a singleton type which
+// is always the same value and thus can be used for types that need to be Send
+// and Sync without any synchronization.
+
+// // Define a struct with MonoState
+// #[derive(Debug, Default)]
+// struct MySingleton {
+//     state: MonoState,
+//     data: Arc<String>,
+// }
+
+// fn main() {
+//     let instance1 = MySingleton {
+//         state: MonoState::default(),
+//         data: Arc::new("Hello, MonoState!".to_string()),
+//     };
+
+//     let instance2 = instance1.clone();
+
+//     println!("Instance 1: {:?}", instance1);
+//     println!("Instance 2: {:?}", instance2);
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/754)
