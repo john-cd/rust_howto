474f63c48b48c6a5379d07a40c92c7052e20e722
John <john-cd@users.noreply.github.com>
2025-01-01 09:58:35 +0000

    code example placeholder (#687)

--- crates/ex/categories/c/tests/cryptography/aes_gcm.rs
+++ crates/ex/categories/c/tests/cryptography/aes_gcm.rs
@@ -1,12 +1,41 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// extern crate aes_gcm;
+// extern crate rand;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/687)
+// use aes_gcm::{
+//     Aes256Gcm, // Use Aes128Gcm or Aes256Gcm, respectively
+//     aead::{Aead, AeadInPlace, Key, KeyInit, Nonce, OsRng, Payload}, /* Or
+// `AeadInPlace` for in-place encryption */ };
+
+// fn main() {
+//     // Generate a random 256-bit key
+//     let key = Key::<Aes256Gcm>::generate(&mut OsRng);
+
+//     // Generate a random 96-bit nonce
+//     let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per
+// message
+
+//     let cipher = Aes256Gcm::new(&key);
+
+//     let plaintext = b"Secret message";
+//     let ciphertext = cipher
+//         .encrypt(nonce, plaintext.as_ref())
+//         .expect("encryption failure!");
+
+//     let decrypted_ciphertext = cipher
+//         .decrypt(nonce, ciphertext.as_ref())
+//         .expect("decryption failure!");
+
+//     assert_eq!(plaintext, decrypted_ciphertext.as_slice());
+
+//     println!("Ciphertext: {:?}", ciphertext);
+//     println!("Decrypted Ciphertext: {:?}", decrypted_ciphertext);
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/687)
