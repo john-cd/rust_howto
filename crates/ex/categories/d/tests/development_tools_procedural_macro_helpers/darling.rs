ad763fc13b1f1bfd58f2e021c21be99b33edddc0
John <john-cd@users.noreply.github.com>
2025-01-01 10:12:02 +0000

    code example placeholder (#739)

--- crates/ex/categories/d/tests/development_tools_procedural_macro_helpers/darling.rs
+++ crates/ex/categories/d/tests/development_tools_procedural_macro_helpers/darling.rs
@@ -1,12 +1,41 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// // darling is a crate in Rust that helps you write custom derive macros more
+// easily. It provides a way to parse attributes on structs and enums, which can
+// be useful when writing procedural macros. use proc_macro::TokenStream;
+// use syn::{parse_macro_input, DeriveInput};
+// use quote::quote;
+// use darling::FromDeriveInput;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/739)
+// #[derive(Debug, FromDeriveInput)]
+// #[darling(attributes(my_macro))]
+// struct MyMacroOpts {
+//     name: String,
+// }
+
+// #[proc_macro_derive(MyMacro, attributes(my_macro))]
+// pub fn my_macro_derive(input: TokenStream) -> TokenStream {
+//     let input = parse_macro_input!(input as DeriveInput);
+
+//     let opts = MyMacroOpts::from_derive_input(&input).expect("Wrong
+// options!");
+
+//     let name = &opts.name;
+
+//     let expanded = quote! {
+//         impl MyMacro {
+//             pub fn hello() {
+//                 println!("Hello, {}!", #name);
+//             }
+//         }
+//     };
+
+//     TokenStream::from(expanded)
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/739)
