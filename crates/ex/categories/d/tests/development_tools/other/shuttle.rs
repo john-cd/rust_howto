096933a31f6bb9398a93bb2a7a99456074f8f14f
John <john-cd@users.noreply.github.com>
2025-01-01 10:11:16 +0000

    code example placeholder (#49)

--- crates/ex/categories/d/tests/development_tools/other/shuttle.rs
+++ crates/ex/categories/d/tests/development_tools/other/shuttle.rs
@@ -1,12 +1,23 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use shuttle_microservice::prelude::*;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [add shuttle.rs example P1](https://github.com/john-cd/rust_howto/issues/49)
+// // We use the shuttle_microservice crate to create a simple web application.
+// // We define an async init function with the shuttle_service attribute to initialize the application.
+// // Inside the init function, we create an instance of App and define a single route (/) that returns "Hello, Shuttle!" when accessed.
+// // We return an instance of ShuttleService with the initialized app.
+
+// #[shuttle_service]
+// async fn init() -> Result<ShuttleService, ShuttleError> {
+//     let app = shuttle_microservice::app::App::new()
+//         .route("/", get(|| async { "Hello, Shuttle!" }));
+
+//     Ok(ShuttleService::new(app))
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [add shuttle.rs example P1](https://github.com/john-cd/rust_howto/issues/49)
