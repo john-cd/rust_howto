1ae274f9e571e0f9f6e019c871495e901a7fca3d
John <john-cd@users.noreply.github.com>
2025-01-03 15:09:36 +0000

    add paste example (#740)

--- crates/ex/categories/d/tests/development_tools_procedural_macro_helpers/paste.rs
+++ crates/ex/categories/d/tests/development_tools_procedural_macro_helpers/paste.rs
@@ -1,11 +1,45 @@
 // ANCHOR: example
+use paste::paste;
+
+// The `paste` macro concatenate identifiers within the `create_structs` macro,
+// creating structures and their implementations dynamically.
+
+macro_rules! create_structs {
+    ($($name:ident),*) => {
+        paste! {
+            $(
+                struct [< $name Struct >] {
+                    value: i32,
+                }
+
+                impl [< $name Struct >] {
+                    fn new(value: i32) -> Self {
+                        Self { value }
+                    }
+
+                    fn get_value(&self) -> i32 {
+                        self.value
+                    }
+                }
+            )*
+        }
+    }
+}
+
+create_structs!(Foo, Bar, Baz);
+
 fn main() {
-    todo!();
+    let foo = FooStruct::new(10);
+    let bar = BarStruct::new(20);
+    let baz = BazStruct::new(30);
+
+    println!("Foo: {}", foo.get_value());
+    println!("Bar: {}", bar.get_value());
+    println!("Baz: {}", baz.get_value());
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
