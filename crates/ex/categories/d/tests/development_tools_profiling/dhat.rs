49d8fab4924f342293ad09767e8cb0400b4fa15f
John <john-cd@users.noreply.github.com>
2025-01-03 15:37:23 +0000

    add dhat example (WIP) (#746)

--- crates/ex/categories/d/tests/development_tools_profiling/dhat.rs
+++ crates/ex/categories/d/tests/development_tools_profiling/dhat.rs
@@ -1,12 +1,31 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use dhat::Dhat;
+// use dhat::DhatAlloc;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/746)
+// // We set DhatAlloc as the global allocator using the #[global_allocator]
+// attribute. #[global_allocator]
+// static ALLOC: DhatAlloc = DhatAlloc;
+
+// fn main() {
+//     // Start the heap profiling session
+//     let _dhat = Dhat::start_heap_profiling();
+
+//     // Example code to profile
+//     // Create a vector and pushes 1000 elements into it.
+//     let mut vec = Vec::new();
+//     for i in 0..1000 {
+//         vec.push(i);
+//     }
+
+//     println!("Memory profiling complete!");
+//     // When you run this code, dhat will collect heap profiling information.
+//     // The profiling data will be saved in a file named `dhat-heap.json` in
+// the current directory.     // You can then use the `dhat` tool to analyze the
+// profiling data. }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/746)
