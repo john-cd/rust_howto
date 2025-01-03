5ed2a1f256361cbcf162b284934f8fe7c375995f
John <john-cd@users.noreply.github.com>
2025-01-03 15:36:14 +0000

    add criterion example (WIP) (#745)

--- crates/ex/categories/d/tests/development_tools_profiling/criterion.rs
+++ crates/ex/categories/d/tests/development_tools_profiling/criterion.rs
@@ -1,11 +1,33 @@
 // ANCHOR: example
-fn main() {
-    todo!();
+use criterion::Criterion;
+use criterion::black_box;
+use criterion::criterion_group;
+use criterion::criterion_main;
+
+// Define a simple fibonacci function.
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
 }
+
+// The benchmark_fibonacci function benchmarks the performance of the fibonacci
+// function with the input 20.
+fn benchmark_fibonacci(c: &mut Criterion) {
+    c.bench_function("fibonacci 20", |b| b.iter(|| fibonacci(black_box(20))));
+}
+
+// Use criterion_group! and criterion_main! macros to specify and run the
+// benchmarks.
+criterion_group!(benches, benchmark_fibonacci);
+criterion_main!(benches);
+
+// Run the benchmarks with `cargo bench`
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
