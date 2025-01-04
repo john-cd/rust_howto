54b2a671e452c8e265a24303fc5624ca301281ca
John <john-cd@users.noreply.github.com>
2025-01-04 11:08:27 +0000

    add rhai example (WIP) (#887)

--- crates/ex/other/tests/scripting/rhai.rs
+++ crates/ex/other/tests/scripting/rhai.rs
@@ -1,12 +1,40 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/887)
+// // Rhai is a lightweight scripting language.
+// // It allows to embed scripting capabilities in Rust applications.
+
+// use rhai::Engine;
+// use rhai::RegisterFn;
+
+// fn add(x: i64, y: i64) -> i64 {
+//     x + y
+// }
+
+// fn main() {
+//     // Create a new `Rhai` scripting engine
+//     let mut engine = Engine::new();
+
+//     // Register a Rust function to be used in the script
+//     engine.register_fn("add", add);
+
+//     // Define a simple script
+//     let script = r#"
+//         let a = 42;
+//         let b = 58;
+//         let result = add(a, b);
+//         result
+//     "#;
+
+//     // Evaluate the script
+//     match engine.eval::<i64>(script) {
+//         Ok(result) => println!("The result of the script is: {}", result),
+//         Err(e) => println!("Error evaluating the script: {:?}", e),
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/887)
