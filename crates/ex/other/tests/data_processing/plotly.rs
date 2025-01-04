7269006b2393c01ed3bd3e226b1c3f5881cc516b
John <john-cd@users.noreply.github.com>
2025-01-04 10:58:45 +0000

    create plotly example (WIP) (#884)

--- crates/ex/other/tests/data_processing/plotly.rs
+++ crates/ex/other/tests/data_processing/plotly.rs
@@ -1,12 +1,31 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use plotly::common::Mode;
+// use plotly::Plot;
+// use plotly::Scatter;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/884)
+// fn main() {
+//     // Create sample data
+//     let x_data = vec![1, 2, 3, 4, 5];
+//     let y_data = vec![10, 15, 13, 17, 14];
+
+//     // Create a Scatter plot
+//     let trace = Scatter::new(x_data, y_data)
+//         .mode(Mode::LinesMarkers)
+//         .name("Sample Data");
+
+//     // Create a Plot and add the Scatter plot
+//     let mut plot = Plot::new();
+//     plot.add_trace(trace);
+
+//     // Save the plot as an HTML file
+//     plot.write_html("plot.html");
+
+//     println!("Plot saved as plot.html");
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/884
