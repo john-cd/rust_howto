2a8185997dc9f708038c71d1cdaf9e65c1067c39
John <john-cd@users.noreply.github.com>
2025-01-04 10:45:44 +0000

    add serde_ignored example (WIP) (#756)

--- crates/ex/categories/efghijkl/tests/encoding/serde_ignored.rs
+++ crates/ex/categories/efghijkl/tests/encoding/serde_ignored.rs
@@ -1,12 +1,63 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P0](https://github.com/john-cd/rust_howto/issues/756)
+// // `serde_ignored` is a crate that helps you handle unknown fields when
+// // deserializing with `serde`. It's useful when you want to ignore fields that are
+// // present in the input but not defined in your struct.
+
+// // In `Cargo.toml`:
+// // [dependencies]
+// // serde = { version = "1.0", features = ["derive"] }
+// // serde_derive = "1.0"
+// // serde_ignored = "0.1"
+// // serde_json = "1.0"
+
+// use serde::Deserialize;
+// use serde_ignored::Path;
+// use serde_json::Value;
+
+// #[derive(Deserialize, Debug)]
+// struct MyStruct {
+//     field1: i32,
+//     field2: String,
+// }
+
+// fn main() {
+//     let json_data = r#"
+//     {
+//         "field1": 42,
+//         "field2": "Hello, world!",
+//         "unknown_field": "This will be ignored",
+//         "another_unknown": 123
+//     }
+//     "#;
+
+//     // Create a vector to store ignored fields
+//     let mut ignored_fields = Vec::new();
+
+//     // Deserialize the JSON data, collecting ignored fields
+//     let result: Result<MyStruct, _> = serde_ignored::deserialize(
+//         &mut serde_json::Deserializer::from_str(json_data),
+//         |path: Path| {
+//             println!("Ignored field: {}", path);
+//             ignored_fields.push(path.to_string());
+//         },
+//     );
+
+//     // Check the result of deserialization
+//     match result {
+//         Ok(my_struct) => {
+//             println!("Deserialized struct: {:?}", my_struct);
+//             println!("Ignored fields: {:?}", ignored_fields);
+//         }
+//         Err(e) => {
+//             println!("Error deserializing: {}", e);
+//         }
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P0](https://github.com/john-cd/rust_howto/issues/756)
