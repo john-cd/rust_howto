bc548455ed81cc6efb0305c4e5cef6c181351f9a
John <john-cd@users.noreply.github.com>
2025-01-04 10:40:13 +0000

    add proc_macro2 example (WIP) (#741)

--- crates/ex/categories/d/tests/development_tools_procedural_macro_helpers/proc_macro2.rs
+++ crates/ex/categories/d/tests/development_tools_procedural_macro_helpers/proc_macro2.rs
@@ -1,12 +1,48 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// // `proc-macro2` is a library for working with Rust's procedural macros.
+// // We create below a simple procedural macro that
+// // generates code to implement the `Debug` trait for a struct.
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/741)
+// // In `Cargo.toml`
+// // [dependencies]
+// // proc-macro2 = "1.0"
+// // quote = "1.0"
+// // syn = { version = "1.0", features = ["full"] }
+
+// // [lib]
+// // proc-macro = true
+
+// use proc_macro::TokenStream;
+// use proc_macro2::TokenStream as TokenStream2;
+// use quote::quote;
+// use syn::DeriveInput;
+// use syn::parse_macro_input;
+
+// #[proc_macro_derive(Debug)]
+// pub fn derive_debug(input: TokenStream) -> TokenStream {
+//     // Parse the input TokenStream into a DeriveInput
+//     let input = parse_macro_input!(input as DeriveInput);
+//     // Generate the implementation
+//     let expanded = impl_debug(&input);
+//     // Return the generated code as a TokenStream
+//     TokenStream::from(expanded)
+// }
+
+// fn impl_debug(input: &DeriveInput) -> TokenStream2 {
+//     let name = &input.ident;
+//     let expanded = quote! {
+//         impl std::fmt::Debug for #name {
+//             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) ->
+// std::fmt::Result {                 write!(f, "{} {{ ... }}",
+// stringify!(#name))             }
+//         }
+//     };
+//     expanded
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/741)
