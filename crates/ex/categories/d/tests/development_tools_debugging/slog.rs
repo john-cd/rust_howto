38bf8bc0adfd0f1a90430947e01e157168a5dfe6
John <john-cd@users.noreply.github.com>
2025-01-04 10:34:40 +0000

    add slog example (WIP) (#735)

--- crates/ex/categories/d/tests/development_tools_debugging/slog.rs
+++ crates/ex/categories/d/tests/development_tools_debugging/slog.rs
@@ -1,12 +1,44 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use slog::Drain;
+// use slog::Logger;
+// use slog::o;
+// use slog_async;
+// use slog_term;
+// // `slog` is a structured, composable logging framework.
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/735)
+// // slog = "2.7"
+// // slog-async = "2.7"
+// // slog-term = "2.8"
+// // slog-scope = "4.4"
+
+// fn main() {
+//     // Create a terminal decorator
+//     let decorator = slog_term::TermDecorator::new().build();
+//     // Create a terminal drain
+//     let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+//     // Create an async drain
+//     let drain = slog_async::Async::new(drain).build().fuse();
+//     // Create a root logger
+//     let root_logger =
+//         Logger::root(drain, o!("version" => env!("CARGO_PKG_VERSION")));
+
+//     // Log some messages
+//     slog::info!(root_logger, "Logging with slog"; "example" => "simple");
+//     slog::warn!(root_logger, "A warning message");
+//     slog::error!(root_logger, "An error occurred"; "code" => 500);
+
+//     // Do some work with logging
+//     perform_some_logging(root_logger.clone());
+// }
+
+// fn perform_some_logging(logger: Logger) {
+//     slog::info!(logger, "Performing some work"; "task" => "example task");
+//     // More work...
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/735)
