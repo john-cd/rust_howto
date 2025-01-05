138bcfa13cbf5ee0ccb4bf13af7ec93d8e667192
John <john-cd@users.noreply.github.com>
2025-01-05 14:14:10 +0000

    add libc example (#816)

--- crates/ex/categories/opqr/tests/os/libc.rs
+++ crates/ex/categories/opqr/tests/os/libc.rs
@@ -1,11 +1,26 @@
 // ANCHOR: example
+use std::ffi::CString;
+
+use libc::c_char;
+
+// Declare an external C function puts that takes a pointer to a c_char (C
+// string) and returns an i32.
+unsafe extern "C" {
+    fn puts(s: *const c_char) -> i32;
+}
+
 fn main() {
-    todo!();
+    // create a CString containing the message we want to print and call the
+    // puts function inside an unsafe block, since interacting with C code is
+    // considered unsafe in Rust.
+    let c_string = CString::new("Hello from C!").expect("CString::new failed");
+    unsafe {
+        puts(c_string.as_ptr());
+    }
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
