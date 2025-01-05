7ff4882f54344af847d2ffaf494de46867cbd066
John <john-cd@users.noreply.github.com>
2025-01-05 16:21:49 +0000

    add num_bigint example; close #798

--- crates/ex/categories/mn/tests/mathematics/num_bigint.rs
+++ crates/ex/categories/mn/tests/mathematics/num_bigint.rs
@@ -1,12 +1,37 @@
 // ANCHOR: example
+use std::str::FromStr;
+
+use num_bigint::BigInt;
+use num_traits::One;
+
 fn main() {
-    todo!();
+    // Parse big integers from strings
+    let a = BigInt::from_str("123456789012345678901234567890").unwrap();
+    let b = BigInt::from_str("987654321098765432109876543210").unwrap();
+
+    // Add
+    let sum = &a + &b;
+    println!("Sum: {}", sum);
+
+    // Subtract
+    let difference = &b - &a;
+    println!("Difference: {}", difference);
+
+    // Multiply
+    let product = &a * &b;
+    println!("Product: {}", product);
+
+    // Divide
+    let quotient = &b / &a;
+    println!("Quotient: {}", quotient);
+
+    // Create a big integer with value 1
+    let one = BigInt::one();
+    println!("One: {}", one);
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
-// [P1](https://github.com/john-cd/rust_howto/issues/798)
