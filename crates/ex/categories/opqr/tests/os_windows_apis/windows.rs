41ddc277ec1128648ed61bc9ac782e1dbc78d847
John <john-cd@users.noreply.github.com>
2025-01-06 20:36:12 +0000

    add windows example (WIP) (#823)

--- crates/ex/categories/opqr/tests/os_windows_apis/windows.rs
+++ crates/ex/categories/opqr/tests/os_windows_apis/windows.rs
@@ -1,11 +1,22 @@
 // ANCHOR: example
+use windows::Win32::Foundation::{HWND, PWSTR};
+use windows::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_OK};
+
+// Simple Windows application that displays a message box saying "Hello, windows!".
 fn main() {
-    todo!();
+    unsafe {
+        MessageBoxW(
+            HWND(0),
+            PWSTR("Hello, windows!".encode_utf16().collect::<Vec<u16>>().as_mut_ptr()),
+            PWSTR("Greetings".encode_utf16().collect::<Vec<u16>>().as_mut_ptr()),
+            MB_OK,
+        );
+    }
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
+#[ignore = "WIP"]
 fn test() {
     main();
 }
