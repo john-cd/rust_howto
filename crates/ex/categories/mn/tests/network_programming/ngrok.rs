3f0dcd5e4a24dfdaf12ff04e613f0fabba018ca2
John <john-cd@users.noreply.github.com>
2025-01-07 14:30:05 +0000

    ngrok example WIP (#811)

--- crates/ex/categories/mn/tests/network_programming/ngrok.rs
+++ crates/ex/categories/mn/tests/network_programming/ngrok.rs
@@ -1,12 +1,39 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use std::convert::Infallible;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P2](https://github.com/john-cd/rust_howto/issues/811)
+// use hyper::Request;
+// use hyper::Response;
+// use hyper::Server;
+// use hyper::body::Body;
+// use hyper::service::service_fn;
+// use hyper::service_fn::make_service_fn;
+// use tokio::runtime;
+
+// async fn handle_request(
+//     _req: Request<Body>,
+// ) -> Result<Response<Body>, Infallible> {
+//     Ok(Response::new(Body::from("Hello, ngrok!")))
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let make_svc = make_service_fn(|_conn| async {
+//         Ok::<_, Infallible>(service_fn(handle_request))
+//     });
+
+//     let addr = ([127, 0, 0, 1], 3000).into();
+//     let server = Server::bind(&addr).serve(make_svc);
+
+//     println!("Server running on http://{}", addr);
+//     if let Err(e) = server.await {
+//         eprintln!("server error: {}", e);
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [P2](https://github.com/john-cd/rust_howto/issues/811)
