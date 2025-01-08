08eef25c2b242612bf30ba0e3126764889ee8bb2
John CD <john-cd@users.noreply.github.com>
2025-01-08 23:23:34 +0100

    add ureq example (WIP) (#862)

--- crates/ex/categories/wxyz/tests/web_programming_http_client/ureq.rs
+++ crates/ex/categories/wxyz/tests/web_programming_http_client/ureq.rs
@@ -1,12 +1,18 @@
 // ANCHOR: example
-fn main() {
-    todo!();
+use ureq::Error;
+
+fn main() -> Result<(), Error> {
+    let url = "https://jsonplaceholder.typicode.com/posts/1";
+    let response: String = ureq::get(url).call()?.into_string()?;
+
+    println!("Response: {}", response);
+    Ok(())
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
+fn test() -> Result<(), Error> {
+    main()?;
+    Ok(())
 }
 // [P1](https://github.com/john-cd/rust_howto/issues/862)
