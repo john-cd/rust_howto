ba527e18ba315b2b2379d78ec1eed7db840dffd4
John CD <john-cd@users.noreply.github.com>
2025-01-09 00:09:28 +0100

    add winit example (WIP) (#794)

--- crates/ex/categories/efghijkl/tests/gui/winit.rs
+++ crates/ex/categories/efghijkl/tests/gui/winit.rs
@@ -1,12 +1,38 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use winit::event::Event;
+// use winit::event::WindowEvent;
+// use winit::event_loop::ControlFlow;
+// use winit::event_loop::EventLoop;
+// use winit::window::WindowBuilder;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/794)
+// fn main() {
+//     // Create an event loop
+//     let event_loop = EventLoop::new();
+
+//     // Build the window
+//     let window = WindowBuilder::new()
+//         .with_title("Hello, winit!")
+//         .build(&event_loop)
+//         .unwrap();
+
+//     // Run the event loop
+//     event_loop.run(move |event, _, control_flow| {
+//         *control_flow = ControlFlow::Wait;
+
+//         match event {
+//             Event::WindowEvent {
+//                 event: WindowEvent::CloseRequested,
+//                 ..
+//             } => *control_flow = ControlFlow::Exit,
+//             _ => (),
+//         }
+//     });
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "WIP"]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/794)
