878bae94ee8f0f2baa007a3e7a1f880604604386
John CD <john-cd@users.noreply.github.com>
2025-01-08 18:01:49 +0100

    hide tonic build.rs given lack of protoc (#870)

--- crates/ex/categories/wxyz/build.rs
+++ crates/ex/categories/wxyz/build.rs
@@ -1,5 +1,5 @@
 // ANCHOR: example
 fn main() {
-    tonic_build::compile_protos("proto/helloworld.proto").unwrap();
+// TODO install protoc   tonic_build::compile_protos("proto/helloworld.proto").unwrap();
 }
 // ANCHOR_END: example
