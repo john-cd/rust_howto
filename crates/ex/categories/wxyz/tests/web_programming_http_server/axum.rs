8ecfd0eb3b4d7a41592e9094824c71907336f8a3
John CD <john-cd@users.noreply.github.com>
2025-01-09 00:15:05 +0100

    add axum example (WIP) (#865)

--- crates/ex/categories/wxyz/tests/web_programming_http_server/axum.rs
+++ crates/ex/categories/wxyz/tests/web_programming_http_server/axum.rs
@@ -1,12 +1,31 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use std::net::SocketAddr;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P0](https://github.com/john-cd/rust_howto/issues/865)
+// use axum::Router;
+// use axum::handler::get;
+
+// async fn hello() -> &'static str {
+//     "Hello, world!"
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     // build our application with a route
+//     let app = Router::new().route("/", get(hello));
+
+//     // run it
+//     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+//     println!("Listening on {}", addr);
+//     axum::Server::bind(&addr)
+//         .serve(app.into_make_service())
+//         .await
+//         .unwrap();
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [P0](https://github.com/john-cd/rust_howto/issues/865)
