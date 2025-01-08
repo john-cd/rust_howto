e3013f425dda0dc0623a3e8031f2e8c68a012656
John CD <john-cd@users.noreply.github.com>
2025-01-08 16:28:59 +0100

    hide windows example

--- crates/ex/categories/opqr/tests/os_windows_apis/windows.rs
+++ crates/ex/categories/opqr/tests/os_windows_apis/windows.rs
@@ -1,36 +1,36 @@
-// ANCHOR: example
-use windows::Win32::Foundation::HWND;
-//use windows::Win32::Foundation::PWSTR;
-use windows::Win32::UI::WindowsAndMessaging::MB_OK;
-use windows::Win32::UI::WindowsAndMessaging::MessageBoxW;
+// // ANCHOR: example
+// use windows::Win32::Foundation::HWND;
+// //use windows::Win32::Foundation::PWSTR;
+// use windows::Win32::UI::WindowsAndMessaging::MB_OK;
+// use windows::Win32::UI::WindowsAndMessaging::MessageBoxW;
 
-// Simple Windows application that displays a message box saying "Hello,
-// windows!".
-fn main() {
-    unsafe {
-        MessageBoxW(
-            HWND(0),
-            PWSTR(
-                "Hello, windows!"
-                    .encode_utf16()
-                    .collect::<Vec<u16>>()
-                    .as_mut_ptr(),
-            ),
-            PWSTR(
-                "Greetings"
-                    .encode_utf16()
-                    .collect::<Vec<u16>>()
-                    .as_mut_ptr(),
-            ),
-            MB_OK,
-        );
-    }
-}
-// ANCHOR_END: example
+// // Simple Windows application that displays a message box
+// // saying "Hello, windows!".
+// fn main() {
+//     unsafe {
+//         MessageBoxW(
+//             HWND(0),
+//             PWSTR(
+//                 "Hello, windows!"
+//                     .encode_utf16()
+//                     .collect::<Vec<u16>>()
+//                     .as_mut_ptr(),
+//             ),
+//             PWSTR(
+//                 "Greetings"
+//                     .encode_utf16()
+//                     .collect::<Vec<u16>>()
+//                     .as_mut_ptr(),
+//             ),
+//             MB_OK,
+//         );
+//     }
+// }
+// // ANCHOR_END: example
 
-#[test]
-#[ignore = "WIP"]
-fn test() {
-    main();
-}
-// [P2](https://github.com/john-cd/rust_howto/issues/823)
+// #[test]
+// #[ignore = "WIP"]
+// fn test() {
+//     main();
+// }
+// // [P2](https://github.com/john-cd/rust_howto/issues/823)
