aab9eb63ba7b9d5e444105c0c9539d7ed549d15e
John <john-cd@users.noreply.github.com>
2025-01-08 10:30:40 +0000

    add 'open' example

--- crates/ex/categories/efghijkl/tests/filesystem/open.rs
+++ crates/ex/categories/efghijkl/tests/filesystem/open.rs
@@ -1,12 +1,33 @@
 // ANCHOR: example
+use open::that;
+
 fn main() {
-    todo!();
+    // Open a file using the default program
+    if let Err(e) = that("example.txt") {
+        eprintln!("Failed to open file: {}", e);
+    }
+
+    // Open a URL using the default web browser
+    if let Err(e) = that("https://www.rust-lang.org") {
+        eprintln!("Failed to open URL: {}", e);
+    }
+    // OR: open::with("http://rust-lang.org", "firefox")?;
+
+    // Depending on the platform and system configuration, launchers can block.
+    // If you want to be sure they don’t, use that_in_background() or
+    // that_detached instead.
+
+    // Get the commands to try open the path with.
+    let path = "http://rust-lang.org";
+    open::commands(path)
+        .iter()
+        .for_each(|x| println!("{:?}", x));
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
+#[ignore = "WIP"]
 fn test() {
     main();
 }
-// TODO P1
+// TODO P1 how to test properly?
