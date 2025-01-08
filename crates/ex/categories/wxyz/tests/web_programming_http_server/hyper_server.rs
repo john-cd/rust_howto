7483f8efaa82c228bd0c242de8c3af5ea5d6fd0a
John CD <john-cd@users.noreply.github.com>
2025-01-09 00:13:53 +0100

    add hyper server example (WIP) (#866)

--- crates/ex/categories/wxyz/tests/web_programming_http_server/hyper_server.rs
+++ crates/ex/categories/wxyz/tests/web_programming_http_server/hyper_server.rs
@@ -1,12 +1,39 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use std::convert::Infallible;
+// use std::net::SocketAddr;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P0](https://github.com/john-cd/rust_howto/issues/866)
+// use hyper::body;
+// use hyper::Request;
+// use hyper::Response;
+// use hyper::server;
+// use hyper::service::make_service_fn;
+// use hyper::service::service_fn;
+
+// async fn hello_world(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+//     Ok(Response::new(Body::from("Hello, world!")))
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+//     let make_svc = make_service_fn(|_conn| async {
+//         Ok::<_, Infallible>(service_fn(hello_world))
+//     });
+
+//     let server = Server::bind(&addr).serve(make_svc);
+
+//     println!("Listening on http://{}", addr);
+
+//     if let Err(e) = server.await {
+//         eprintln!("Server error: {}", e);
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "WIP"]
+// fn test() {
+//     main();
+// }
+// // [P0](https://github.com/john-cd/rust_howto/issues/866)
