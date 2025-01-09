2d383ae371466bad8c00171cddb5fb706b5975fe
John CD <john-cd@users.noreply.github.com>
2025-01-10 00:48:30 +0100

    add example draft for AWS SDK (WIP) (#879)

--- crates/ex/other/tests/cloud/aws_sdk.rs
+++ crates/ex/other/tests/cloud/aws_sdk.rs
@@ -1,12 +1,50 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use aws_config::meta::region::RegionProviderChain;
+// use aws_sdk_s3::Client;
+// use aws_sdk_s3::Error;
+// use tokio;
+// use tracing::error;
+// use tracing::info;
+// use tracing_subscriber;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/879)
+// // List the contents of an S3 bucket
+
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     // Initialize tracing subscriber
+//     tracing_subscriber::fmt::init();
+
+//     // Load AWS configuration
+//     let region_provider =
+//         RegionProviderChain::default_provider().or_else("us-west-2");
+//     let version: BehaviorVersion;
+//     let config = aws_config::load_defaults(version).await;
+//     // TODO .region(region_provider);
+//     let client = Client::new(&config);
+
+//     // List objects in the S3 bucket.
+//     let bucket_name = "your-bucket-name";
+//     let result = client.list_objects_v2().bucket(bucket_name).send().await;
+
+//     match result {
+//         Ok(output) => {
+//             if let Some(objects) = output.contents {
+//                 for object in objects {
+//                     info!("Object key: {}", object.key.unwrap_or_default());
+//                 }
+//             }
+//         }
+//         Err(e) => {
+//             error!("Failed to list objects: {}", e);
+//         }
+//     }
+
+//     Ok(())
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/879)
