206a82bd89ad4305e984e1a5ba8fa9cfed8b5d60
John CD <john-cd@users.noreply.github.com>
2025-01-09 10:24:29 +0100

    add actix_web example (WIP) (#863)

--- crates/ex/categories/wxyz/tests/web_programming_http_server/actix_web.rs
+++ crates/ex/categories/wxyz/tests/web_programming_http_server/actix_web.rs
@@ -1,12 +1,27 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use actix_web::App;
+// use actix_web::HttpResponse;
+// use actix_web::HttpServer;
+// use actix_web::Responder;
+// use actix_web::web;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/863)
+// async fn greet() -> impl Responder {
+//     HttpResponse::Ok().body("Hello, world!")
+// }
+
+// #[tokio::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| App::new().route("/", web::get().to(greet)))
+//         .bind("127.0.0.1:8080")?
+//         .run()
+//         .await
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "WIP"]
+// fn test() -> std::io::Result<()> {
+//     main()?;
+//     Ok(())
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/863)
