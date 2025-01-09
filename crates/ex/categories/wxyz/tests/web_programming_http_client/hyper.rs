ff7f24d19676fd38fbe0550ee7bcbb38a5f58c06
John CD <john-cd@users.noreply.github.com>
2025-01-09 18:32:12 +0100

    add hyper client example (WIP) (#859)

--- crates/ex/categories/wxyz/tests/web_programming_http_client/hyper.rs
+++ crates/ex/categories/wxyz/tests/web_programming_http_client/hyper.rs
@@ -1,13 +1,44 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use hyper::Client;
+// use hyper::Request;
+// use hyper::body::Body;
+// use hyper::http::response::Response;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P0](https://github.com/john-cd/rust_howto/issues/859)
-// dupe?
+// async fn fetch_url(url: &str) -> Result<Response<Body>, hyper::Error> {
+//     let client = Client::new();
+
+//     // Build the request
+//     let req = Request::builder()
+//         .method("GET")
+//         .uri(url)
+//         .header("User-Agent", "hyper")
+//         .body(Body::empty())
+//         .expect("Request::builder failed!");
+
+//     // Send the request and wait for the response
+//     let res = client.request(req).await?;
+
+//     Ok(res)
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let url = "http://example.com";
+
+//     match fetch_url(url).await {
+//         Ok(response) => {
+//             println!("Response: {:?}", response);
+//         }
+//         Err(e) => {
+//             eprintln!("Error: {:?}", e);
+//         }
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P0](https://github.com/john-cd/rust_howto/issues/859)
+// // TODO https://hyper.rs/
