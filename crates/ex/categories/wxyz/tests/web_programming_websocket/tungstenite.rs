decbc0d41bc9a4b4b82d5062bbda01a73833f9db
John CD <john-cd@users.noreply.github.com>
2025-01-09 12:43:32 +0100

    add tungstenite example (WIP) (#874)

--- crates/ex/categories/wxyz/tests/web_programming_websocket/tungstenite.rs
+++ crates/ex/categories/wxyz/tests/web_programming_websocket/tungstenite.rs
@@ -1,12 +1,38 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use tungstenite::Message;
+// use tungstenite::connect;
+// use url::Url;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/874)
+// // Lightweight, flexible WebSockets for Rust
+
+// fn main() -> anyhow::Result<()> {
+//     // Connect to the WebSocket server
+//     let (mut socket, response) =
+//         connect(Url::parse("ws://echo.websocket.org")?)?;
+
+//     println!("Connected to the server");
+//     println!("Response HTTP code: {}", response.status());
+//     println!("Response contains the following headers:");
+//     for (ref header, _value) in response.headers() {
+//         println!("* {}", header);
+//     }
+
+//     // Send a message to the server
+//     socket.send(Message::Text("Hello WebSocket".into()))?;
+//     println!("Message sent");
+
+//     // Read a message from the server
+//     let msg = socket.read()?;
+//     println!("Received: {}", msg);
+
+//     Ok(())
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "WIP"]
+// fn test()  -> anyhow::Result<()> {
+//     main()?;
+//     Ok(())
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/874)
