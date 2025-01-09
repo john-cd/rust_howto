3af153267b6b6cb04d8c38850002c2209964b1d4
John CD <john-cd@users.noreply.github.com>
2025-01-10 00:29:50 +0100

    add example stub for aws lambda (#878)

--- crates/ex/other/tests/cloud/aws_lambda.rs
+++ crates/ex/other/tests/cloud/aws_lambda.rs
@@ -1,12 +1,52 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use lambda_runtime::Context;
+// use lambda_runtime::Error;
+// // use lambda_runtime::LambdaEvent;
+// use lambda_runtime::service_fn;
+// use serde::Deserialize;
+// use serde::Serialize;
+// // use serde_json::json;
+// // use tracing::{error, info};
+// use tracing_subscriber;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/878)
+// // Install AWS CLI
+// // Install AWS SAM CLI
+// // cargo build --release --target x86_64-unknown-linux-musl
+// // sam build
+// // sam deploy --guided
+
+// #[derive(Deserialize)]
+// struct Request {
+//     name: String,
+// }
+
+// #[derive(Serialize)]
+// struct Response {
+//     message: String,
+// }
+
+// async fn function_handler(
+//     event: Request,
+//     _: Context,
+// ) -> Result<Response, Error> {
+//     Ok(Response {
+//         message: format!("Hello, {}!", event.name),
+//     })
+// }
+
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     // Initialize tracing subscriber
+//     tracing_subscriber::fmt::init();
+
+//     let func = service_fn(function_handler);
+//     lambda_runtime::run(func).await?;
+//     Ok(())
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/878)
