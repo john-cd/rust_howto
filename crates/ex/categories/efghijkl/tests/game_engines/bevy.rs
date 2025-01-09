ef952b19751ae3199cfa60f0e014e213783f8c2a
John CD <john-cd@users.noreply.github.com>
2025-01-10 00:11:09 +0100

    add stubs for bevy, dioxus, iced examples (#767) (#775) (#781)

--- crates/ex/categories/efghijkl/tests/game_engines/bevy.rs
+++ crates/ex/categories/efghijkl/tests/game_engines/bevy.rs
@@ -1,12 +1,42 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use bevy::prelude::*;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P2](https://github.com/john-cd/rust_howto/issues/767)
+// // Sets up a basic Bevy application with a 2D camera and a simple system that
+// // moves a sprite horizontally across the screen.
+
+// fn main() {
+//     // Creates a new Bevy app.
+//     App::build()
+//         .add_plugins(DefaultPlugins) // Adds the default plugins, including
+// window, input, and rendering.         .add_startup_system(setup.system()) //
+// Adds the setup system to run once at the start.
+//         .add_system(move_system.system()) // Adds the move_system to run
+// every frame.         .run();
+// }
+// fn setup(mut commands: Commands) {
+//     // commands.spawn_bundle(OrthographicCameraBundle::new_2d()); // Sets up
+// a     // 2D camera.
+//     commands.spawn_bundle(Sprite {
+//         // Spawns a simple sprite.
+//         transform: Transform {
+//             translation: Vec3::new(0.0, 0.0, 0.0),
+//             scale: Vec3::splat(0.5),
+//             ..Default::default()
+//         },
+//         ..Default::default()
+//     });
+// }
+
+// fn move_system(mut query: Query<&mut Transform>) {
+//     for mut transform in query.iter_mut() {
+//         transform.translation.x += 0.1;
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "WIP"]
+// fn test() {
+//     main();
+// }
+// // [P2](https://github.com/john-cd/rust_howto/issues/767)
