5de1a2461d75f3a9f5dee1816badaaf2251c9514
John <john-cd@users.noreply.github.com>
2025-01-10 10:05:53 +0000

    add pingora example (WIP) (#812)

--- crates/ex/categories/mn/tests/network_programming/pingora.rs
+++ crates/ex/categories/mn/tests/network_programming/pingora.rs
@@ -1,12 +1,29 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use pingora::prelude::*;
+// use std::net::SocketAddr;
+// use pingora::server::Server;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P2](https://github.com/john-cd/rust_howto/issues/812)
+// // Pingora is a high-performance and low-level HTTP library in Rust.
+// // Http 1.x and Http 2
+// // Modern TLS with OpenSSL or BoringSSL (FIPS compatible)
+// // Zero downtime upgrade
+
+// async fn handle_request(_: Request) -> Response {
+//     Response::new(200, "Hello, world!".into())
+// }
+
+// // Create a basic HTTP server that responds with "Hello, world!".
+// #[tokio::main]
+// async fn main() {
+//     let addr: SocketAddr = "127.0.0.1:8080".parse().expect("Unable to parse
+// socket address");     let server = Server::bind(&addr).serve(handle_request);
+//     println!("Listening on http://{}", addr);
+//     server.await.expect("Server failed");
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [P2](https://github.com/john-cd/rust_howto/issues/812)
