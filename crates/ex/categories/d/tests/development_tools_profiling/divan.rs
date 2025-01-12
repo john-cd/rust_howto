fb91b63f9738d03113647c8bf7cd84ce1d3b38ef
John <john-cd@users.noreply.github.com>
2025-01-12 13:36:01 +0000

    add divan example draft (#747)

--- crates/ex/categories/d/tests/development_tools_profiling/divan.rs
+++ crates/ex/categories/d/tests/development_tools_profiling/divan.rs
@@ -1,12 +1,34 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// fn main() {
+//     // Run registered benchmarks.
+//     divan::main();
+// }
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/747)
+// // Define a `fibonacci` function and
+// // register it for benchmarking.
+// #[divan::bench]
+// // #[divan::bench(
+// //    max_time = 0.001, // seconds
+// //    sample_size = 64, // 64 × 84 = 5376
+// //)]
+// fn fibonacci() -> u64 {
+//     fn compute(n: u64) -> u64 {
+//         if n <= 1 {
+//             1
+//         } else {
+//             compute(n - 2) + compute(n - 1)
+//         }
+//     }
+
+//     compute(divan::black_box(10))
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "not yet implemented"]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/747)
+// // TODO rewrite example
+// // https://nikolaivazquez.com/blog/divan/
