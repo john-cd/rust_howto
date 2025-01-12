e69f032ec33d217bac6327120dd033327ad79369
John <john-cd@users.noreply.github.com>
2025-01-12 13:21:56 +0000

    add slint example (#787)

--- crates/ex/categories/efghijkl/tests/gui/slint.rs
+++ crates/ex/categories/efghijkl/tests/gui/slint.rs
@@ -1,12 +1,22 @@
 // ANCHOR: example
-fn main() {
-    todo!();
+
+slint::slint!{
+    export component HelloWorld {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    HelloWorld::new()?.run()?;
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
 }
 // [P1](https://github.com/john-cd/rust_howto/issues/787)
