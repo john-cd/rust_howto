e9ad6b54ce7a1892153e8d25eb7fd34ec87ed6b0
John <john-cd@users.noreply.github.com>
2025-01-13 19:16:01 +0000

    add TODO

--- crates/ex/categories/wxyz/tests/wasm/wasmtime.rs
+++ crates/ex/categories/wxyz/tests/wasm/wasmtime.rs
@@ -10,3 +10,5 @@ fn test() {
     main();
 }
 // [P1](https://github.com/john-cd/rust_howto/issues/855)
+// TODO review https://docs.wasmtime.dev/introduction.html
+// https://docs.rs/wasmtime/latest/wasmtime/
