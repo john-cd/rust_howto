209de49edb4f0a9161ed1ef4d7bbe8c00ddc68ff
John <john-cd@users.noreply.github.com>
2025-01-13 16:01:16 +0000

    fix slint example; remove test for now (#787)

--- crates/ex/categories/efghijkl/tests/gui/slint.rs
+++ crates/ex/categories/efghijkl/tests/gui/slint.rs
@@ -1,7 +1,7 @@
 // ANCHOR: example
 
 slint::slint! {
-    export component HelloWorld {
+    export component HelloWorld inherits Window {
         Text {
             text: "hello world";
             color: green;
@@ -13,12 +13,14 @@ fn main() -> anyhow::Result<()> {
     // `run` is a convenience function that first calls Self::show, followed by
     // crate::run_event_loop() and Self::hide.
     HelloWorld::new()?.run()?;
+    Ok(())
 }
 // ANCHOR_END: example
 
-#[test]
-fn test() -> anyhow::Result<()> {
-    main()?;
-    Ok(())
-}
+// #[test]
+// fn test() -> anyhow::Result<()> {
+//     main()?;
+//     Ok(())
+// }
 // [P1](https://github.com/john-cd/rust_howto/issues/787)
+// TODO figure how to test - neither WAYLAND_DISPLAY nor WAYLAND_SOCKET nor DISPLAY is set.
