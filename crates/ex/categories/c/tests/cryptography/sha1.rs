7d0bcf3c46c177e1ad1595ac47c4774b89397c88
John CD <john-cd@users.noreply.github.com>
2025-01-17 00:49:29 +0100

    early draft of sha1 example (#702)

--- crates/ex/categories/c/tests/cryptography/sha1.rs
+++ crates/ex/categories/c/tests/cryptography/sha1.rs
@@ -1,12 +1,32 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use hex_literal::hex;
+// use sha1::Digest;
+// use sha1::Sha1;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// [P1](https://github.com/john-cd/rust_howto/issues/702)
+// SHA-1 is considered cryptographically broken and should not be used for new
+// security-critical applications. It is primarily used for legacy compatibility
+// or non-security-sensitive purposes.
+
+// fn main() {
+//     // Create a Sha1 object
+//     let mut hasher = Sha1::new();
+
+//     // Process input message
+//     hasher.update(b"hello world");
+
+//     // Acquire hash digest
+//     let result = hasher.finalize();
+
+//     // Assert the expected hash value
+//     assert_eq!(result[..], hex!("2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"));
+
+//     println!("SHA-1 hash of 'hello world': {:x}", result);
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// #[ignore = "WIP"]
+// fn test() {
+//     main();
+// }
+// // [P1](https://github.com/john-cd/rust_howto/issues/702)
