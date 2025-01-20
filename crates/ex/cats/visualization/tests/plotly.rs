c98e7f73f181a0d25e74127b94edcf1876be23a1
John <john-cd@users.noreply.github.com>
2025-01-20 18:22:00 +0000

    fix plotly example: make sure the folder exists

--- crates/ex/cats/visualization/tests/plotly.rs
+++ crates/ex/cats/visualization/tests/plotly.rs
@@ -1,9 +1,12 @@
 // ANCHOR: example
+use std::fs;
+
+use plotly::Layout;
 use plotly::Plot;
 use plotly::Scatter;
 use plotly::common::Mode;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // Create sample data
     let x_data = vec![1, 2, 3, 4, 5];
     let y_data = vec![10, 15, 13, 17, 14];
@@ -14,18 +17,30 @@ fn main() {
         .name("Sample Data");
 
     // Create a Plot and add the Scatter plot
+    // Plot is a container for structs that implement the Trace trait.
+    // Optionally a Layout can also be specified. Its function is to serialize
+    // Traces and the Layout in html format and display and/or persist the
+    // resulting plot.
     let mut plot = Plot::new();
     plot.add_trace(trace);
 
+    let layout = Layout::new().title("<b>Scatter Plot</b>");
+    plot.set_layout(layout);
+
+    if !fs::exists("temp")? {
+        fs::create_dir("temp")?;
+    }
     // Save the plot as an HTML file
     plot.write_html("temp/plot.html");
 
     println!("Plot saved as plot.html");
+    Ok(())
 }
 // ANCHOR_END: example
 
 #[test]
-fn test() {
-    main();
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
 }
 // [P1](https://github.com/john-cd/rust_howto/issues/884
