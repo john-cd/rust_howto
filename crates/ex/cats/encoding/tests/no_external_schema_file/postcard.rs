d7ebb526f72fe5e0a635e981ab6e43dd83ccdd5b
John <john-cd@users.noreply.github.com>
2025-01-23 18:01:35 +0000

    add postcard example; upgrade flatbuffers, prototbuf; cargo update

--- crates/ex/cats/encoding/tests/no_external_schema_file/postcard.rs
+++ crates/ex/cats/encoding/tests/no_external_schema_file/postcard.rs
@@ -1,11 +1,55 @@
-// ANCHOR: example
+// ANCHOR: exampleuse core::ops::Deref;
+use std::ops::Deref;
+
+use heapless::Vec;
+use postcard::from_bytes;
+use postcard::to_vec;
+use serde::Deserialize;
+use serde::Serialize;
+
+// [dependencies]
+// postcard = "1.0.0"
+
+// # By default, `serde` has the `std` feature enabled, which makes it
+// unsuitable for embedded targets # disabling default-features fixes this
+// serde = { version = "1.0.*", default-features = false }
+
+// Example from https://lib.rs/crates/postcard
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+struct RefStruct<'a> {
+    bytes: &'a [u8],
+    str_s: &'a str,
+}
+
 fn main() {
-    todo!();
+    let message = "hElLo";
+
+    let bytes = [0x01, 0x10, 0x02, 0x20];
+
+    let output: Vec<u8, 11> = to_vec(&RefStruct {
+        bytes: &bytes,
+        str_s: message,
+    })
+    .unwrap();
+
+    assert_eq!(
+        &[
+            0x04, 0x01, 0x10, 0x02, 0x20, 0x05, b'h', b'E', b'l', b'L', b'o',
+        ],
+        output.deref()
+    );
+
+    let out: RefStruct = from_bytes(output.deref()).unwrap();
+    assert_eq!(out, RefStruct {
+        bytes: &bytes,
+        str_s: message,
+    });
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
+// #[ignore = "WIP"]
 fn test() {
     main();
 }
