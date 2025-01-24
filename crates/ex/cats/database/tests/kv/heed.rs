2319b46b768515bc6ae2b2feafe014c9ac6596d8
John <john-cd@users.noreply.github.com>
2025-01-24 17:57:33 +0000

    add stub for heed example

---
+++ crates/ex/cats/database/tests/kv/heed.rs
@@ -0,0 +1,16 @@
+// ANCHOR: example
+
+// A fully typed LMDB (mdb.master) wrapper with minimum overhead
+// Lightning Memory-Mapped Database (LMDB) is an embedded transactional database in the form of a key-value store. 
+fn main() -> anyhow::Result<()> {
+    Ok(())
+}
+// ANCHOR_END: example
+
+#[test]
+#[ignore = "not implemented yet"]
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
+// TODO P2 write heed example
