cb3ee7ff5865268f9a88fcb4651482fc394b1b81
John <john-cd@users.noreply.github.com>
2025-01-24 19:06:19 +0000

    add stub for lapin example

---
+++ crates/ex/cats/database/tests/AMQP/lapin.rs
@@ -0,0 +1,16 @@
+// ANCHOR: example
+
+// AMQP client library
+
+fn main() -> anyhow::Result<()> {
+    Ok(())
+}
+// ANCHOR_END: example
+
+#[test]
+#[ignore = "not implemented yet"]
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
+// TODO P2 write; https://lib.rs/crates/lapin
