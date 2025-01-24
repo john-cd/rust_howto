063dddd329f9c3e8a8d9638ee71b52dcb5a5085e
John <john-cd@users.noreply.github.com>
2025-01-24 19:07:55 +0000

    add stub for rocksdb example

---
+++ crates/ex/cats/database/tests/rocksdb/rocksdb.rs
@@ -0,0 +1,17 @@
+// ANCHOR: example
+
+// Rust wrapper for Facebook's RocksDB embeddable database
+// Requirements: Clang and LLVM
+
+fn main() -> anyhow::Result<()> {
+    Ok(())
+}
+// ANCHOR_END: example
+
+#[test]
+#[ignore = "not implemented yet"]
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
+// TODO P2 write
