94097da9a21631c08a59fe9d0502416c44e776cf
John CD <john-cd@users.noreply.github.com>
2025-01-25 11:51:38 -0800

    add rkyv example

--- crates/ex/cats/encoding/tests/no_external_schema_file/rkyv.rs
+++ crates/ex/cats/encoding/tests/no_external_schema_file/rkyv.rs
@@ -1,16 +1,44 @@
 // ANCHOR: example
+use rkyv::Archive;
+use rkyv::Deserialize;
+use rkyv::Serialize;
+use rkyv::rancor::Error;
 
-// Fast zero-copy deserialization framework that allows arbitrary field types
+// `rkyv` (short for "archive") is a high-performance, zero-copy
+// (de)serialization framework that allows arbitrary field types
 // and safe zero-copy mutation.
 
-fn main() {
-    todo!();
+#[derive(Archive, Deserialize, Serialize, Debug)]
+struct MyStruct {
+    field1: String,
+    field2: i32,
+}
+
+fn main() -> anyhow::Result<()> {
+    // Create an instance of MyStruct
+    let data = MyStruct {
+        field1: "Hello, world!".to_string(),
+        field2: 42,
+    };
+
+    // Serialize the data to a byte array
+    let buffer = rkyv::to_bytes::<Error>(&data)?;
+
+    // Print the serialized data
+    println!("Serialized data: {:?}", buffer);
+
+    // Deserialize the data from the byte array
+    let deserialized_data = rkyv::from_bytes::<MyStruct, Error>(&buffer[..])?;
+
+    println!("{:?}", deserialized_data);
+
+    Ok(())
 }
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
 }
 // TODO P1 write rkyv example
