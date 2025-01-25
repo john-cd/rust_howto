2ae33826798c2d3df494e6ad9188a730081d773b
John CD <john-cd@users.noreply.github.com>
2025-01-25 10:37:17 -0800

    draft objc2 example

--- crates/ex/cats/development_tools_ffi/tests/objc/objc2.rs
+++ crates/ex/cats/development_tools_ffi/tests/objc/objc2.rs
@@ -1,12 +1,30 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use objc2::{class, msg_send, sel, sel_impl};
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// TODO P1 write; add to markdown
+// fn main() {
+//     unsafe {
+//         let class_name = "NSString";
+//         // Retrieve the Objective-C class object for the given class name
+//         // ("NSString" in this case).
+//         // NSString is a fundamental class in the
+//         // Foundation framework of Apple's Cocoa and Cocoa Touch frameworks.
+//         // It represents an immutable sequence of Unicode characters.
+//         let class = class!(class_name);
+
+//         // Send messages (invoke methods) to Objective-C objects.
+//         let string = msg_send![class, alloc];
+//         // Send the initWithString: message to the allocated object,
+//         // initializing it with the given string.
+//         let string = msg_send![string, initWithString: "Hello from Rust!"];
+//         // Send the length message to the initialized string object, which
+// returns its length.         let length = msg_send![string, length];
+//         println!("String length: {}", length);
+//     }
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // TODO P1 write; add to markdown; https://docs.rs/objc2/latest/objc2/
