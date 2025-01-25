7f0df660ccad312ec02980527a763f58c31cba08
John CD <john-cd@users.noreply.github.com>
2025-01-24 21:29:41 -0800

    add bincode and rpm-serde deps; draft examples

--- crates/ex/cats/encoding/tests/proto/bincode.rs
+++ crates/ex/cats/encoding/tests/proto/bincode.rs
@@ -1,12 +1,52 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use std::fs::File;
+// use std::io::Cursor;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// TODO P1 write bincode example
+// use bincode::deserialize_from;
+// use bincode::serialize_into;
+
+// #[derive(Serialize, Deserialize, Debug)]
+// struct MyData {
+//     name: String,
+//     age: u32,
+//     is_active: bool,
+// }
+
+// fn main() -> anyhow::Result<()> {
+//     // Create a sample data object
+//     let data = MyData {
+//         name: "Alice".to_string(),
+//         age: 30,
+//         is_active: true,
+//     };
+
+//     // Serialize to a file
+//     let mut file = File::create("data.bin")?;
+//     serialize_into(&mut file, &data)?;
+
+//     // Deserialize from a file
+//     let mut file = File::open("data.bin")?;
+//     let deserialized_data: MyData = deserialize_from(&mut file)?;
+
+//     // Serialize to a buffer
+//     let mut buf = Vec::new();
+//     serialize_into(&mut buf, &data)?;
+
+//     // Deserialize from a buffer
+//     let mut cursor = Cursor::new(&buf[..]);
+//     let deserialized_data_from_buf: MyData = deserialize_from(&mut cursor)?;
+
+//     // Print the deserialized data
+//     println!("{:?}", deserialized_data);
+//     println!("{:?}", deserialized_data_from_buf);
+
+//     Ok(())
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() -> anyhow::Result<()> {
+//     main()?;
+//     Ok(())
+// }
+// // TODO P1 write bincode example
--- crates/ex/cats/encoding/tests/proto/rmp_serde.rs
+++ crates/ex/cats/encoding/tests/proto/rmp_serde.rs
@@ -1,12 +1,57 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use std::io::Cursor;
+// // use std::io::Read;
+// // use std::io::Write;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// TODO P1 write rmp_serde example
+// use rmp_serde::Deserializer;
+// use rmp_serde::Serializer;
+// use serde::Deserialize;
+// use serde::Serialize;
+
+// // Provides serialization and deserialization to/from MessagePack.
+// // Key features of rmp_serde:
+// // Efficient: MessagePack is a compact binary format, leading to smaller
+// // serialized data sizes and faster serialization/deserialization. Versatile:
+// // Supports a wide range of data types and can be used with various
+// input/output // sources. Interoperable: MessagePack is language-agnostic,
+// enabling data // exchange between different programming languages.
+
+// #[derive(Serialize, Deserialize, Debug)]
+// struct MyData {
+//     name: String,
+//     age: u32,
+//     is_active: bool,
+// }
+
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     // Create a sample data object
+//     let data = MyData {
+//         name: "Alice".to_string(),
+//         age: 30,
+//         is_active: true,
+//     };
+
+//     // Serialize to MessagePack
+//     let mut buf = Vec::new();
+//     // Creates a serializer that writes to the buffer,
+//     // Serializes the data object into MessagePack format and writes it to
+// the     // buffer.
+//     data.serialize(&mut Serializer::new(&mut buf))?;
+
+//     // Deserialize from MessagePack
+//     let mut cursor = Cursor::new(&buf[..]);
+//     let deserialized_data: MyData =
+//         Deserializer::from_reader(&mut cursor)?.deserialize()?;
+
+//     // Print the deserialized data
+//     println!("{:?}", deserialized_data);
+
+//     Ok(())
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // TODO P1 finish
