64fffd5b09f462f841391e104aeb1cb7216d88e6
John CD <john-cd@users.noreply.github.com>
2025-01-25 10:20:57 -0800

    draft napi example

--- crates/ex/cats/development_tools_ffi/tests/node/napi.rs
+++ crates/ex/cats/development_tools_ffi/tests/node/napi.rs
@@ -1,15 +1,54 @@
-// ANCHOR: example
+// // ANCHOR: example
 
-// Framework for building pre-compiled Node.js addons in Rust.
+// // Framework for building pre-compiled Node.js addons in Rust.
 
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// /// import the preludes
+// use napi::bindgen_prelude::*;
+// use napi_derive::napi;
+// use napi::Env;
+// use napi::Error;
+// use napi::JsFunction;
+// use napi::JsObject;
+// use napi::JsString;
+// use napi::Result;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// TODO P1 write; add to markdown
+// #[napi] // Indicates that the function is exposed to JavaScript.
+// fn hello(mut this: JsObject, args: Vec<napi::Value>) -> Result<JsString> {
+//     let env = this.env();
+//     let name = args.get(0)?.as_string()?.into_owned();
+
+//     let greeting = format!("Hello, {}!", name);
+//     env.create_string(&greeting)
+// }
+
+// #[napi]
+// fn create_function(
+//     mut this: JsObject,
+//     args: Vec<napi::Value>,
+// ) -> Result<JsFunction> {
+//     let env = this.env();
+
+//     let js_fn = JsFunction::new(&env, hello)?;
+//     this.set_named_property("hello", &js_fn)?;
+
+//     Ok(js_fn)
+// }
+
+// #[napi]
+// fn init(mut exports: JsObject) -> Result<()> {
+//     exports.set_named_property("createFunction", &create_function)?;
+//     Ok(())
+// }
+
+// napi_module_init!(init);
+
+// fn main() {
+//     todo!();
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // TODO P1 write; add to markdown; https://lib.rs/crates/napi
