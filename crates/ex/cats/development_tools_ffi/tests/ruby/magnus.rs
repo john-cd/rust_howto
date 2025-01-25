0342f5d8348534905659669e2ba965f233af1e1d
John CD <john-cd@users.noreply.github.com>
2025-01-25 11:21:33 -0800

    draft magnus example

--- crates/ex/cats/development_tools_ffi/tests/ruby/magnus.rs
+++ crates/ex/cats/development_tools_ffi/tests/ruby/magnus.rs
@@ -1,12 +1,39 @@
-// ANCHOR: example
-fn main() {
-    todo!();
-}
-// ANCHOR_END: example
+// // ANCHOR: example
+// use magnus::Error;
+// use magnus::RString;
+// use magnus::define_global_function;
+// use magnus::eval;
 
-#[test]
-#[ignore = "not yet implemented"]
-fn test() {
-    main();
-}
-// TODO P1 write; add to markdown
+// fn hello_rust() -> String {
+//     String::from("Hello from Rust again!")
+// }
+
+// fn main() -> Result<(), Error> {
+//     // Initialize the Ruby VM
+//     let ruby = magnus::init()?;
+
+//     // Create a new Ruby string object from the given Rust string
+//     let string = RString::new(&ruby, "Hello from Rust!")?;
+
+//     // Evaluate Ruby code
+//     ruby.eval(&format!("puts '{}'", string.to_str()?))?;
+
+//     // Define a global Ruby function that calls a Rust function
+//     define_global_function("hello_rust", hello_rust);
+
+//     // Evaluate Ruby code that calls the Rust function
+//     eval::<()>(
+//         r#"
+//         puts hello_rust()
+//     "#,
+//     )?;
+
+//     Ok(())
+// }
+// // ANCHOR_END: example
+
+// #[test]
+// fn test() {
+//     main();
+// }
+// // TODO P1 write; add to markdown
