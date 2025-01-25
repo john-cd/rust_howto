d32a09bff420508c69ef7008997bb9a20fabc9e3
John CD <john-cd@users.noreply.github.com>
2025-01-25 15:03:57 -0800

    formatting

--- crates/ex/cats/development_tools_ffi/tests/flutter/flutter_rust_bridge.rs
+++ crates/ex/cats/development_tools_ffi/tests/flutter/flutter_rust_bridge.rs
@@ -4,19 +4,21 @@
 
 // // Typical scenarios to combine them include:
 // //
-// // - UI framework for Rust: When you want a UI framework for your Rust
-// system. // - Use arbitrary Rust libraries in Flutter: When the desired
-// functionality only has a library in Rust, not Dart (Flutter). // - Need high-performance
-// code for Flutter: Rust makes it easy and performant to write multi-thread
-// code, algorithms, data-intensive operations, SIMD code, etc.
+// // - When you want a UI framework for your Rust system.
+// // - Use arbitrary Rust libraries in Flutter: When the desired
+// // functionality only has a library in Rust, not Dart / Flutter.
+// // - Need high-performance code for Flutter: Rust makes it easy and
+// performant // to write multi-thread code, data-intensive operations, SIMD
+// code.
 
 // // Install Rust.
 // // Install Flutter: https://flutter.dev/
-// // ```
+// // ```sh
 // // cargo install flutter_rust_bridge_codegen && flutter_rust_bridge_codegen
-// create my_app && cd my_app && flutter run // ```
-// // You can also use: `cargo binstall flutter_rust_bridge_codegen` to install
-// the binary directly.
+// // create my_app && cd my_app && flutter run
+// // ```
+// // You can also use: `cargo binstall flutter_rust_bridge_codegen`
+// to install the binary directly.
 
 // // Regenerate the bridge when the Rust code changes:
 // // flutter_rust_bridge_codegen generate && flutter run
