8039ee9e4ce2db9311156fb13422ed7bc6592a1c
John <john-cd@users.noreply.github.com>
2025-01-27 21:43:48 +0000

    rust_gpu: add notes / links / crates

--- crates/ex/other/tests/gpu/rust_gpu.rs
+++ crates/ex/other/tests/gpu/rust_gpu.rs
@@ -3,8 +3,19 @@ fn main() {}
 // ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
+#[ignore = "later"]
 fn test() {
     main();
 }
 // [P1](https://github.com/john-cd/rust_howto/issues/886) need to review - see gpu.md
+// Crates:
+// https://github.com/Rust-GPU
+// https://github.com/Rust-GPU/rust-gpu https://rust-gpu.github.io/ https://rust-gpu.github.io/rust-gpu/book/
+// https://github.com/cogciprocate/ocl
+// https://wgpu.rs/  https://github.com/gfx-rs/wgpu https://github.com/gfx-rs/wgpu/tree/v24/examples#readme
+// https://docs.rs/ash/latest/ash/
+// https://rust-gpu.github.io/Rust-CUDA/index.html
+// https://github.com/Rust-GPU/Rust-CUDA  https://rust-gpu.github.io/blog/2025/01/27/rust-cuda-reboot/
+// Examples to look into
+// https://github.com/MWATelescope/mwa_hyperdrive/tree/main
+// https://github.com/TheZoq2/locksort
