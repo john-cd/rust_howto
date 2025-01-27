fd8468b38b185421fb7cf163a075656d50a4cf41
John <john-cd@users.noreply.github.com>
2025-01-27 18:28:38 +0000

    formatting

--- crates/ex/cats/database/tests/cassandra/cassandra_protocol.rs
+++ crates/ex/cats/database/tests/cassandra/cassandra_protocol.rs
@@ -31,8 +31,8 @@
 
 //     // Send a query message
 //     let query =
-//         Query::new("SELECT * FROM system.local", Consistency::One, None, None);
-//     transport.send(Frame::new(Opcode::Query, query)).await?;
+//         Query::new("SELECT * FROM system.local", Consistency::One, None,
+// None);     transport.send(Frame::new(Opcode::Query, query)).await?;
 
 //     // Receive the result message
 //     let frame = transport.receive().await?;
