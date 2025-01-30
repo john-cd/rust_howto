ffc7c34b13ac331917a77d664788d6148f34865c
John <john-cd@users.noreply.github.com>
2025-01-30 05:16:50 +0000

    fix rand_dist example; close #1008

--- crates/ex/cats/algorithms/tests/rand/rand_dist.rs
+++ crates/ex/cats/algorithms/tests/rand/rand_dist.rs
@@ -1,20 +1,19 @@
-// // ANCHOR: example
-// use rand::distr::Distribution;
-// use rand_distr::Normal;
-// use rand_distr::NormalError;
+// ANCHOR: example
+use rand::distr::Distribution;
+use rand_distr::Normal;
+use rand_distr::NormalError;
 
-// fn main() -> Result<(), NormalError> {
-//     let mut rng = rand::rng();
-//     let normal = Normal::new(2.0, 3.0)?;
-//     let v = normal.sample(&mut rng);
-//     println!("{} is from a N(2, 9) distribution", v);
-//     Ok(())
-// }
-// // ANCHOR_END: example
+fn main() -> Result<(), NormalError> {
+    let mut rng = rand::rng();
+    let normal = Normal::new(2.0, 3.0)?;
+    let v = normal.sample(&mut rng);
+    println!("{} is from a N(2, 9) distribution", v);
+    Ok(())
+}
+// ANCHOR_END: example
 
-// #[test]
-// fn test() -> anyhow::Result<()> {
-//     main()?;
-//     Ok(())
-// }
-// [ P0 fix version conflict for rand_distr](https://github.com/john-cd/rust_howto/issues/1008)
+#[test]
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
