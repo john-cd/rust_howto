3e24c4ea2790ee61d77373ac8332b4f1ff72018e
John <john-cd@users.noreply.github.com>
2025-01-31 17:10:55 +0000

    fix ignore example; close #762

--- crates/ex/cats/filesystem/tests/directory_traversal/ignore.rs
+++ crates/ex/cats/filesystem/tests/directory_traversal/ignore.rs
@@ -1,42 +1,43 @@
-// // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
+// ANCHOR: example
+use ignore::WalkBuilder;
 
-// use ignore::WalkBuilder;
-// use std::path::Path;
+// The `ignore` crate provides a fast recursive directory iterator
+// that respects `.gitignore` and similar files, ignores hidden files, etc.
 
-// // In `Cargo.toml`:
-// // ignore = "0.4"
+// In `Cargo.toml`, add:
+// ignore = "0.4" # or latest
 
-// fn main() {
-//     // Specify the directory to walk
-//     let root = Path::new("path/to/directory");
+fn main() {
+    // Specify the directory to walk
+    let root = std::path::Path::new("tests/");
 
-//     // Create a WalkBuilder to walk the directory
-//     let walker = WalkBuilder::new(root)
-//         .standard_filters(true)  // Respect .gitignore and other ignore files
-//         .build();
+    // Create a WalkBuilder and build a new Walk iterator.
+    // A lot more options are available
+    let walker = WalkBuilder::new(root)
+        .add("temp/") // Add another directory to walk
+        .standard_filters(true)  // Respect .gitignore and other ignore files
+        .build();
 
-//     // Iterate over the entries
-//     for result in walker {
-//         match result {
-//             Ok(entry) => {
-//                 let path = entry.path();
-//                 if path.is_file() {
-//                     println!("File: {:?}", path);
-//                 } else if path.is_dir() {
-//                     println!("Directory: {:?}", path);
-//                 }
-//             }
-//             Err(err) => {
-//                 eprintln!("Error: {}", err);
-//             }
-//         }
-//     }
-// }
+    // Iterate over the entries
+    for result in walker {
+        match result {
+            Ok(entry) => {
+                let path = entry.path();
+                if path.is_file() {
+                    println!("File: {:?}", path);
+                } else if path.is_dir() {
+                    println!("Directory: {:?}", path);
+                }
+            }
+            Err(err) => {
+                eprintln!("Error: {}", err);
+            }
+        }
+    }
+}
+// ANCHOR_END: example
 
-// #[test]
-// fn test() {
-//     main();
-// }
-// // [P0](https://github.com/john-cd/rust_howto/issues/762)
+#[test]
+fn test() {
+    main();
+}
