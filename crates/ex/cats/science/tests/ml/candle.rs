71b8818605f7789d853773d24a740966a0cfc14b
John <john-cd@users.noreply.github.com>
2025-02-02 05:05:37 +0000

    candle example (WIP, #835)

--- crates/ex/cats/science/tests/ml/candle.rs
+++ crates/ex/cats/science/tests/ml/candle.rs
@@ -1,11 +1,11 @@
 // // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
 
+// // Data types of elements allowed in tensors.
 // use candle_core::DType;
-// use candle_core::Device;
+// // Tensors are reference counted with `Arc`, so cloning them is cheap.
 // use candle_core::Tensor;
 // use candle_core::error::Result;
+
 // use candle_nn::Module;
 // use candle_nn::Optimizer;
 // use candle_nn::VarBuilder;
@@ -13,37 +13,46 @@
 // use candle_nn::Linear;
 // use candle_nn::linear;
 
+// // Candle consists of a number of crates.
 // // Add to your `Cargo.toml` as needed:
-// // candle-core. Core Datastructures and DataTypes.
-// // candle-nn. Building blocks for Neural Nets.
-// // candle-datasets. Rust access to commonly used Datasets like MNIST.
-// // candle-examples. Examples of Candle in Use.
-// // candle-onnx. Loading and using ONNX models.
-// // candle-pyo3. Access to Candle from Python.
-// // candle-transformers. Candle implemntation of many published transformer
-// models. // safetensors
-// // tokenizers
+// // Core Datastructures and DataTypes.
+// // candle-core = "0.8.2"
+// // Building blocks for Neural Nets.
+// // candle-nn = "0.8.2"
+// // Rust access to commonly used Datasets like MNIST.
+// // candle-datasets = "0.8.2"
+// // Examples of Candle in Use.
+// // candle-examples = "0.8.1"
+// // Loading and using ONNX models.
+// // candle-onnx = "0.8.2"
+// // Access to Candle from Python.
+// // candle-pyo3 = "0.8.2"
+// // Candle implementation of many published transformer models.
+// // candle-transformers = "0.8.2"
 
-// const DEVICE: Device = Device::Cpu;
+// // Choose between `Cpu`, `Cuda`, and `Metal`
+// // For CUDA / Metal, review https://huggingface.github.io/candle/guide/installation.html
+// const DEVICE: candle_core::Device = candle_core::Device::Cpu;
+
+// // Define the model architecture
+// struct LinearModel {
+//     linear: Linear,
+// }
+
+// // Define a module with the `forward` method using a single argument.
+// impl Module for LinearModel {
+//     fn forward(&self, x: &Tensor) -> Result<Tensor> {
+//         Ok(self.linear.forward(x)?)
+//     }
+// }
+
+// impl LinearModel {
+//     fn parameters(&self) -> &Tensor {
+//         vec![("linear.weight".to_string(), self.linear.weight())]
+//     }
+// }
 
 // fn main() -> anyhow::Result<()> {
-//     // Define model architecture
-//     struct LinearModel {
-//         linear: Linear,
-//     }
-
-//     // Define a module with the `forward` method using a single argument.
-//     impl Module for LinearModel {
-//         fn forward(&self, x: &Tensor) -> Result<Tensor> {
-//             Ok(self.linear.forward(x)?)
-//         }
-//     }
-
-//     impl LinearModel {
-//         fn parameters(&self) -> Vec<(String, VarBuilder)> {
-//             vec![("linear.weight".to_string(), self.linear.weight.clone())]
-//         }
-//     }
 
 //     // Create model instance
 //     let model = LinearModel {
@@ -95,5 +104,7 @@
 
 //     main();
 // }
+// // ANCHOR_END: example
 // // [P2](https://github.com/john-cd/rust_howto/issues/835) review https://huggingface.github.io/candle/index.html
 // // https://github.com/ToluClassics/candle-tutorial
+// // https://huggingface.github.io/candle/index.html
