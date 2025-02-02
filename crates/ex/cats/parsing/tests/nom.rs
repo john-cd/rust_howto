10b615167510862dc6c4a8d3fa2495abfed9d356
John <john-cd@users.noreply.github.com>
2025-02-02 18:12:01 +0000

    fix nom example after upgrade

--- crates/ex/cats/parsing/tests/nom.rs
+++ crates/ex/cats/parsing/tests/nom.rs
@@ -1,5 +1,6 @@
 // ANCHOR: example
 use nom::IResult;
+use nom::Parser;
 use nom::bytes::complete::tag;
 use nom::character::complete::digit1;
 use nom::combinator::map_res;
@@ -13,14 +14,14 @@ fn parse_number(input: &str) -> IResult<&str, u32> {
     // `digit1` recognizes one or more ASCII numerical characters: 0-9
     // `map_res` applies a function returning a `Result` over the result of a
     // parser. In this case, we parse the string of digits into an integer.
-    map_res(digit1, str::parse)(input)
+    map_res(digit1, str::parse).parse(input)
 }
 
 fn parse_comma_separated_numbers(input: &str) -> IResult<&str, Vec<u32>> {
     // `tag` recognizes a pattern / string
     // `separated_list0` alternates between two parsers to produce a list of
     // elements
-    separated_list0(tag(","), parse_number)(input)
+    separated_list0(tag(","), parse_number).parse(input)
 }
 
 fn main() {
