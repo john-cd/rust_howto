244306199a0b3a09a7d580b35a29bbc8dc83398f
John <john-cd@users.noreply.github.com>
2025-02-02 21:12:52 +0000

    finish sha2 example; close #703

--- crates/ex/cats/cryptography/tests/sha2.rs
+++ crates/ex/cats/cryptography/tests/sha2.rs
@@ -1,63 +1,68 @@
-// // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
+// ANCHOR: example
+// Constant-Time Base64 encoding
+use base64ct::Base64;
+use base64ct::Encoding;
+// Convenience wrapper trait covering functionality of
+// cryptographic hash functions with fixed output size.
+use sha2::Digest;
+// SHA-256 hasher
+use sha2::Sha256;
+// SHA-512 hasher.
+use sha2::Sha512;
 
-// use std::fs;
-// use std::io;
-// use base64ct::Base64;
-// use base64ct::Encoding;
-// use sha2::Digest;
-// use sha2::Sha256;
-// use sha2::Sha512;
-// use sha2::{Digest, Sha256};
+fn main() -> anyhow::Result<()> {
+    // If a complete message is available, then you can use the convenience
+    // `Digest::digest` method:
+    let hash1 = Sha256::digest(b"my message");
+    // Print the hash as a hexadecimal string
+    println!("SHA-256 hash #1: {:x}", hash1);
 
-// fn main() {
-//     // Create a Sha256 object
-//     let mut hasher = Sha256::new();
+    // Otherwise, create a Sha256 hasher
+    let mut hasher = Sha256::new();
 
-//     // Write input data
-//     let data = b"hello world";
-//     hasher.update(data);
+    // Add input data
+    let data = b"hello world";
+    hasher.update(data);
 
-//     // `update` can be called repeatedly and is generic over `AsRef<[u8]>`
-//     hasher.update("String data");
+    // `update` can be called repeatedly and is generic over `AsRef<[u8]>`
+    hasher.update("String data");
 
-//     // Read hash digest and consume hasher
-//     let result = hasher.finalize();
+    // Read hash digest and consume hasher
+    let hash2 = hasher.finalize();
+    println!("SHA-256 hash #2: {:x}", hash2);
 
-//     // Print the hash as a hexadecimal string
-//     println!("SHA-256 hash of '{:?}': {:x}", data, result);
+    // Same exercise, but using `Sha512` and `chain_update`:
+    let hash3 = Sha512::new()
+        .chain_update(b"Hello world!")
+        .chain_update("String data")
+        .finalize();
 
-//     // same for Sha512
-//     // Using `chain_update` instead of `update`:
-//     let hash = Sha512::new()
-//         .chain_update(b"Hello world!")
-//         .chain_update("String data")
-//         .finalize();
+    let base64_hash = Base64::encode_string(&hash3);
+    println!("Base64-encoded hash #3: {}", base64_hash);
 
-//     let base64_hash = Base64::encode_string(&hash);
-//     println!("Base64-encoded hash: {}", base64_hash);
+    // Hash the contents of a file:
+    // First, we will create a file inside of `env::temp_dir()`.
+    let mut file = tempfile::tempfile()?;
+    use std::io::Write;
+    writeln!(file, "Some data")?;
+    // or: let mut file = fs::File::open(&some_path)?;
 
-//     let hex_hash = base16ct::lower::encode_string(&hash);
-//     println!("Hex-encoded hash: {}", hex_hash);
-// }
-// fn hash_message() -> Result<(), Box<dyn std::error::Error>> {
+    // Copies the entire contents of a reader into a writer,
+    // in this case the hasher
+    let mut hasher = Sha256::new();
 
-//     // If a complete message is available, then you can use the convenience
-//     // Digest::digest method:
-//     let hash = Sha256::digest(b"my message");
-// }
+    std::io::copy(&mut file, &mut hasher)?;
+    let hash4 = hasher.finalize();
+    // Constant-time conversion to hexadecimal
+    let hex_hash = base16ct::lower::encode_string(&hash4);
+    println!("Hex-encoded hash #4: {}", hex_hash);
 
-//     // Hashing Readable Objects
-// fn hash_file(path: &str) -> Result<(), Box<dyn std::error::Error>> {
-//     let mut file = fs::File::open(&path)?;
-//     let mut hasher = Sha256::new();
-//     let n = io::copy(&mut file, &mut hasher)?;
-//     let hash = hasher.finalize();
-// }
+    Ok(())
+}
+// ANCHOR_END: example
 
-// #[test]
-// fn test() {
-//     main();
-// }
-// // [P1](https://github.com/john-cd/rust_howto/issues/703)
+#[test]
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
