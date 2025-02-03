f54186befe47e7b97e483ec704f3b9110acd0b41
John <john-cd@users.noreply.github.com>
2025-02-03 05:29:25 +0000

    sha1 example - close #702

--- crates/ex/cats/cryptography/tests/sha1.rs
+++ crates/ex/cats/cryptography/tests/sha1.rs
@@ -1,31 +1,31 @@
-// // ANCHOR: example
-// use hex_literal::hex;
-// use sha1::Digest;
-// use sha1::Sha1;
+// ANCHOR: example
+use hex_literal::hex;
+use sha1::Digest;
+use sha1::Sha1;
 
-// SHA-1 is considered cryptographically broken and should not be used for new
+// SHA-1 is considered cryptographically broken and should NOT be used for new
 // security-critical applications. It is primarily used for legacy compatibility
 // or non-security-sensitive purposes.
 
-// fn main() {
-//     // Create a Sha1 object
-//     let mut hasher = Sha1::new();
+fn main() {
+    // Create a SHA-1 hasher
+    let mut hasher = Sha1::new();
 
-//     // Process input message
-//     hasher.update(b"hello world");
+    // Process input message
+    hasher.update(b"hello world");
 
-//     // Acquire hash digest
-//     let result = hasher.finalize();
+    // Compute the hash digest
+    let result = hasher.finalize();
 
-//     // Assert the expected hash value
-//     assert_eq!(result[..], hex!("2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"));
+    // Assert the expected hash value
+    assert_eq!(result[..], hex!("2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"));
 
-//     println!("SHA-1 hash of 'hello world': {:x}", result);
-// }
-// // ANCHOR_END: example
+    println!("SHA-1 hash of 'hello world': {:x}", result);
+}
+// Example adapted from https://docs.rs/sha1/0.10.6/sha1/index.html
+// ANCHOR_END: example
 
-// #[test]
-// fn test() {
-//     main();
-// }
-// // [P1](https://github.com/john-cd/rust_howto/issues/702)
+#[test]
+fn test() {
+    main();
+}
