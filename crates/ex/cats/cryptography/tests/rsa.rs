d8c18c067a8d7facd06511b008e683934bfa8ba5
John <john-cd@users.noreply.github.com>
2025-02-03 04:42:23 +0000

    finish rsa example; close #699

--- crates/ex/cats/cryptography/tests/rsa.rs
+++ crates/ex/cats/cryptography/tests/rsa.rs
@@ -1,71 +1,61 @@
-// // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
+// ANCHOR: example
+// Operating System's Random Number Generator
+use rand::rngs::OsRng;
+// Encryption using PKCS#1 v1.5 padding.
+use rsa::Pkcs1v15Encrypt;
+use rsa::RsaPrivateKey;
+use rsa::RsaPublicKey;
 
-// use rand::rngs::OsRng;
-// use rsa::PaddingScheme;
-// use rsa::PublicKey;
-// use rsa::RsaPrivateKey;
-// use rsa::RsaPublicKey;
-// use sha2::Sha256;
+// RSA encryption and decryption
+//
+// In this example, we
+// - generate a 2048-bit RSA private key using `RsaPrivateKey::new`.
+// - derive the corresponding public key using `RsaPublicKey::from`.
+// - encrypt a message to be encrypted using the public key with
+// `PaddingScheme::new_pkcs1v15_encrypt`.
+// - decrypt the message using the private key with the same padding scheme.
+//
+// Read the security section of https://docs.rs/rsa/
 
-// // In this example:
+fn main() {
+    // Define the message to be encrypted
+    let message = b"Secret message";
+    println!(
+        "Original message: {:?}",
+        String::from_utf8(message.to_vec()).unwrap()
+    );
 
-// // We generate a 2048-bit RSA private key using RsaPrivateKey::new.
+    // Generate a 2048-bit private key
+    let mut rng = OsRng;
+    let bits = 2048;
+    let private_key =
+        RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
 
-// // We derive the corresponding public key using RsaPublicKey::from.
+    // Derive the public key from the private key
+    let public_key = RsaPublicKey::from(&private_key);
 
-// // We define a message to be encrypted.
+    // Encrypt the message using the public key
+    let encrypted_message = public_key
+        .encrypt(&mut rng, Pkcs1v15Encrypt, &message[..])
+        .expect("failed to encrypt");
 
-// // We encrypt the message using the public key with
-// // PaddingScheme::new_pkcs1v15_encrypt.
+    println!("Encrypted message: {:?}", encrypted_message);
 
-// // We decrypt the message using the private key with the same padding scheme.
+    // Decrypt the message using the private key
+    let decrypted_message = private_key
+        .decrypt(Pkcs1v15Encrypt, &encrypted_message)
+        .expect("failed to decrypt");
 
-// // This code demonstrates how to perform RSA encryption and decryption in
-// Rust.
+    assert_eq!(message.to_vec(), decrypted_message);
 
-// fn main() {
-//     // Generate a 2048-bit private key
-//     let mut rng = OsRng;
-//     let bits = 2048;
-//     let private_key =
-//         RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a
-// key");
+    println!(
+        "Decrypted message: {:?}",
+        String::from_utf8(decrypted_message).unwrap()
+    );
+}
+// ANCHOR_END: example
 
-//     // Derive the public key from the private key
-//     let public_key = RsaPublicKey::from(&private_key);
-
-//     // Define the message to be encrypted
-//     let message = b"Secret message";
-
-//     // Encrypt the message using the public key
-//     let padding = PaddingScheme::new_pkcs1v15_encrypt();
-//     let encrypted_message = public_key
-//         .encrypt(&mut rng, padding, &message[..])
-//         .expect("failed to encrypt");
-
-//     // Decrypt the message using the private key
-//     let padding = PaddingScheme::new_pkcs1v15_encrypt();
-//     let decrypted_message = private_key
-//         .decrypt(padding, &encrypted_message)
-//         .expect("failed to decrypt");
-
-//     assert_eq!(message.to_vec(), decrypted_message);
-
-//     println!(
-//         "Original message: {:?}",
-//         String::from_utf8(message.to_vec()).unwrap()
-//     );
-//     println!("Encrypted message: {:?}", encrypted_message);
-//     println!(
-//         "Decrypted message: {:?}",
-//         String::from_utf8(decrypted_message).unwrap()
-//     );
-// }
-
-// #[test]
-// fn test() {
-//     main();
-// }
-// // [P1](https://github.com/john-cd/rust_howto/issues/699)
+#[test]
+fn test() {
+    main();
+}
