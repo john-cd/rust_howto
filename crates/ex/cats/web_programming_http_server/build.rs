5a630f5119e539036e60f9cc82a16122ec2018f2
John <john-cd@users.noreply.github.com>
2025-02-05 01:17:39 +0000

    tonic example (#870)

--- crates/ex/cats/web_programming_http_server/build.rs
+++ crates/ex/cats/web_programming_http_server/build.rs
@@ -1,6 +1,18 @@
 // ANCHOR: example
 fn main() {
-    // [ P1 install protoc](https://github.com/john-cd/rust_howto/issues/1006)
-    // tonic_build::compile_protos("proto/helloworld.proto").unwrap();
+    // `tonic-build` compiles ProtoBuf files via `prost` and generates service
+    // stubs and proto definitions for use with `tonic`.
+    // Install `protoc` first: https://grpc.io/docs/protoc-installation/
+    tonic_build::compile_protos("proto/helloworld.proto").unwrap();
 }
+// A more complicated example may be:
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+//    tonic_build::configure()
+//         .build_server(false) // Disable gRPC server code generation
+//         .compile_protos(
+//             &["proto/helloworld.proto"],
+//             &["proto"],
+//         )?;
+//    Ok(())
+// }
 // ANCHOR_END: example
--- crates/ex/cats/web_programming_http_server/tests/grpc/tonic.rs
+++ crates/ex/cats/web_programming_http_server/tests/grpc/tonic.rs
@@ -1,50 +1,73 @@
-// // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
+#![allow(dead_code)]
+// ANCHOR: example
+// Imports from the module created by `tonic` (see below):
+// pub struct HelloReply { pub message: String, }
+use helloworld::HelloReply;
+// pub struct HelloRequest { pub name: String, }
+use helloworld::HelloRequest;
+// Generated trait containing gRPC methods that should be implemented for
+// use with GreeterServer
+use helloworld::greeter_server::Greeter;
+// Wrapper around the Greeter implementation (below)
+use helloworld::greeter_server::GreeterServer;
+// A gRPC request and metadata from an RPC call:
+use tonic::Request;
+// A gRPC response and metadata from an RPC call:
+use tonic::Response;
+// A gRPC status describing the result of an RPC call:
+use tonic::Status;
+use tonic::transport::Server;
 
-// use tonic::{transport::Server, Request, Response, Status};
-// use helloworld::greeter_server::{Greeter, GreeterServer};
-// use helloworld::{HelloRequest, HelloReply};
-// use tonic::server::NamedService;
+// Tonic is a gRPC over HTTP/2 library.
 
-// // Tonic is a gRPC library.
+// Add the following to your `Cargo.toml`:
+// [dependencies]
+// tonic = <tonic-version>
+// prost = <prost-version>
+//
+// [build-dependencies]
+// tonic-build = <tonic-version>
 
-// pub mod helloworld {
-//     tonic::include_proto!("helloworld");
-// }
+pub mod helloworld {
+    tonic::include_proto!("helloworld");
+}
 
-// #[derive(Debug, Default)]
-// pub struct MyGreeter {}
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
 
-// #[tonic::async_trait]
-// impl Greeter for MyGreeter {
-//     async fn say_hello(
-//         &self,
-//         request: Request<HelloRequest>,
-//     ) -> Result<Response<HelloReply>, Status> {
-//         let reply = helloworld::HelloReply {
-//             message: format!("Hello {}!", request.into_inner().name),
-//         };
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let reply = helloworld::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
 
-//         Ok(Response::new(reply))
-//     }
-// }
+        Ok(Response::new(reply))
+    }
+}
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let addr = "[::1]:50051".parse().unwrap();
-//     let greeter = MyGreeter::default();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
 
-//     Server::builder()
-//         .add_service(GreeterServer::new(greeter))
-//         .serve(addr)
-//         .await?;
+    Server::builder()
+        // Create a router
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
 
-//     Ok(())
-// }
+    Ok(())
+}
+// Look for additional examples in https://github.com/hyperium/tonic/tree/master/examples
+// ANCHOR_END: example
 
 // #[test]
-// fn test() {
-//     main();
+// fn test() -> anyhow::Result<()> {
+//     main()?;
+//     Ok(())
 // }
-// // [P1](https://github.com/john-cd/rust_howto/issues/870)
+// [P1](https://github.com/john-cd/rust_howto/issues/870) need to write full integration test, testing the endpoint then stopping the server
