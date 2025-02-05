7633bedc6bfbba19d0259d06fc1a1d57fc54be8e
John <john-cd@users.noreply.github.com>
2025-02-05 07:04:14 +0000

    rustls example (WIP, #700)

--- crates/ex/cats/cryptography/tests/rustls.rs
+++ crates/ex/cats/cryptography/tests/rustls.rs
@@ -1,55 +1,62 @@
 // // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
-
 // use std::io;
 // use std::sync::Arc;
 
+// // Common configuration for (typically) all connections made by a program:
 // use rustls::ClientConfig;
-// use rustls::RootCertStore;
 // use rustls::pki_types::ServerName;
+// // Extension trait, adding utility methods to all AsyncRead types
 // use tokio::io::AsyncReadExt;
+// // Extension trait, adding utility methods to all AsyncWrite types
 // use tokio::io::AsyncWriteExt;
-// use tokio::net::TcpStream;
-// use tokio_rustls::TlsConnector;
+
+// // Rustls is a TLS library that aims to provide a good level of cryptographic
+// // security, requires no configuration to achieve that security,
+// // and provides no unsafe features or obsolete cryptography by default.
+// // Rustls implements TLS1.2 and TLS1.3 for both clients and servers
+
+// // If you’re already using Tokio for an async runtime you may prefer to use
+// // `tokio-rustls` instead of interacting with `rustls` directly.
 
 // #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // Load the root certificate store
-//     let mut root_cert_store = RootCertStore::empty();
-//     root_cert_store.add_server_trust_anchors(
-//         webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
-//             OwnedTrustAnchor::from_subject_spki_name_constraints(
-//                 ta.subject,
-//                 ta.spki,
-//                 ta.name_constraints,
-//             )
-//         }),
+// async fn main() -> anyhow::Result<()> {
+//     // Load the root certificate store,
+//     // a container for root certificates able to provide
+//     // a root-of-trust for connection authentication:
+//     let root_store = rustls::RootCertStore::from_iter(
+//         // Mozilla's root certificates for use with the webpki or rustls crates
+//         // https://github.com/rustls/webpki-roots
+//         webpki_roots::TLS_SERVER_ROOTS.iter().cloned(),
 //     );
 
 //     // Create a `ClientConfig` with the root certificate store
+//     // - Create a builder for a client configuration with the process-default
+//     //   CryptoProvider and safe protocol version defaults.
 //     let config = ClientConfig::builder()
-//         .with_safe_defaults()
-//         .with_root_certificates(root_cert_store)
-//         .with_no_client_auth();
-//     let connector = TlsConnector::from(Arc::new(config));
+//         .with_root_certificates(root_store)
+//         .with_no_client_auth(); // Disable client authentication (most common)
+//     // Create a wrapper around a rustls::ClientConfig, providing an async
+//     // connect method
+//     let connector = tokio_rustls::TlsConnector::from(Arc::new(config));
 
 //     // Define the domain and server address
 //     let domain = "example.com";
-//     let addr = "93.184.216.34:443";
+//     let addr = "23.215.0.136:443";
 
 //     // Connect to the server
-//     let stream = TcpStream::connect(addr).await?;
-//     let domain = rustls::ServerName::try_from(domain).map_err(|_| {
+//     let stream = tokio::net::TcpStream::connect(addr).await?;
+//     //
+//     let domain = ServerName::try_from(domain).map_err(|_| {
 //         io::Error::new(io::ErrorKind::InvalidInput, "Invalid domain")
 //     })?;
+//     //
 //     let mut stream = connector.connect(domain, stream).await?;
 
 //     // Send a HTTP GET request
 //     stream
 //         .write_all(
-//             b"GET / HTTP/1.1\r\nHost: example.com\r\nConnection:
-// close\r\n\r\n",         )
+//             b"GET / HTTP/1.1\r\nHost: example.com\r\nConnection:close\r\n\r\n",
+//         )
 //         .await?;
 
 //     // Read the response
@@ -61,9 +68,12 @@
 
 //     Ok(())
 // }
+// // ANCHOR_END: example
 
 // #[test]
-// fn test() {
-//     main();
+// fn test() -> anyhow::Result<()> {
+//     main()?;
+//     Ok(())
 // }
-// // [P1](https://github.com/john-cd/rust_howto/issues/700)
+// // [P1](https://github.com/john-cd/rust_howto/issues/700) need full integration test; document further; review https://github.com/rustls/rustls/tree/main/examples
+// // review tokio-rustls
