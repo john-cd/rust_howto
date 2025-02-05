af32f2d80091ce8f72ff8cb319c65036118df702
John <john-cd@users.noreply.github.com>
2025-02-05 20:17:12 +0000

    fix "convert examples placeholder"s script

--- scripts/examples/convert_example_placeholders.sh
+++ scripts/examples/convert_example_placeholders.sh
@@ -4,9 +4,9 @@ set -eu
 # Convert {{#example <name>}} placeholders into ```rust {#include ...}``` blocks
 # and create the necessary code stubs (in subfolders of crates/**/tests/)
 #
-# Usage: /code/scripts/examples/convert_example_placeholders.sh
+# Usage: /code/scripts/examples/convert_example_placeholders.sh /code
 
-root="/code/"
+root="$(realpath $1)/"
 
 for file in $(find ${root}src -type f -name "*.md" -not -name "*.incl.md" )
 do
@@ -16,47 +16,46 @@ do
   # read -r = do not allow backslashes to escape any characters
   grep -Poh '(?<=\{\{#example ).+?(?=\}\})' $file | tr '-' '_' | while read -r examplename ;
   do
-    if [[ -n $examplename ]]; then
-        echo "Processing example: $examplename"
+    if [[ -n "${examplename}" ]]; then
+        echo "Processing example: ${examplename}"
         # P2 does not handle non-category examples
         current_file_dir=$(dirname $file)
-        category=$( basename $current_file_dir | tr '-' '_')
-        # echo "rel: $rel"
-        case ${category:0:1} in
+        category=$(basename $current_file_dir | tr '-' '_')
+        folder_in_tests=$(basename $file '.md')
+    #     case ${category:0:1} in
 
-        "a"|"b")
-          example_crate_name="ab"
-          ;;
+    #     "a"|"b")
+    #       example_crate_name="ab"
+    #       ;;
 
-        "c")
-          example_crate_name="c"
-          ;;
+    #     "c")
+    #       example_crate_name="c"
+    #       ;;
 
-        "d")
-          example_crate_name="d"
-          ;;
-        "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l")
-          example_crate_name="efghijkl"
-          ;;
-        "m" | "n")
-          example_crate_name="mn"
-          ;;
-        "o" | "p" | "q" |"r")
-          example_crate_name="opqr"
-          ;;
-        "s" | "t" | "u" | "v")
-          example_crate_name="stuv"
-          ;;
-        "w" | "x" | "y" | "z")
-          example_crate_name="wxyz"
-          ;;
-        *)
-          exit 108
-          ;;
-      esac
+    #     "d")
+    #       example_crate_name="d"
+    #       ;;
+    #     "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l")
+    #       example_crate_name="efghijkl"
+    #       ;;
+    #     "m" | "n")
+    #       example_crate_name="mn"
+    #       ;;
+    #     "o" | "p" | "q" |"r")
+    #       example_crate_name="opqr"
+    #       ;;
+    #     "s" | "t" | "u" | "v")
+    #       example_crate_name="stuv"
+    #       ;;
+    #     "w" | "x" | "y" | "z")
+    #       example_crate_name="wxyz"
+    #       ;;
+    #     *)
+    #       exit 108
+    #       ;;
+    #   esac
 
-      absoluteexampledir="${root}crates/ex/categories/${example_crate_name}/tests/${category}"
-      current_file_dir=$(dirname $file)
+      absoluteexampledir="${root}crates/ex/cats/${category}/tests/${folder_in_tests}"
       exampledir=$(realpath --relative-to=$current_file_dir "${absoluteexampledir}" | tr '-' '_')
       # echo "exampledir: $exampledir"
       examplefile="${exampledir}/${examplename}.rs"
@@ -64,8 +63,9 @@ do
       sed -Ei 's~\{\{#example\s*?'${examplename}'\s*?\}\}~```rust,editable\n\{\{#include '$examplefile':example\}\}\n```~g' $file
       # Create the folder for the test, if missing
       mkdir -p $absoluteexampledir
-      # Add a stub file for the example
+      # Create a GitHub ticket
       issue=$( gh issue create --title "Add example ${examplename}" --body "${examplefile}" )
+      # Add a stub file for the example
       cat > "$absoluteexampledir/${examplename}.rs" <<- EOF
 // ANCHOR: example
 fn main() {
@@ -81,10 +81,10 @@ fn test() {
 // [P1 add example](${issue})
 EOF
 
-      # Add the example file as a module to `mod.rs`
-      if [[ -z $(grep -Foh "${examplename}" "$absoluteexampledir/mod.rs") ]]; then
-        echo " Adding to $absoluteexampledir/mod.rs"
-        cat >> "$absoluteexampledir/mod.rs" <<- EOF
+      # Add the example file as a module to `main.rs`
+      if [[ -z $(grep -Foh "${examplename}" "$absoluteexampledir/main.rs") ]]; then
+        echo " Adding to $absoluteexampledir/main.rs"
+        cat >> "$absoluteexampledir/main.rs" <<- EOF
 mod ${examplename};
 EOF
       fi
