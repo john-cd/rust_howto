55c13e197ebcd8aee7ad5e7468b85951d56c3850
John <john-cd@users.noreply.github.com>
2025-02-05 07:01:47 +0000

    native_tls example (WIP, #696)

--- crates/ex/cats/cryptography/tests/native_tls.rs
+++ crates/ex/cats/cryptography/tests/native_tls.rs
@@ -1,23 +1,20 @@
 // // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
-
-// use hyper::Client;
 // use hyper::Uri;
+// use hyper::client;
 // use hyper_tls::HttpsConnector;
 // use tokio::runtime::Runtime;
 
-// fn main() {
+// fn main() -> anyhow::Result<()> {
 //     // Create a new runtime
 //     let rt = Runtime::new().unwrap();
 
 //     rt.block_on(async {
 //         // Create an HTTPS connector
 //         let https = HttpsConnector::new();
-//         let client = Client::builder().build::<_, hyper::Body>(https);
+//         let client = Client::builder().build::<_, hyper::body::Body>(https);
 
 //         // Create a request to fetch data from an HTTPS endpoint
-//         let uri: Uri = "https://www.example.com".parse().unwrap();
+//         let uri: Uri = "https://www.example.com".parse()?;
 //         match client.get(uri).await {
 //             Ok(response) => {
 //                 println!("Response status: {}", response.status());
@@ -27,10 +24,13 @@
 //             }
 //         }
 //     });
+//     Ok(())
 // }
+// // ANCHOR_END: example
 
 // #[test]
-// fn test() {
-//     main();
+// fn test() -> anyhow::Result<()> {
+//     main()?;
+//     Ok(())
 // }
 // // [P1](https://github.com/john-cd/rust_howto/issues/696)
