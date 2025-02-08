9d274050a64c68ecc995abafe5d12714037f6f8c
John <john-cd@users.noreply.github.com>
2025-02-08 04:31:38 +0000

    tokio_tungstenite example; close #873

--- crates/ex/cats/web_programming_websocket/tests/tokio_tungstenite.rs
+++ crates/ex/cats/web_programming_websocket/tests/tokio_tungstenite.rs
@@ -1,56 +1,50 @@
-// // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
+// ANCHOR: example
+// ANCHOR_END: example
+// Extension trait for Streams; defines `next()`
+use futures_util::StreamExt;
+// Extension trait for Sinks; defines `send()`
+use futures_util::SinkExt;
+use tokio_tungstenite::connect_async;
+// An enum representing the various forms of a WebSocket message: text, binary...
+use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 
-// use futures_util::StreamExt;
-// use futures_util::SinkExt;
-// //use futures_util::future;
-// //use futures_util::pin_mut;
-// //use tokio::io::AsyncReadExt;
-// //use tokio::io::AsyncWriteExt;
-// use tokio_tungstenite::connect_async;
-// use tokio_tungstenite::tungstenite::protocol::Message;
-// use tokio_tungstenite::tungstenite::client::IntoClientRequest;
+// Create an asynchronous WebSocket client.
+// The WebSocket protocol provides a simultaneous two-way communication channel over a single TCP connection,
+// typically between a web browser and a web server.
 
-// // Create an asynchronous WebSocket client.
-// //
-// // Connect to the WebSocket server, send the message "Hello WebSocket," and
-// // print the response from the server, which will be an echo of the message
-// // you sent.
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Connect to a test WebSocket server, which will echo the message you sent.
+    let request = "ws://echo.websocket.in".into_client_request()?;
+    // Add headers if needed: request.headers_mut().insert("api-key", "42".parse()?);
+    let (ws_stream, response) = connect_async(request).await?;
+    println!("Connected to the server");
 
-// #[tokio::main]
-// async fn main() -> anyhow::Result<()> {
-//     // Connect to the WebSocket server
-//     let mut request = "ws://echo.websocket.org/".into_client_request()?;
-//     request.headers_mut().insert("api-key", "42".parse()?);
-//     let (ws_stream, response) = connect_async(request).await?;
-//     println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
+    }
 
-//     println!("Response HTTP code: {}", response.status());
-//     println!("Response contains the following headers:");
-//     for (ref header, _value) in response.headers() {
-//         println!("* {}", header);
-//     }
+    let (mut write, mut read) = ws_stream.split();
 
-//     let (mut write, mut read) = ws_stream.split();
+    // Send a message to the server
+    write.send(Message::Text("Hello WebSocket".into())).await?;
+    println!("Message sent!");
 
-//     // Send a message to the server
-//     write.send(Message::Text("Hello WebSocket".into())).await?;
-//     println!("Message sent");
+    // Read a message from the server
+    if let Some(msg) = read.next().await {
+        let msg = msg?;
+        println!("Received: {}", msg);
+    }
 
-//     // Read a message from the server
-//     if let Some(msg) = read.next().await {
-//         let msg = msg?;
-//         println!("Received: {}", msg);
-//     }
+    Ok(())
+}
 
-//     Ok(())
-// }
-
-// #[test]
-// fn test() -> anyhow::Result<()> {
-//     main()?;
-//     Ok(())
-// }
-// // [P1](https://github.com/john-cd/rust_howto/issues/873)
-// // review https://github.com/snapview/tokio-tungstenite/tree/master/examples
+#[test]
+fn require_network() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
+// TODO P2 review https://github.com/snapview/tokio-tungstenite/tree/master/examples
