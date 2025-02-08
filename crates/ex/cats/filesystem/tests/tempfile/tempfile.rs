5ab9c448ad2ca7422952e0ae29a56c1fe2a527fd
John <john-cd@users.noreply.github.com>
2025-02-08 19:10:56 +0000

    tempfile example - close #763

--- crates/ex/cats/filesystem/tests/tempfile/tempfile.rs
+++ crates/ex/cats/filesystem/tests/tempfile/tempfile.rs
@@ -1,17 +1,44 @@
 // ANCHOR: example
 use std::io::Write;
-use std::io::{self};
 
-use tempfile::NamedTempFile;
+use tempfile::Builder;
+use tempfile::tempdir;
+use tempfile::tempfile;
 
-// Create a named temporary file, write to it, read from it, and print its path
-// and content using the tempfile crate in Rust.
+// Temporary files and directories.
 
-fn main() -> io::Result<()> {
-    // Create a named temporary file
-    let mut temp_file = NamedTempFile::new()?;
+fn main() -> std::io::Result<()> {
+    // 1. Create a temporary file (inside of `env::temp_dir()`).
+    // It will be automatically removed by the OS
+    // when the last handle to it is closed.
+    let mut temp_file = tempfile()?;
+    writeln!(temp_file, "This is temporary.")?;
 
-    // Write some data to the temporary file
+    // 2. Create a temporary directory (inside of `env::temp_dir()`).
+    let temp_dir = tempdir()?;
+    let temp_file_path = temp_dir.path().join("temporary_file");
+    let mut temp_file = std::fs::File::create(temp_file_path)?;
+    writeln!(temp_file, "This is also temporary.")?;
+
+    // By closing the `TempDir` explicitly, we can check that it has
+    // been deleted successfully. If we don't close it explicitly,
+    // the directory will still be deleted when `tmp_dir` goes out
+    // of scope, but we won't know whether deleting the directory
+    // succeeded.
+    drop(temp_file);
+    temp_dir.close()?;
+
+    // 3. Create a named temporary file
+    // Note: NamedTempFile rely on Rust destructors for cleanup.
+    // They may fail to run if the process exits through an unhandled signal
+    // interrupt.
+    let mut temp_file = Builder::new()
+        .prefix("my-temporary-file")
+        .suffix(".txt")
+        .rand_bytes(6)
+        .tempfile()?;
+    // or, for example: let mut temp_file = tempfile::NamedTempFile::new()?;
+
     writeln!(temp_file, "Hello, temporary file!")?;
 
     // Print the path of the temporary file
@@ -27,10 +54,11 @@ fn main() -> io::Result<()> {
 
     Ok(())
 }
+// Adapted from https://docs.rs/tempfile
 // ANCHOR_END: example
 
 #[test]
-fn test() {
-    main().unwrap();
+fn test() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
 }
-// [P0](https://github.com/john-cd/rust_howto/issues/763)
