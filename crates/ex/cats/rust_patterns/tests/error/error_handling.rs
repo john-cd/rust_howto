711e5d6e7255b1101f1336d1f69dfc3a44de6d57
John <john-cd@users.noreply.github.com>
2025-02-08 20:18:17 +0000

    error_handling example: close #169

--- crates/ex/cats/rust_patterns/tests/error/error_handling.rs
+++ crates/ex/cats/rust_patterns/tests/error/error_handling.rs
@@ -3,19 +3,29 @@ use std::io;
 use std::io::BufRead;
 
 fn main() {
-    let mut cursor = io::Cursor::new(b"foo\nbar");
-    let mut buf = String::new();
+    let mut my_cursor = io::Cursor::new(b"foo\nbar"); // A `Cursor` behaves like a `File` (but wraps a in-memory buffer).
+    let mut my_string = String::new();
 
-    cursor
-        // `read_line` puts whatever the user enters into the string we pass to it,
-        // but it also returns a `Result` value.
-        .read_line(&mut buf)
-        // If this instance of `Result` is an `Err` value, expect will cause the program to crash
-        // and display the message that you passed as an argument to expect.
-        .expect("Failed to read line");
+    // `readline` reads all bytes until a newline (the 0xA byte) is reached, and
+    // append them to the provided String. `readline` returns a `Result`
+    // value, which is either success (`Ok`) or failure (`Err`).
+    let result: Result<usize, std::io::Error> =
+        my_cursor.read_line(&mut my_string);
+    // We can process the Result as needed - here we just print the status.
+    match result {
+        Ok(0) => println!("End of file reached."),
+        Ok(n) => println!("The total number of bytes read is {n}."),
+        Err(ref e) => eprintln!("Error: {}", e), /* We could also retry `read_line` to attempt to recover from the error. */
+    }
+    assert_eq!(my_string, "foo\n");
 
-    // Alternative: `unwrap` panics if there is an error
-    // let _greeting_file = std::fs::File::open("temp/hello.txt").unwrap();
+    // Alternatively, if the error is truly not recoverable, we could cause the
+    // program to crash if this instance of `Result` is an `Err` value.
+    // `expect` panics and displays the message that you passed as an argument
+    // to.
+    result.expect("Failed to read line");
+    // You may also use `unwrap`, which panics if there is an error but does not
+    // display a custom message. result.unwrap();
 }
 // ANCHOR_END: example
 
@@ -23,4 +33,3 @@ fn main() {
 fn test() {
     main();
 }
-// [error_handling: review (P0)](https://github.com/john-cd/rust_howto/issues/169)
