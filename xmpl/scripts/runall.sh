29f681ec27cf579f04c98bdec6c37e4661c16591
John <john-cd@users.noreply.github.com>
2025-02-10 22:09:39 +0000

    fix justfiles and Cargo.toml, part 2

--- xmpl/scripts/runall.sh
+++ xmpl/scripts/runall.sh
@@ -8,8 +8,8 @@ set -o pipefail
 set -e
 
 root="$(realpath $1)/"
-# Create a list of the (last part of) folder names under the `crates/xmpl` directory, space separated
-xmpl=$(find ${root}crates/xmpl -mindepth 1 -maxdepth 1 -type d | awk -F'/' '{print $(NF)}' | tr '\n' ' ')
+# Create a list of the (last part of) folder names under the `xmpl` directory, space separated
+xmpl=$(find ${root} -mindepth 1 -maxdepth 1 -type d -not -name "target" -not -name "scripts" | awk -F'/' '{print $(NF)}' | tr '\n' ' ')
 
 # Run additional examples in the xmpl folder, if any
 for d in $xmpl; do ( echo $d; cargo run --package $d --locked ); done
