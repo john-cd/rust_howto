a5b583545a948e8865133a110b41c8a598c08950
John CD <john-cd@users.noreply.github.com>
2025-02-13 11:21:18 -0800

    finish crates_io example; close #729

--- bk/crates/cats/development_tools/tests/crate_registries/crates_io.rs
+++ bk/crates/cats/development_tools/tests/crate_registries/crates_io.rs
@@ -6,62 +6,68 @@ use crates_io_api::Sort;
 use crates_io_api::SyncClient;
 use tracing::warn;
 
-/// Instantiate the crates.io API client.
+/// Instantiate the 'crates.io' API client.
 fn get_client() -> Result<SyncClient> {
     let client = SyncClient::new(
         "my-user-agent (my-contact@domain.com)",
-        std::time::Duration::from_millis(1000), // rate limit interval
+        std::time::Duration::from_millis(1000), // Rate limit interval
     )?;
     Ok(client)
 }
 
-/// Returns information for a crate, given its name
+/// Returns information for a crate, given its name.
 fn get_info_for_crate(crate_name: &str) -> Result<CrateResponse> {
     let client = get_client()?;
-    warn!("Calling crates.io API for {crate_name}");
+    warn!("Calling the 'crates.io' API for {crate_name}");
     let crt = client.get_crate(crate_name)?;
     Ok(crt)
 }
 
-/// List, for the most downloaded crates, the top dependencies
+/// List, for the most downloaded crates, the top dependencies.
 fn list_top_dependencies() -> Result<()> {
     let client = get_client()?;
     // Retrieve summary data.
     let summary = client.summary()?;
     for c in summary.most_downloaded {
-        println!("{}:", c.id);
+        println!("{} - {}:", c.id, c.downloads);
         for dep in client.crate_dependencies(&c.id, &c.max_version)? {
             // Ignore optional dependencies.
             if !dep.optional {
-                println!("    * {} - {}", dep.id, dep.version_id);
+                println!("    * {}", dep.crate_id);
             }
         }
     }
     Ok(())
 }
 
-/// Search for crates
+/// Search for crates.
 fn search_for_crates(search: impl Into<String>) -> Result<CratesPage> {
     let client = get_client()?;
 
+    // Build a query first
     let q = crates_io_api::CratesQuery::builder()
-    .sort(Sort::Alphabetical)
-    //.page_size(size)
-    .search(search)
-    .build();
+        .sort(Sort::Relevance)
+        .page_size(1) // To limit the output size of this example
+        .search(search)
+        // You can also specify .category("category-slug")
+        .build();
+    // Retrieve a page of crates
     let crates = client.crates(q)?;
     Ok(crates)
 }
 
 fn main() -> Result<()> {
-    println!("Search:");
-    search_for_crates("serde")?;
+    // Search for crates that mention "signature verification"
+    println!("Search:\n");
+    let result = search_for_crates("signature verification")?;
+    println!("{:#?}", result);
 
-    println!("Info:");
-    let info = get_info_for_crate("approx")?;
+    // Display information for a crate
+    println!("\n\nInfo:\n");
+    let info = get_info_for_crate("wgsldoc")?;
     println!("{:#?}", info);
 
-    println!("Top dependencies for the most downloaded crates:");
+    println!("\n\nTop dependencies for the most downloaded crates:\n");
     list_top_dependencies()?;
 
     Ok(())
@@ -73,4 +79,3 @@ fn test() -> anyhow::Result<()> {
     main()?;
     Ok(())
 }
-// [P0](https://github.com/john-cd/rust_howto/issues/729) fix search
