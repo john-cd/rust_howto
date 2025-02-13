f8ed3505cd50ebc8add3179478c848ac180cda37
John CD <john-cd@users.noreply.github.com>
2025-02-13 12:29:40 -0800

    finish tungstenite example; close #874

--- bk/crates/cats/web_programming_websocket/tests/tungstenite.rs
+++ bk/crates/cats/web_programming_websocket/tests/tungstenite.rs
@@ -1,39 +1,38 @@
-// // ANCHOR: example
-// COMING SOON
-// // ANCHOR_END: example
+// ANCHOR: example
+use tungstenite::Message;
+use tungstenite::connect;
 
-// use tungstenite::Message;
-// use tungstenite::connect;
-// use url::Url;
+// Lightweight, flexible WebSockets for Rust
 
-// // Lightweight, flexible WebSockets for Rust
+fn main() -> anyhow::Result<()> {
+    // Connect to a test WebSocket server in blocking mode
+    let (mut socket, response) = connect("ws://echo.websocket.in")?;
+    // To support secure "wss://"" URLs, feature "native-tls" or "rustls-tls"
+    // must be turned on. If you want a non-blocking or other custom stream,
+    // call `client` instead.
 
-// fn main() -> anyhow::Result<()> {
-//     // Connect to the WebSocket server
-//     let (mut socket, response) =
-//         connect(Url::parse("ws://echo.websocket.org")?)?;
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, value) in response.headers() {
+        println!("* {} {:?}", header, value);
+    }
 
-//     println!("Connected to the server");
-//     println!("Response HTTP code: {}", response.status());
-//     println!("Response contains the following headers:");
-//     for (ref header, _value) in response.headers() {
-//         println!("* {}", header);
-//     }
+    // Send a message to the server:
+    // Writes and immediately flushes a message.
+    socket.send(Message::Text("Hello WebSocket".into()))?;
+    println!("Message sent");
 
-//     // Send a message to the server
-//     socket.send(Message::Text("Hello WebSocket".into()))?;
-//     println!("Message sent");
+    // Read a message from the server
+    let msg = socket.read()?;
+    println!("Received: {}", msg);
 
-//     // Read a message from the server
-//     let msg = socket.read()?;
-//     println!("Received: {}", msg);
+    Ok(())
+}
+// ANCHOR_END: example
 
-//     Ok(())
-// }
-
-// #[test]
-// fn require_network()  -> anyhow::Result<()> {
-//     main()?;
-//     Ok(())
-// }
-// // [P1](https://github.com/john-cd/rust_howto/issues/874)
+#[test]
+fn require_network() -> anyhow::Result<()> {
+    main()?;
+    Ok(())
+}
