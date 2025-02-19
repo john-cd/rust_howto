4249168e446492fc3143e7de0e32a26028ec6a66
John <john-cd@users.noreply.github.com>
2025-02-19 19:06:13 +0000

    edit rocksdb example; close #1024

--- bk/crates/cats/database/tests/rocksdb/rocksdb.rs
+++ bk/crates/cats/database/tests/rocksdb/rocksdb.rs
@@ -1,35 +1,37 @@
 // ANCHOR: example
 
-// Rust wrapper for Facebook's RocksDB embeddable database
+// Example using Facebook's RocksDB embeddable database.
 
 // Ensure you have the RocksDB C++ library installed on your system, because the
-// Rust rocksdb crate is a wrapper around the native C++ RocksDB library.
-// If you're on Ubuntu, you can install it with:
+// Rust `rocksdb` crate is just a wrapper around the native C++ RocksDB library.
+// If you're on `Ubuntu`, you can install it with:
 // sudo apt-get install librocksdb-dev
-// Requirements: Clang and LLVM
+// You will also need: Clang and LLVM.
 
 use rocksdb::DB;
 use rocksdb::Options;
 
 fn main() -> anyhow::Result<()> {
     // Create a new temporary directory to store the database,
-    // which will be deleted when tempdir goes out of scope
+    // which will be deleted when `tempdir` goes out of scope
     let tempdir = tempfile::Builder::new()
         .prefix("rocksdb_storage")
         .tempdir()
-        .expect("Failed to create temporary directory for rocksdb storage");
+        .expect(
+            "Failed to create the temporary directory for `rocksdb` storage",
+        );
     let path = tempdir.path();
     // In real life, use e.g.: let path = "my_rocksdb_path";
     {
-        // Open a RocksDB database
-        // (this will create a new database if it doesn't exist)
+        // Open a RocksDB database.
+        // This will create a new database if it doesn't exist.
         let db = DB::open_default(path)?;
 
-        // Insert some key-value pairs
+        // Insert some key-value pairs:
         db.put(b"key1", b"value1")?;
         db.put(b"key2", b"value2")?;
 
-        // Retrieve a value by its key
+        // Retrieve a value by its key:
         match db.get(b"key1")? {
             Some(value) => println!(
                 "Found key1 with value: {}",
@@ -38,10 +40,10 @@ fn main() -> anyhow::Result<()> {
             None => println!("key1 not found"),
         }
 
-        // Delete a key-value pair
+        // Delete a key-value pair:
         db.delete(b"key1")?;
 
-        // Try to get the deleted key
+        // Try to get the deleted key:
         match db.get(b"key1")? {
             Some(value) => println!(
                 "Found key1 with value: {}",
@@ -61,4 +63,3 @@ fn test() -> anyhow::Result<()> {
     main()?;
     Ok(())
 }
-// [ P2 write](https://github.com/john-cd/rust_howto/issues/1024)
