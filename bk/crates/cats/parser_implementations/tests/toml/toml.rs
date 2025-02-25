3d451398256d7a810526fe99587658a7ee421df2
John <john-cd@users.noreply.github.com>
2025-02-25 22:50:53 +0000

    toml example; close #1093

--- bk/crates/cats/parser_implementations/tests/toml/toml.rs
+++ bk/crates/cats/parser_implementations/tests/toml/toml.rs
@@ -1,13 +1,41 @@
+#![allow(dead_code)]
 // ANCHOR: example
-// COMING SOON
-// ANCHOR_END: example
-fn main() {
-    todo!();
+use serde::Deserialize;
+use toml::value::Datetime;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    title: String,
+    database: Database,
 }
 
+#[derive(Deserialize, Debug)]
+struct Database {
+    server: String,
+    ports: Vec<u32>,
+    connection_max: u32,
+    enabled: bool,
+    date: Datetime,
+}
+
+fn main() {
+    let toml_string = r#"
+        title = "TOML Example"
+
+        [database]
+        server = "192.168.1.1"
+        ports = [ 8001, 8001, 8002 ]
+        connection_max = 5000
+        enabled = true
+        date = 1979-05-27T07:32:00Z
+    "#;
+
+    let parsed_toml: Config = toml::from_str(toml_string).unwrap();
+    println!("{:#?}", parsed_toml);
+}
+// ANCHOR_END: example
+
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
-// [P1 add example](https://github.com/john-cd/rust_howto/issues/1093)
