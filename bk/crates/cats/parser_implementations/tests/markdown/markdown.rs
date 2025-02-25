ed1c03c9dadc585b8f2ea9baebf8e9cd3e36e76e
John <john-cd@users.noreply.github.com>
2025-02-25 16:49:28 +0000

    markdown example; close #1098

--- bk/crates/cats/parser_implementations/tests/markdown/markdown.rs
+++ bk/crates/cats/parser_implementations/tests/markdown/markdown.rs
@@ -1,13 +1,37 @@
 // ANCHOR: example
-// COMING SOON
-// ANCHOR_END: example
+use markdown::to_html;
+
+// `markdown` is a simple native Rust library for parsing Markdown and
+// outputting HTML.
+//
+// Add to your `Cargo.toml`:
+// [dependencies]
+// markdown = "0.3" # You may also try 1.0.0-alpha.xx
+
 fn main() {
-    todo!();
+    let markdown_text = r#"
+# Hello, Markdown!
+
+This is a simple paragraph with **bold** and *italic* text.
+
+* List item 1
+* List item 2
+
+```rust
+fn example() {
+    println!("Hello from Rust!");
 }
+```
+"#;
+
+    let html_output = to_html(markdown_text);
+    // Also consider: `markdown::file_to_html`, `markdown::tokenize`
+
+    println!("{}", html_output);
+}
+// ANCHOR_END: example
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
-// [P1 add example](https://github.com/john-cd/rust_howto/issues/1098)
