fe3c9eac9ebc02aa20302a707fb1e08534aee722
John <john-cd@users.noreply.github.com>
2025-03-14 01:51:00 +0000

    update link replacement scripts

--- bk/scripts/links/replace_inline_links.sh
+++ bk/scripts/links/replace_inline_links.sh
@@ -1,18 +1,42 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# TODO FIX do not convert links to GitHub issues
-# respect conventions
-# move refdefs into central file
+# WORK IN PROGRESS
+# - Convert inline links e.g. [...](http://...) into reference-style links: [...][...] [...]: http://...
+# - Process http://... naked links
 
-# (Rough) Convert inline links e.g. [...](http://...) into reference-style links: [...][...] [...]: http://...
+# Example of URLs to process:
+# https://arangodb.com/
+# https://crates.io/crates/sd
+# https://github.com/rustdesk/rustdesk
+
+# TODO fix
+# 1. do not convert links to GitHub issues e.g. https://github.com/john-cd/rust_howto/issues links
+# 2. skip text between ``` and ```
+# p = print; t = branch; : = label
+# '/```/, /```/n; tx;     ; :x ;' "${file}"
+# 3. move refdefs into central file
+# 4. respect conventions
 
 root="$(realpath $1)/"
 
-for file in $(find ${root}src -type f \( -name "*.md" -not -name "SUMMARY.md" \) )
+for file in $( find ${root}src ${root}drafts -type f -name "*.md"  -not -name "refs.incl.md" -not -name "SUMMARY.md" -not -name "*refs.md" )
 do
-  #echo ">> $file"
-  sed -E -i 's~\[(`)?([^`]+?)(`)?\]\((.+?)\)~[\1\2\3][\L\2] \n[\2]: \4\n~g' $file
+  echo ">> $file"
+  # Replace naked URLs by reference-style link
+  # Trick: '\'' to escape ' in bash
+  sed -n -E -s '\~(^|[^"'\''(])https?://~ {
+  # replace https::/github.com/.../...
+  s~([^"'\''(]?https?://github.com/)([^/ ]+/)([^/ ]+)(/[^"'\'')⮳]*)?~[`\3`][\3-github] [\3-github]: \1\2\3\4~gp;
+  # skip to end if the above matched
+  tx;
+  # General case http://...
+  s~([^"'\''(]?https?://)([^/\s]+)(/[^"'\'')⮳]*)?~[\2][\2] [\2]: \1\2\3~gp ;
+  :x
+  }
+  ' "${file}"
+  # Replace [...](...)
+  #sed -E -n 's~\[(`)?([^`]+?)(`)?\]\((.+?)\)~[\1\2\3][\L\2] \n[\2]: \4\n~gp' "${file}" # use -i to replace in-place
 done
 
 echo "DONE"
--- bk/scripts/links/suggest_links_for_bare_urls.sh
+++ bk/scripts/links/suggest_links_for_bare_urls.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -u
 
+# WORK IN PROGRESS
 # Create reference-style links and reference definitions, which can be used to replace bare URLs in the Markdown
 # Manual review necessary
 # Requires ripgrep
@@ -8,16 +9,22 @@ set -euo pipefail
 # Usage: ./scripts/urls/list_bare_urls.sh <root folder>
 
 root="$(realpath $1)/"
-# Look for http(s)://... and outputs references
-rg --pcre2 --no-line-number --no-filename --only-matching '(?<!: |["`([])(http(?:s)?://(?:www\d?\.|github\.com/)?)([^./]+)(\S+)?' ${root}src \
--g '*.md' -g '!*refs.md' -g 'refs.incl.md' -r '[$2-website]: $1$2$3' | sort | sed 's~/$~~'
 
-# Outputs reference-style links
-rg --pcre2 --no-line-number --no-filename --only-matching '(?<!: |["`([])(http(?:s)?://(?:www\d?\.|github\.com/)?)([^./]+)(\S+)?' ${root}src \
--g '*.md' -g '!*refs.md' -g 'refs.incl.md' -r '[`$2`][$2-website]' | sort | sed 's~/$~~'
-# --pcre2 = Perl regex enabled (allows look-arounds) -g = glob, -r = replace
+# TODO pass a var
+#pattern=('(?<!: |["`([])(http(?:s)?://(?:www\d?\.|github\.com/)?)([^./]+)(\S+)?')
+#"${pattern[@]}"
+
+for file in $( find ${root}src ${root}drafts -type f -name "*.md"  -not -name "refs.incl.md" -not -name "SUMMARY.md" -not -name "*refs.md" )
+do
+  echo ">> $file"
+  contents=$(rg --multiline --invert-match '`.*`' $file)
+  # Look for http(s)://... and outputs references
+  # Outputs reference-style links
+  {
+  echo "${contents}" | rg --pcre2 --only-matching -r '[`$2`][$2-website] [$2-website]: $1$2$3' '(?<!: |["`([])(http(?:s)?://(?:www\d?\.)?)([^./]+)(\S+)?'
+  echo "${contents}" | rg --pcre2 --only-matching -r '[`$2`][$2-github] [$2-github]: $1$2$3' '(?<!: |["`([])(http(?:s)?://(?:github\.com/)?)([^./]+)(\S+)?'
+  # --pcre2 = Perl regex enabled (allows look-arounds) -g = glob, -r = replace
+  } | sed 's~/$~~' | sort
+done
 
 echo "DONE"
-
-# TODO P1 improve; output [...-github] if https://github.com/...
-# TODO P1 ignore base URLs within ```...```
