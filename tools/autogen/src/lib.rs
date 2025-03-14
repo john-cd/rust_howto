5eacb4d12e7ed342c5f41354121953c73df24bb1
John <john-cd@users.noreply.github.com>
2025-03-14 01:55:50 +0000

    WIP - create pest parser

---
+++ tools/autogen/src/lib.rs
@@ -0,0 +1,9 @@
+mod parser;
+// use anyhow::Result;
+// use std::fs;
+
+// fn main() {
+//     let unparsed_file = fs::read_to_string("numbers.csv").expect("cannot read file");
+
+//     // ...
+// }
--- tools/autogen/src/main.rs
+++ tools/autogen/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
-use tool_lib::*;
+// use tool_lib::*;
+
+mod cli;
 
 fn main() {}
 // [ P2 decide what to do](https://github.com/john-cd/rust_howto/issues/1061)
