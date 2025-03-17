5f356bf983e68483fd051696119dd72fa5cfca6c
John <john-cd@users.noreply.github.com>
2025-03-17 19:57:30 +0000

    heck example #1130

--- bk/crates/cats/text_processing/tests/string_manipulation/heck.rs
+++ bk/crates/cats/text_processing/tests/string_manipulation/heck.rs
@@ -1,11 +1,28 @@
 // ANCHOR: example
-// COMING SOON
+
+// Add to Cargo.toml:
+// [dependencies]
+// heck = "0.4.1"
+
+use heck::ToKebabCase;
+use heck::ToLowerCamelCase;
+use heck::ToPascalCase;
+use heck::ToSnakeCase;
+use heck::ToTitleCase;
+
+fn main() {
+    let input = "hello_world example-string";
+
+    println!("Original: {}", input);
+    println!("Snake case: {}", input.to_snake_case()); // hello_world_example_string
+    println!("Kebab case: {}", input.to_kebab_case()); // hello-world-example-string
+    println!("Pascal case: {}", input.to_pascal_case()); // HelloWorldExampleString
+    println!("Camel case: {}", input.to_lower_camel_case()); // helloWorldExampleString
+    println!("Title case: {}", input.to_title_case()); // Hello World Example String
+}
 // ANCHOR_END: example
-fn main() {}
 
 #[test]
-#[ignore = "not yet implemented"]
 fn test() {
     main();
 }
-// [add example](https://github.com/john-cd/rust_howto/issues/1130)
