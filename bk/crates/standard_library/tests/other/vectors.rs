a565e3feecb6e490c782d38392d79e6b83d8fdbc
John <john-cd@users.noreply.github.com>
2025-03-23 23:24:22 +0000

    improve Vec examples #631

--- bk/crates/standard_library/tests/other/vectors.rs
+++ bk/crates/standard_library/tests/other/vectors.rs
@@ -2,22 +2,106 @@
 
 // ANCHOR: example
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    v.push(5);
-    v.push(6);
+    // Creating a `Vec`
+    let _numbers: Vec<i32> = vec![1, 2, 3]; // or vec!(1, 2, 3)
 
-    let mut v = vec![1, 2, 3]; // or vec!(1, 2, 3)
+    // The above is a shortcut for:
+    let mut numbers: Vec<i32> = Vec::new();
+    numbers.push(1);
+    numbers.push(2);
+    numbers.push(3);
 
-    let _third: &i32 = &v[2]; // read
+    // Reading a value at a given index
+    // 1. Panics if out of bounds
+    let _third: &i32 = &numbers[2];
+    // 2. Returns `None` if out of bounds
+    let _third: Option<&i32> = numbers.get(2);
 
-    let _third: Option<&i32> = v.get(2);
+    // Manipulating the `Vec`
+    numbers.push(4);
+    numbers.pop();
 
-    for i in &v {
-        println!("{i}");
+    // Iterating over a `Vec`
+    println!("Iterating using a `for` loop:");
+    for num in &numbers {
+        println!("{}", num);
     }
 
-    for i in &mut v {
-        *i += 50; // dereference operator
+    println!("Iterating using `iter()`:");
+    numbers.iter().for_each(|num| println!("{}", num));
+
+    // Modify the vector while iterating
+    for num in &mut numbers {
+        *num += 50; // Use the dereference operator to access the value
+    }
+
+    // Searching
+    if let Some(index) = numbers.iter().position(|&x| x == 52) {
+        println!("Found the value `52` at index: {}", index);
+    } else {
+        println!("Not found.");
+    }
+
+    // Sorting
+    let mut sorted_numbers = numbers.clone();
+    sorted_numbers.sort();
+    println!("Sorted numbers: {:?}", sorted_numbers);
+
+    // Filtering
+    let even_numbers: Vec<i32> = numbers.iter().filter(|&x| x % 2 == 0).cloned().collect();
+    println!("Even numbers: {:?}", even_numbers);
+
+    // Map
+    let squared_numbers: Vec<i32> = numbers.iter().map(|&x| x * x).collect();
+    println!("Squared numbers: {:?}", squared_numbers);
+
+    // Reduce
+    let sum: i32 = numbers.iter().sum();
+    println!("Sum: {}", sum);
+
+    let product: i32 = numbers.iter().fold(1, |acc, &x| acc * x);
+    println!("Product: {}", product);
+
+    // Slices of a `Vec`
+    let slice: &[i32] = &numbers[1..3];
+    println!("Slice: {:?}", slice);
+
+    // Iterating over slices
+    println!("Iterating over slice:");
+    for num in slice.iter() {
+        println!("{}", num);
+    }
+
+    // Mutable slices
+    let mut mutable_numbers = vec![1, 2, 3, 4, 5];
+    let mutable_slice: &mut [i32] = &mut mutable_numbers[1..4];
+
+    for num in mutable_slice.iter_mut() {
+        *num *= 2;
+    }
+    println!("Mutable numbers after slice mutation: {:?}", mutable_numbers);
+
+    // Chaining iterators for complex operations
+    let numbers2: Vec<i32> = vec![10, 20, 30, 40, 50];
+    let filtered_squared_sum: i32 = numbers2
+        .iter()
+        .filter(|&x| x > &20)
+        .map(|&x| x * x)
+        .sum();
+
+    println!("Filtered squared sum: {}", filtered_squared_sum);
+
+    // Using `enumerate` to get index and value
+    println!("Enumerate:");
+    for (index, value) in numbers2.iter().enumerate() {
+        println!("Index: {}, Value: {}", index, value);
+    }
+
+    // Using `zip` to combine two iterators
+    let letters = vec!['a', 'b', 'c'];
+    println!("Zip:");
+    for (number, letter) in numbers2.iter().zip(letters.iter()) {
+        println!("Number: {}, Letter: {}", number, letter);
     }
 }
 // ANCHOR_END: example
--- bk/src/standard-library/vectors.md
+++ bk/src/standard-library/vectors.md
@@ -37,5 +37,4 @@ Vectors (`Vec`) can only store values that are the same type.
 
 <div class="hidden">
 [vectors: add](https://github.com/john-cd/rust_howto/issues/631)
-Creating, manipulating, iterating, searching, [sorting][p-sorting], filtering, and performing common operations like map/reduce. Include examples of using slices and [iterators][p-iterators] efficiently.
 </div>
