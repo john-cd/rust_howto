1fba152887776de76d90df4c65d6fb973543593e
John CD <john-cd@users.noreply.github.com>
2025-03-26 15:51:10 -0700

    fix: flaky test; make it work in WSL

--- bk/crates/cats/config/tests/env/env.rs
+++ bk/crates/cats/config/tests/env/env.rs
@@ -5,11 +5,12 @@ fn env_extract() -> String {
     let log_env_var = env::var("RUST_LOG").unwrap_or_else(|_| "debug".into());
     println!("RUST_LOG: {log_env_var}");
 
-    let user_env_var = env::var("USER").expect("$USER is not set");
-    println!("USER: {user_env_var}");
+    // You may also choose to panic if the env. variable is not set:
+    // Uncomment to test.
+    // let user_env_var = env::var("USER").expect("$USER is not set");
+    // println!("USER: {user_env_var}");
 
     // Inspect an environment variable at compile-time.
-    // Uncomment to test.
     // let shell = env!("SHELL", "$SHELL is not set");
 
     let optional_value = option_env!("SHELL");
@@ -22,7 +23,6 @@ fn main() {
 }
 // ANCHOR_END: example
 
-#[should_panic]
 #[test]
 fn test() {
     main();
