715acb7e163d55c063b1a56083a6988acaf67cc7
John CD <john-cd@users.noreply.github.com>
2025-03-26 21:58:19 -0700

    crate-indices: list_crates: handle rare case of alias in [dependency] section

--- tools/tool_lib/src/cargo_toml/mod.rs
+++ tools/tool_lib/src/cargo_toml/mod.rs
@@ -1,10 +1,13 @@
 //! Read Cargo.toml files and extract dependencies
 
-use anyhow::Result;
-use cargo_toml::Manifest;
 use std::path::Path;
-use walkdir::WalkDir;
+
+use anyhow::Result;
+use cargo_toml::Dependency;
+use cargo_toml::DepsSet;
+use cargo_toml::Manifest;
 use tracing::debug;
+use walkdir::WalkDir;
 
 /// Return a sorted, deduplicated list of dependencies for the book's code examples.
 ///
@@ -21,27 +24,47 @@ pub fn get_dependencies<P: AsRef<Path>>(root: P) -> Result<Vec<String>> {
             let path = entry.path();
             debug!("Reading {}", path.display());
             let manifest = Manifest::from_path(path)?;
-            // Cargo.toml refers to the workspace Cargo.toml,
+            // Note: Cargo.toml refers to the workspace Cargo.toml,
             // therefore some fields are not available until
             // `cargo package` rewrites it with full details.
-            // let some_fields_unavailable =
-            // manifest.needs_workspace_inheritance();
+            // See  manifest.needs_workspace_inheritance();
+            // In this case, we don't care.
 
-            dependencies.extend(manifest.dependencies.keys().map(|k| String::from(k.trim())));
+            dependencies.extend(extract_crate_names(&manifest.dependencies));
 
-            dependencies.extend(
-                manifest
-                    .build_dependencies
-                    .keys()
-                    .map(|k| String::from(k.trim())),
-            );
+            dependencies.extend(extract_crate_names(&manifest.build_dependencies));
+
             let targets = manifest.target.values();
             for target in targets {
-                dependencies.extend(target.dependencies.keys().map(|k| String::from(k.trim())));
+                dependencies.extend(extract_crate_names(&target.dependencies));
             }
         }
     }
     dependencies.sort();
     dependencies.dedup();
     Ok(dependencies)
+}
+
+/// Extract the crate names from the Dependency Set.
+fn extract_crate_names(depset: &DepsSet) -> Vec<String> {
+    depset
+        .iter()
+        .map(|(k, v)| {
+            match v {
+                // crate_name = "1.2.3"
+                Dependency::Simple(_) => k,
+                // crate_name.workspace = true
+                Dependency::Inherited(_) => k,
+                // alias = { package = "crate_name", ... }
+                Dependency::Detailed(detail) => {
+                    if let Some(ref pkg) = detail.package {
+                        pkg
+                    } else {
+                        k
+                    }
+                }
+            }
+        })
+        .map(|k| String::from(k.trim()))
+        .collect()
 }
