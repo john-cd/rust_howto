6e18abc40a5e0b8485b5949bc58f446f70ae3579
John <john-cd@users.noreply.github.com>
2025-04-01 00:59:06 +0000

    fix backtrace example WIP

--- bk/crates/cats/rust_patterns/tests/error/backtrace.rs
+++ bk/crates/cats/rust_patterns/tests/error/backtrace.rs
@@ -1,9 +1,18 @@
 // ANCHOR: example
+//! # Backtrace
+//!
+//! This example demonstrates how to use backtraces to debug errors.
+//!
+//! ## Example
+//!
+//! The example code defines a struct `Rgb` that represents a color in RGB
+//! format.
 use std::fmt;
 
 use anyhow::Result;
 use anyhow::anyhow;
 use serde::Deserialize;
+// use anyhow::Context;
 
 #[derive(Debug, Deserialize)]
 struct Rgb {
@@ -13,15 +22,19 @@ struct Rgb {
 }
 
 impl Rgb {
+    /// The `Rgb::from_csv` function parses a CSV string and returns an `Rgb`
+    /// struct.
     fn from_csv(csv_data: &[u8]) -> Result<Rgb> {
         let color = csv::Reader::from_reader(csv_data)
-            .deserialize()
+            .deserialize() // Returns a borrowed iterator over deserialized records. Each item is a Result<Rgb, Error>
             .nth(0)
             .ok_or(anyhow!("First CSV row does not exist."))?; // Convert `None` into `Result::Err`.
         Ok(color?)
     }
 }
 
+// The `UpperHex` trait is used to format the `Rgb` struct as a hexadecimal
+// string.
 impl fmt::UpperHex for Rgb {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let hexa = u32::from(self.red) << 16
@@ -31,6 +44,7 @@ impl fmt::UpperHex for Rgb {
     }
 }
 
+// The `main` function parses a CSV string and prints the result.
 fn main() -> Result<()> {
     let csv = "red,blue,green
 102,256,204"; // Note the invalid value.
@@ -42,8 +56,15 @@ fn main() -> Result<()> {
 }
 // ANCHOR_END: example
 
-#[should_panic]
 #[test]
 fn test() {
-    main();
+    unsafe {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+    let res = main();
+    println!("{:?}", res);
+    unsafe {
+        std::env::set_var("RUST_BACKTRACE", "");
+    }
+    assert!(res.is_err());
 }
