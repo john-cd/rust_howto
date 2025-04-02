f2a2898ea0b89ab4c404f26c121f96aad282d7c1
john-cd@users.noreply.github.com <John CD>
2025-04-02 12:37:43 -0700

    document #1156

--- bk/crates/cats/asynchronous/tests/async/async1.rs
+++ bk/crates/cats/asynchronous/tests/async/async1.rs
@@ -5,7 +5,7 @@ struct SomeStruct;
 
 // Most often, we will use async functions.
 // Rust transforms the `async fn` at compile time into a state machine
-// that _implicitly_ returns a `Future`. A future represents an
+// that _implicitly_ returns a `Future`. A `Future` represents an
 // asynchronous computation that might not have finished yet.
 async fn first_task() -> SomeStruct {
     // ...
@@ -18,13 +18,15 @@ async fn second_task_1(_s: &SomeStruct) {
     println!("Second task, part 1");
 }
 
-// `async fn` is really syntaxic sugar for a function...
+// `async fn` is really syntactic sugar for a function...
 #[allow(clippy::manual_async_fn)]
 fn second_task_2() -> impl Future<Output = ()> {
-    // ...that contains an `async` block.
+    // ...that contains an `async` block. An `async` block is a block of code
+    // that can be paused and resumed.
     async {
         println!("Second task, part 2");
-    } // returns `Future<Output = ()>`
+    }
+    // It returns `Future<Output = ()>`
 }
 
 async fn do_something() {
@@ -33,23 +35,26 @@ async fn do_something() {
     // `await` yields control back to the executor, which may decide to do
     // other work if the task is not ready, then come back here.
 
-    // `join!` is like `.await` but can wait for multiple futures
+    // `join!` is like `.await`, but can wait for multiple futures
     // concurrently, returning when all branches complete.
     let f1 = second_task_1(&s);
     let f2 = second_task_2();
-    futures::join!(f1, f2); // or tokio::join!
+    futures::join!(f1, f2); // You could use tokio::join! as well.
 }
 
 // We replace `fn main()` by `async fn main()` and declare which
 // executor runtime we'll use - in this case, Tokio. The runtime crate
-// must be added to `Cargo.toml`: `tokio = { version = "1", features =
-// that transforms it into a synchronous fn main() that initializes a
+// must be added to `Cargo.toml`:
+// `tokio = { version = "1", features = ["full"] }`.
+//
+// The `#[tokio::main]` attribute is a macro that transforms `async fn main()`
+// into a synchronous function that initializes a
 // runtime instance and executes the async main function.
 #[tokio::main]
 async fn main() {
     do_something().await;
-    // note: `await` must be called or nothing is executing.
-    // Futures are lazy
+    // Note: `await` must be called or nothing is executing!
+    // Futures are lazy.
 }
 // ANCHOR_END: example
 
