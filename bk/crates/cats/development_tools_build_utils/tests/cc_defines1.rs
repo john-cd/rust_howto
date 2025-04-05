46f60220e61bfb4d16f1095e19a64e9f5c4d64fb
John CD <john-cd@users.noreply.github.com>
2025-04-04 17:26:51 -0700

    temp. hide example

--- bk/crates/cats/development_tools_build_utils/tests/cc_defines1.rs
+++ bk/crates/cats/development_tools_build_utils/tests/cc_defines1.rs
@@ -1,23 +1,23 @@
-#[allow(dead_code)]
-// ANCHOR: example
-// Declare an external C function.
-unsafe extern "C" {
-    /// This function is defined in C code and prints application information.
-    fn print_app_info();
-}
+// #[allow(dead_code)]
+// // ANCHOR: example
+// // Declare an external C function.
+// unsafe extern "C" {
+//     /// This function is defined in C code and prints application
+// information.     fn print_app_info();
+// }
 
-fn main() {
-    // Call the external C function.
-    unsafe {
-        print_app_info();
-    }
-    println!("Printed app info.");
-}
-// ANCHOR_END: example
+// fn main() {
+//     // Call the external C function.
+//     unsafe {
+//         print_app_info();
+//     }
+//     println!("Printed app info.");
+// }
+// // ANCHOR_END: example
 
-#[ignore = "Needs review"]
-#[test]
-fn test() {
-    main();
-}
-// [finish; deal with extern](https://github.com/john-cd/rust_howto/issues/901)
+// #[ignore = "Needs review"]
+// #[test]
+// fn test() {
+//     main();
+// }
+// // [finish; deal with extern](https://github.com/john-cd/rust_howto/issues/901)
