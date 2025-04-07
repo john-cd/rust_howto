fccbe75f8cb2ed326ed4c01ba7a6cc3ce7418d0c
John CD <john-cd@users.noreply.github.com>
2025-04-07 15:02:25 -0700

    formatting

--- bk/crates/cats/rust_patterns/tests/functional_programming/itertools.rs
+++ bk/crates/cats/rust_patterns/tests/functional_programming/itertools.rs
@@ -1,32 +1,36 @@
 // ANCHOR: example
-//! This example demonstrates the usage of various functions provided by the `itertools` crate.
-    use itertools::Itertools;
-    use itertools::assert_equal;
-    use itertools::chain;
+//! This example demonstrates the usage of various functions provided by the
+//! `itertools` crate.
+use itertools::Itertools;
+use itertools::assert_equal;
+use itertools::chain;
 
 fn main() {
     // `assert_equal`:
     // This function asserts that two iterables produce equal sequences.
     assert_equal("hello world".split(' '), "hello world".split(' '));
 
-    // `chain` takes two iterables and creates a new iterator over both in sequence.
+    // `chain` takes two iterables and creates a new iterator over both in
+    // sequence.
     let mut result: Vec<i32> = Vec::new();
     for element in chain(&[1, 2, 3], &[4]) {
         result.push(*element);
     }
     assert_eq!(result, vec![1, 2, 3, 4]);
 
-    // `cloned` creates an iterator that clones the elements of the original iterator.
-    //  Here, it's used to clone the bytes of the byte string "abc" and check if the first element is 'a'.
+    // `cloned` creates an iterator that clones the elements of the original
+    // iterator.  Here, it's used to clone the bytes of the byte string
+    // "abc" and check if the first element is 'a'.
     assert_eq!(itertools::cloned(b"abc").next(), Some(b'a'));
 
     // `dedup` removes consecutive duplicate elements from an iterator.
-    // Here, it removes consecutive duplicates from the vector [1., 1., 2., 3., 3., 2., 2.]
-    // and asserts that the result is [1., 2., 3., 2.].
+    // Here, it removes consecutive duplicates from the vector [1., 1., 2., 3.,
+    // 3., 2., 2.] and asserts that the result is [1., 2., 3., 2.].
     let data = vec![1., 1., 2., 3., 3., 2., 2.];
     itertools::assert_equal(data.into_iter().dedup(), vec![1., 2., 3., 2.]);
 
-    // `into_group_map` groups elements of an iterator into a map based on a key.
+    // `into_group_map` groups elements of an iterator into a map based on a
+    // key.
     let data = vec![(0, 10), (2, 12), (3, 13), (0, 20), (3, 33), (2, 42)];
     let lookup = data.into_iter().into_group_map();
 
