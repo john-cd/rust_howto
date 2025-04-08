ead7379981f3beb5e11cc29aa93f815ad4a40c55
John CD <john-cd@users.noreply.github.com>
2025-04-08 08:05:21 -0700

    viz. example: document

--- bk/crates/cats/visualization/tests/plotly.rs
+++ bk/crates/cats/visualization/tests/plotly.rs
@@ -1,4 +1,7 @@
 // ANCHOR: example
+//! This example demonstrates how to create a simple scatter plot using the
+//! `plotly` crate.
+
 use std::fs;
 
 use plotly::Layout;
@@ -7,19 +10,19 @@ use plotly::Scatter;
 use plotly::common::Mode;
 
 fn main() -> anyhow::Result<()> {
-    // Create sample data
+    // Sample data.
     let x_data = vec![1, 2, 3, 4, 5];
     let y_data = vec![10, 15, 13, 17, 14];
 
-    // Create a Scatter plot
+    // Create a `Scatter` plot.
     let trace = Scatter::new(x_data, y_data)
         .mode(Mode::LinesMarkers)
         .name("Sample Data");
 
-    // Create a Plot and add the Scatter plot
-    // Plot is a container for structs that implement the Trace trait.
-    // Optionally a Layout can also be specified. Its function is to serialize
-    // Traces and the Layout in html format and display and/or persist the
+    // Create a `Plot` and add the `Scatter` plot.
+    // `Plot` is a container for structs that implement the `Trace` trait.
+    // Optionally a `Layout` can also be specified. Its function is to serialize
+    // `Trace`s and the `Layout` in html format and display and/or persist the
     // resulting plot.
     let mut plot = Plot::new();
     plot.add_trace(trace);
@@ -30,7 +33,7 @@ fn main() -> anyhow::Result<()> {
     if !fs::exists("temp")? {
         fs::create_dir("temp")?;
     }
-    // Save the plot as an HTML file
+    // Save the plot as an HTML file.
     plot.write_html("temp/plot.html");
 
     println!("Plot saved as plot.html");
