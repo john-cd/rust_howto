21381bb96f0aff3947010c748d42b545d1a879d7
John CD <john-cd@users.noreply.github.com>
2025-04-12 15:34:25 -0700

    fake example #1124

--- bk/crates/cats/development_tools_testing/tests/property_based_testing/fake.rs
+++ bk/crates/cats/development_tools_testing/tests/property_based_testing/fake.rs
@@ -5,12 +5,18 @@
 //! This example demonstrates how to use the `fake` crate to generate fake data
 //! for testing. It covers basic usage, generating data for custom structs, and
 //! using different data providers.
+//!
+//! Add to your `Cargo.toml`:
+//! ```toml
+//! fake = { version = "4.3.0", features = [ "derive" ] }
+//! ```
 
 // Trait that generates fake values given a type that implements `Dummy`.
 use fake::Fake;
-// Generate default fake value for given type using `Fake`.
+// Generates default fake value for given type using `Fake`.
 use fake::Faker;
-// Fake value generation for specific formats.
+use fake::Dummy;
+// Fake value generation for specific formats:
 use fake::faker::address::en::CityName;
 use fake::faker::address::en::StreetName;
 use fake::faker::address::en::ZipCode;
@@ -32,18 +38,24 @@ struct User {
     active: bool,
 }
 
+// The `Fake` trait is implemented for any type that implements `Dummy`:
+// Dummy should be implemented instead, and you get the `Fake` implementation for free.
+
 // A structure to represent an Order.
-#[derive(Debug)]
+#[derive(Debug, Dummy)]
 struct Order {
+    #[dummy(faker = "1000..2000")]
     id: u64,
     user_id: u64,
     items: Vec<String>,
     total: f64,
     shipping_address: Address,
     created_at: String,
+    // #[dummy(default)]
+    // other: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Dummy)]
 struct Address {
     street: String,
     city: String,
@@ -55,6 +67,11 @@ fn main() {
     let tuple = Faker.fake::<(u8, u32, f32)>();
     println!("Tuple: {:?}", tuple);
     println!("String: {}", Faker.fake::<String>());
+    assert_eq!(10.fake::<String>().len(), 10);
+    let a: [[u8; 2]; 3] = (1..10).fake();
+    println!("{:?}", a);
+    let b: Option<usize> = (1..10).fake();
+    println!("{:?}", b);
 
     // Generate a random user.
     let user = User {
@@ -81,6 +98,10 @@ fn main() {
     println!("Generated Users: {:#?}", users);
 
     // Generate a random order with random items.
+
+    // type derived Dummy
+// let f: Foo = Faker.fake();
+// println!("{:?}", f);
     let order = Order {
         id: Faker.fake(),
         user_id: user.id,
@@ -99,12 +120,12 @@ fn main() {
     };
     println!("Generated Order: {:#?}", order);
 
-    // Generate random sentences.
+    // Generate random sentences:
     let sentences: Vec<String> =
         (0..3).map(|_| Sentence(3..10).fake()).collect();
     println!("Random Sentences: {:#?}", sentences);
 
-    // Generate random user agents.
+    // Generate random user agents:
     let user_agents: Vec<String> = (0..3).map(|_| UserAgent().fake()).collect();
     println!("User Agents: {:#?}", user_agents);
 }
@@ -113,4 +134,4 @@ fn main() {
 fn test() {
     main();
 }
-// [review https://docs.rs/fake/4.2.0/fake/index.html cover derive, Dummy, complex types... NOW](https://github.com/john-cd/rust_howto/issues/1124)
+// [review https://docs.rs/fake/4.2.0/fake/index.html  NOW](https://github.com/john-cd/rust_howto/issues/1124)
