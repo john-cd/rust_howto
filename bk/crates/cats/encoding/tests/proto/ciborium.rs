e96b30c9a678250cc3d09aefe1416a865665ef73
John <john-cd@users.noreply.github.com>
2025-04-13 00:06:03 +0000

    ciborium example finish #1042

--- bk/crates/cats/encoding/tests/proto/ciborium.rs
+++ bk/crates/cats/encoding/tests/proto/ciborium.rs
@@ -1,10 +1,19 @@
 // ANCHOR: example
 //! This example show how to (de)serialize CBOR data using the ciborium crate.
+//!
+//! CBOR stands for Concise Binary Object Representation, a binary data
+//! serialization format. It is based on the JSON data model but is designed to
+//! be more compact and efficient, making it ideal for applications like the
+//! Internet of Things (IoT) and other scenarios where small message sizes and
+//! fast processing are crucial. CBOR supports numbers, strings, arrays, maps,
+//! and values like true, false, and null. Unlike JSON, it can handle binary
+//! data directly, avoiding the need for encoding methods like 'Base64'.
+
 use std::io::Cursor;
 
 use ciborium::de::from_reader;
 use ciborium::ser::into_writer;
-// Import the `Serialize` and `Deserialize` traits from the serde crate.
+// Import the `Serialize` and `Deserialize` traits from the `serde` crate.
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -15,8 +24,8 @@ struct MyData {
     is_active: bool,
 }
 
-// The main function demonstrates serializing and deserializing a struct to and
-// from CBOR.
+/// Demonstrates serializing and deserializing a struct to and
+/// from CBOR.
 fn main() -> anyhow::Result<()> {
     // Create a sample data object.
     let data = MyData {
@@ -30,6 +39,7 @@ fn main() -> anyhow::Result<()> {
     into_writer(&data, &mut buf)?;
 
     // Deserialize from CBOR.
+    // (the `Cursor` just wraps the in-memory buffer).
     let mut cursor = Cursor::new(&buf[..]);
     let deserialized_data: MyData = from_reader(&mut cursor)?;
 
@@ -45,4 +55,3 @@ fn test() -> anyhow::Result<()> {
     main()?;
     Ok(())
 }
-// [rewiew NOW](https://github.com/john-cd/rust_howto/issues/1042)
