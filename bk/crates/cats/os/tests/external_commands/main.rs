abb418e90f222556b07744b77b06e74af6ce0d64
John CD <john-cd@users.noreply.github.com>
2025-04-16 09:10:11 -0700

    fix read_env_variable example

--- bk/crates/cats/os/tests/external_commands/main.rs
+++ bk/crates/cats/os/tests/external_commands/main.rs
@@ -2,5 +2,6 @@ mod continuous;
 mod error_file;
 mod piped;
 mod process_output;
+mod read_env_variable;
 mod send_input;
 mod which;
--- bk/crates/cats/os/tests/external_commands/read_env_variable.rs
+++ bk/crates/cats/os/tests/external_commands/read_env_variable.rs
@@ -1,28 +1,19 @@
 #![cfg(target_family = "unix")]
 // ANCHOR: example
-//! This example demonstrates how to read an environment variable and use it to
-//! determine the path to a configuration file. If the environment variable is
-//! not set, a default path is used.
+//! This example demonstrates how to read an environment variable.
+//! If the environment variable is not set, a default value is used.
 
 use std::env;
-use std::fs;
-use std::io::Error;
 
-/// Reads a configuration file from a path specified by the `CONFIG` environment
-/// variable, or a default path if the variable is not set.
-fn main() -> Result<(), Error> {
-    let config_path =
-        env::var("CONFIG").unwrap_or("/etc/subversion/config".to_string());
-
-    let config: String = fs::read_to_string(config_path)?;
-    println!("Config: {}", config);
-
-    Ok(())
+/// Reads the `PATH` environment variable, or a default value if the variable is
+/// not set.
+fn main() {
+    let path = env::var("PATH").unwrap_or("".to_string());
+    println!("Path: {}", path);
 }
 // ANCHOR_END: example
 
 #[test]
-fn test() -> anyhow::Result<()> {
-    main()?;
-    Ok(())
+fn test() {
+    main();
 }
