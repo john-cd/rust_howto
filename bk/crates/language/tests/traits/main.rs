5e02f855a85cda1c25d5adf1bad8411c9d7902d2
John CD <john-cd@users.noreply.github.com>
2025-04-24 16:01:01 -0700

    move traits examples

--- bk/crates/language/tests/traits/main.rs
+++ bk/crates/language/tests/traits/main.rs
@@ -1,10 +1,8 @@
 mod associated_types;
 mod const_in_traits;
-mod destructuring;
 mod generic_traits;
 mod newtype;
 mod rpit;
-mod shadowing;
 mod trait_bounds;
 mod traits;
 mod traits2;
@@ -12,4 +10,3 @@ mod traits3;
 mod traits4;
 mod traits5;
 mod traits_as_parameters;
-mod vars_and_consts;
