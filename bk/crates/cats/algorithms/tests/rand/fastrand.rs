8bdbe499ff9e197c74244bb30f4352ecbc0a999c
John CD <john-cd@users.noreply.github.com>
2025-05-10 10:12:49 -0700

    formatting #208

--- bk/crates/cats/algorithms/tests/rand/fastrand.rs
+++ bk/crates/cats/algorithms/tests/rand/fastrand.rs
@@ -11,29 +11,30 @@ use fastrand::Rng;
 fn main() {
     // To get reproducible results on every run,
     // pick an arbitrary number as seed.
-    fastrand::seed(7);
+    fastrand::seed(42);
     // Call a function to generate a number, a boolean, a digit, a character...
-    println!("{} ", fastrand::bool());
+    println!("Boolean: {} ", fastrand::bool());
     // Or create a new random number generator:
     let mut rng = Rng::new();
-    for _ in 0..3 {
-        // Generate a random i32.
-        print!("{} ", rng.u32(..100));
-    }
-    println!();
-    // Generate a random Vec or String:
-    let mut v: Vec<i32> = repeat_with(|| rng.i32(..)).take(10).collect();
-    println!("{:?}", v);
+    // Generate a random i32.
+    println!("u32: {} ", rng.u32(..100));
+    // Generate a random `Vec` or `String`:
+    let mut v: Vec<i32> = repeat_with(|| rng.i32(..)).take(5).collect();
+    println!("Vector: {:?}", v);
     let s: String = repeat_with(fastrand::alphanumeric).take(10).collect();
-    println!("{}", s);
+    println!("String: {}", s);
     // Sample values from an array:
-    println!("{:?}", fastrand::choose_multiple(0..20, 12));
-    // Choose a random element in an array:
+    println!(
+        "Two samples from the 0..20 interval: {:?}",
+        fastrand::choose_multiple(0..20, 2)
+    );
+    // Choose a random element in a vector or array:
     let i = fastrand::usize(..v.len());
     let elem = v[i];
-    println!("{}", elem);
+    println!("Random element: {}", elem);
     // Shuffle an array:
     fastrand::shuffle(&mut v);
+    println!("Shuffled vector: {:?}", v);
 }
 // ANCHOR_END: example
 
