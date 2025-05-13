6663105210f5488a137f10ef8d2d84780223e07c
John CD <john-cd@users.noreply.github.com>
2025-05-13 09:26:25 -0700

    fix and document implement_intoiterator example #546

--- bk/crates/language/tests/iterators/implement_intoiterator.rs
+++ bk/crates/language/tests/iterators/implement_intoiterator.rs
@@ -1,10 +1,15 @@
 // ANCHOR: example
+//! Example of `IntoIterator` implementation.
+
+/// A collection type.
+/// In this example, we simply wrap a `Vec`.
 #[derive(Debug)]
 struct MyVec<T> {
     data: Vec<T>,
 }
 
 impl<T> MyVec<T> {
+    /// Constructor.
     fn new() -> Self {
         MyVec { data: Vec::new() }
     }
@@ -14,37 +19,44 @@ impl<T> MyVec<T> {
     }
 }
 
+/// By implementing `IntoIterator`, you define how the type will be converted to
+/// an iterator.
 impl<T> IntoIterator for MyVec<T> {
     // Associated type for the actual iterator that will be created.
     // It must implement the `Iterator` trait and produce items of type `Item`.
-    type IntoIter = MyVecIterator<T>;
+    type IntoIter = MyVecIntoIterator<T>;
     // Type of the elements that the iterator will yield.
     type Item = T;
 
-    /// The `into_iter` method takes self by value,
+    /// The `into_iter` method takes `self` by value,
     /// meaning it consumes the collection.
     fn into_iter(self) -> Self::IntoIter {
-        MyVecIterator {
-            data: self.data,
-            index: 0,
-        }
+        MyVecIntoIterator { data: self.data }
     }
 }
 
+/// The iterator type.
+/// It is a consuming iterator - it takes ownership of the `MyVec` data.
 #[derive(Debug)]
-struct MyVecIterator<T> {
+struct MyVecIntoIterator<T> {
     data: Vec<T>,
-    index: usize,
+    // Iterator types often also store indices to track the progress of the
+    // iteration. In this simple case, this is not necessary.
 }
 
-impl<T> Iterator for MyVecIterator<T> {
+impl<T> Iterator for MyVecIntoIterator<T> {
     type Item = T;
 
+    /// The core iterator method.
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.data.len() {
-            let result = Some(self.data[self.index].clone()); // Or move if T is not Copy
-            self.index += 1;
-            result
+        if !self.data.is_empty() {
+            // In this example, we use `remove` for simplicity, noting that its
+            // complexity is O(n). `remove` takes the item at the
+            // position n and returns it to us, giving the ownership.
+            // We can always use "0" to return the first element, instead of
+            // storing an index and increasing it sequentially.
+            // We could also use `pop` to remove the last element.
+            Some(self.data.remove(0))
         } else {
             None
         }
@@ -56,6 +68,7 @@ fn main() {
     my_vec.push(1);
     my_vec.push(2);
 
+    // The `for` loop calls `into_iter` under the covers.
     for element in my_vec {
         println!("Element: {}", element);
     }
