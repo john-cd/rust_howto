9327296f685308b1a5425d2955b3ccd672a79a6b
John CD <john-cd@users.noreply.github.com>
2025-05-19 09:06:39 -0700

    code formatting

--- bk/crates/standard_library/tests/option/option_match.rs
+++ bk/crates/standard_library/tests/option/option_match.rs
@@ -10,9 +10,10 @@ fn divide(numerator: f64, denominator: f64) -> Option<f64> {
 fn main() {
     let result1 = divide(10.0, 2.0);
     match result1 {
-        Some(value) => println!("Result: {}", value), /* Handles the `Some`
-                                                        * case */
-        None => println!("Cannot divide by zero!"), // Handles the `None` case
+        // Handles the `Some` case.
+        Some(value) => println!("Result: {}", value),
+        // Handles the `None` case.
+        None => println!("Cannot divide by zero!"),
     }
 }
 // ANCHOR_END: example
