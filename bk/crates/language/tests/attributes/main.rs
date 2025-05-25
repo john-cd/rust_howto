0673eddc9e6e16530e4be17afe4d4bb19c235a50
John CD <john-cd@users.noreply.github.com>
2025-05-25 12:26:27 -0700

    code formatting

--- bk/crates/language/tests/attributes/main.rs
+++ bk/crates/language/tests/attributes/main.rs
@@ -1,7 +1,7 @@
 mod allow_dead_code;
-mod attributes_early_development;
 mod attributes_deprecated;
 mod attributes_derive;
+mod attributes_early_development;
 mod attributes_must_use;
 mod attributes_production;
 mod cfg_if;
