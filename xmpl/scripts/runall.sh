d68b4667ecf64ff09dd7ad0727ef52a5c8558a63
John CD <john-cd@users.noreply.github.com>
2025-06-17 12:54:45 -0700

    clean up

--- xmpl/scripts/runall.sh
+++ xmpl/scripts/runall.sh
@@ -14,12 +14,4 @@ xmpl=$(find ${root} -mindepth 1 -maxdepth 1 -type d -not -name "target" -not -na
 # Run additional examples in the xmpl folder, if any
 for d in $xmpl; do ( echo $d; cargo run --package $d --locked ); done
 
-## The code examples in the book are now implemented as tests (under the `tests` folder).
-## Should you want to add examples in the `examples`folder again, add the following to `runall`:
-#examples=$(find ${root}crates/**/examples -mindepth 1 -maxdepth 1 -type f | xargs basename --suffix=.rs | tr '\n' ' ')
-#for e in $examples; do ( echo $e; cargo run --example $e --locked || true); done
-## Run examples that are in a subfolder of `examples`
-#examples_in_dir=$(find ${root}crates/**/examples -mindepth 1 -maxdepth 1 -type d | xargs basename --multiple | tr '\n' ' ')
-#for e in $examples_in_dir; do ( echo $e; cargo run --example $e --locked || true ); done
-
 echo "DONE"
