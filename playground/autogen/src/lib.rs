13f13629297498785cdb591e5b8a08d08b96ab5b
John CD <john-cd@users.noreply.github.com>
2025-07-01 18:58:37 -0700

    remove autogen

--- playground/autogen/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod parser;
-// use anyhow::Result;
-// use std::fs;
-
-// fn main() {
-//     let unparsed_file = fs::read_to_string("numbers.csv").expect("cannot read file");
-
-//     // ...
-// }
--- playground/autogen/src/main.rs
+++
@@ -1,7 +0,0 @@
-#![allow(unused_imports)]
-// use tool_lib::*;
-
-mod cli;
-
-fn main() {}
-// [decide what to do](https://github.com/john-cd/rust_howto/issues/1061)
