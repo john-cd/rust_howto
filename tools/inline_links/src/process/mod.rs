0ddd5d3466fd517420333907ed7f3e67edb9add7
John CD <john-cd@users.noreply.github.com>
2025-07-01 20:06:45 -0700

    add unit test WIP

--- tools/inline_links/src/process/mod.rs
+++ tools/inline_links/src/process/mod.rs
@@ -37,10 +37,43 @@ where
 
 #[cfg(test)]
 mod tests {
+    // use std::fs::File;
+    // use std::io::Write;
+
+    // use anyhow::Result;
+
     // use super::*;
 
-    // fn test_walk_directory(root: &Path) -> Result<()> {
-    //     walk_directory(root, replace::replace_in_file)?;
+    // #[test]
+    // fn test_walk_directory() -> Result<()> {
+    //     let dir = tempfile::tempdir()?;
+    //     let file_path1 = dir.path().join("test1.md");
+    //     let file_path2 = dir.path().join("test2.md");
+    //     let file_path3 = dir.path().join("test3.txt");
+
+    //     let mut file1 = File::create(&file_path1)?;
+    //     writeln!(file1, "This is test1 https://google.com")?;
+    //     drop(file1);
+
+    //     let mut file2 = File::create(&file_path2)?;
+    //     writeln!(file2, "This is test2 with no match.")?;
+    //     drop(file2);
+
+    //     File::create(&file_path3)?;
+
+    //     walk_directory(dir.path(), replace::replace_in_file)?;
+
+    //     let contents1 = std::fs::read_to_string(&file_path1)?;
+    //     assert_eq!(
+    //         contents1,
+    //         "This is test1 TODO"
+    //     );
+
+    //     let contents2 = std::fs::read_to_string(&file_path2)?;
+    //     assert_eq!(contents2, "This is test2 with no match.\n");
+
+    //     assert!(file_path3.exists());
+    //     dir.close()?;
     //     Ok(())
     // }
 }
