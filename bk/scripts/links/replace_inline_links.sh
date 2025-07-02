a960e9988e8530fca9b674b83b82eed2a1713dd9
John CD <john-cd@users.noreply.github.com>
2025-07-01 18:06:49 -0700

    add comments

--- bk/scripts/links/replace_inline_links.sh
+++ bk/scripts/links/replace_inline_links.sh
@@ -5,14 +5,16 @@ set -euo pipefail
 # - Convert inline links e.g. [...](http://...) into reference-style links: [...][...] [...]: http://...
 # - Process http://... naked links
 
-# Example of URLs to process:
+# Examples of URLs to process:
 # https://arangodb.com/
-
 # https://crates.io/crates/sd
 # https://github.com/rustdesk/rustdesk
+# Examples of URLs to skip:
+# https://github.com/john-cd/rust_howto/issues/1375
+
 
 # [fix](https://github.com/john-cd/rust_howto/issues/1238)
-# 1. do not convert links to GitHub issues e.g. https://github.com/john-cd/rust_howto/issues links
+# 1. do not convert links to GitHub issues e.g. https://github.com/john-cd/rust_howto/issues/1375 links
 # 2. skip text between ``` and ```
 # p = print; t = branch; : = label
 # '/```/, /```/n; tx;     ; :x ;' "${file}"
@@ -21,12 +23,16 @@ set -euo pipefail
 
 root="$(realpath $1)/"
 
-for file in $( find ${root}src ${root}drafts -type f -name "*.md"  -not -name "refs.incl.md" -not -name "SUMMARY.md" -not -name "*refs.md" )
+for file in $( find ${root}src ${root}drafts ${root}later -type f -name "*.md"  -not -name "refs.incl.md" -not -name "SUMMARY.md" -not -name "*refs.md" )
 do
   echo ">> $file"
-  # Replace naked URLs by reference-style link
+  # Replace naked URLs by reference-style link.
+  #
+  # -n = quiet -E = extended regex -s = separate files
+  # \=...= instead of /.../
   # Trick: '\'' to escape ' in bash
-  sed -n -E -s '\~(^|[^"'\''(])https?://~ {
+  # for lines that start with http... or for http... not preceded by " ' or (
+  sed -n -E -s '\=(^|[^"'\''(])https?://= {
   # replace https::/github.com/.../...
   s=([^"'\''(]?https?://github.com/)([^/ ]+/)([^/ ]+)(/[^"'\'')⮳]*)?=[`\3`][\3~github] [\3~github]: \1\2\3\4=gp;
   # skip to end if the above matched
@@ -36,6 +42,7 @@ do
   :x
   }
   ' "${file}"
+
   # Replace [...](...)
   #sed -E -n 's=\[(`)?([^`]+?)(`)?\]\((.+?)\)=[\1\2\3][\L\2] \n[\2]: \4\n=gp' "${file}" # use -i to replace in-place
 done
