b8c9532d955099145d2ffc4d2575e37c66c40674
John CD <john-cd@users.noreply.github.com>
2025-07-27 20:36:58 -0700

    `Any` example #1410

--- bk/crates/standard_library/examples/any/any.rs
+++ bk/crates/standard_library/examples/any/any.rs
@@ -1,11 +1,58 @@
 #![allow(dead_code)]
 // ANCHOR: example
-fn main() {}
+use std::any::Any;
 
+// - `is::<T>(&self) -> bool` checks if the underlying concrete type of the
+//   trait object is `T`.
+// - `downcast_ref::<T>(&self) -> Option<&T>` attempts to downcast the trait
+//   object to a reference `&T`.
+// It returns `Some(&T)` if successful, `None` otherwise.
+// - `downcast_mut::<T>(&mut self) -> Option<&mut T>` is similar to
+//   `downcast_ref`, but provides a mutable reference.
+//
+// All are implemented on `dyn Any`, `dyn Any + Send`, `dyn Any + Send + Sync`.
+fn is_string(s: &dyn Any) -> bool {
+    // `is<T>` is equivalent to `TypeId::of::<T>() == s.type_id()`.
+    let res = s.is::<String>();
+    if res {
+        print!("String: ");
+    } else {
+        println!("Not a string...");
+    }
+    if let Some(string) = s.downcast_ref::<String>() {
+        println!("{string}");
+    }
+    res
+}
+
+// `downcast::<T>(self: Box<Self>) -> Result<Box<T>, Box<Self>>` consumes a
+// `Box<dyn Any>` and attempts to downcast it to a `Box<T>`. `downcast` is
+// implemented on `Box<dyn Any>`, `Box<dyn Any + Send>`, `Box<dyn Any + Send +
+// Sync>`.
+fn print_if_string(value: Box<dyn Any>) {
+    if let Ok(string) = value.downcast::<String>() {
+        println!("String: {string}");
+    }
+}
+
+// Get type name for diagnostics purposes.
+// Note: the returned name may vary with compiler versions.
+fn get_type_name<T>(_: &T) -> String {
+    std::any::type_name::<T>().to_string()
+}
+
+fn main() {
+    assert!(!is_string(&42));
+    let s = "a string".to_string();
+    assert!(is_string(&s));
+
+    print_if_string(Box::new(s));
+
+    println!("{}", get_type_name(&s));
+}
 // ANCHOR_END: example
 
 #[test]
 fn test() {
     main();
 }
-// [write example](https://github.com/john-cd/rust_howto/issues/1410)
--- bk/crates/standard_library/examples/any/type_id.rs
+++ bk/crates/standard_library/examples/any/type_id.rs
@@ -3,9 +3,11 @@
 use std::any::Any;
 use std::any::TypeId;
 
-// The `'static` bound is necessary, because `TypeId` only works with `'static` types.
+// `of` returns the `TypeId` of the generic type parameter.
+//
+// The `'static` bound below is necessary,
+// because `TypeId` only works with `'static` types.
 fn is_same_type<T: 'static, U: 'static>() -> bool {
-    // `of` returns the `TypeId` of the generic type parameter.
     TypeId::of::<T>() == TypeId::of::<U>()
 }
 
@@ -14,18 +16,20 @@ fn main() {
     println!("i32 vs i32: {}", is_same_type::<i32, i32>()); // true.
     println!("i32 vs u32: {}", is_same_type::<i32, u32>()); // false.
 
-    // `Any` can be used to get a `TypeId`:
+    // The `Any` trait can be used to get a `TypeId`:
     let value: &dyn Any = &"hello";
     let tid = value.type_id();
     // `TypeId` implements `Debug`:
     println!("TypeId of value: {tid:?}");
 
-    // Given a smart pointer containing `dyn Any`,
+    // However, given a smart pointer containing `dyn Any`,
+    // calling `.type_id()` on the smart pointer will produce the `TypeId` of
+    // the container, not the underlying object:
     let boxed: Box<dyn Any> = Box::new(3_i32);
-    // Calling `.type_id()` on the smart pointer will produce the `TypeId` of the container, not the underlying object.
     let boxed_id = boxed.type_id();
     // To get the `TypeId` of the inner value,
-    // use `&*` to first dereference the `Box` (calling `Deref`) and then immediately re-borrow.
+    // use `&*` to first dereference the `Box` (calling `Deref`) and then
+    // immediately re-borrow.
     let actual_id = (&*boxed).type_id();
 
     assert_eq!(actual_id, TypeId::of::<i32>());
