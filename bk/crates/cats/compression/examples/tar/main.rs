409f7c193f458b7f91937ffcdfde423f33d6f1e2
John CD <john-cd@users.noreply.github.com>
2025-08-06 11:33:24 -0700

    fix: missing main when compiling in Windows

--- bk/crates/cats/compression/examples/tar/main.rs
+++ bk/crates/cats/compression/examples/tar/main.rs
@@ -1,15 +1,19 @@
-#![cfg(target_family = "unix")]
-
 mod tar_compress;
 mod tar_decompress;
 mod tar_strip_prefix;
 
+#[cfg(target_family = "unix")]
 fn main() -> anyhow::Result<()> {
     tar_compress::main()?;
     // the following requires the archive created above.
     tar_strip_prefix::main()?;
     tar_decompress::main()?;
 
+    Ok(())
+}
+
+#[cfg(not(target_family = "unix"))]
+fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
