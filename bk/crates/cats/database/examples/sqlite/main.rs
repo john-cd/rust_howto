de8c0d4d2c6100eac250e7b78fdd16bc005d8149
John CD <john-cd@users.noreply.github.com>
2025-08-19 13:02:39 -0700

    fix sqlite example (Windows)

--- bk/crates/cats/database/examples/sqlite/main.rs
+++ bk/crates/cats/database/examples/sqlite/main.rs
@@ -1,14 +1,21 @@
-#![cfg(target_os = "linux")]
-
+#[cfg(target_os = "linux")]
 mod initialization;
+#[cfg(target_os = "linux")]
 mod insert_select;
+#[cfg(target_os = "linux")]
 mod transactions;
 
+#[cfg(target_os = "linux")]
 fn main() -> anyhow::Result<()> {
     let _ = std::fs::remove_file("temp/cats.db");
     initialization::main()?;
     insert_select::main()?;
     transactions::main()?;
+    Ok(())
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
