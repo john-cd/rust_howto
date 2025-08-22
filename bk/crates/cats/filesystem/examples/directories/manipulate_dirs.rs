8978c2c70425c2e536b08bcb004e7c84a10c40a1
John CD <john-cd@users.noreply.github.com>
2025-08-22 16:22:00 -0700

    manipulate_dirs: add more

--- bk/crates/cats/filesystem/examples/directories/manipulate_dirs.rs
+++ bk/crates/cats/filesystem/examples/directories/manipulate_dirs.rs
@@ -4,9 +4,8 @@ use std::fs;
 use std::path::Path;
 
 fn main() -> std::io::Result<()> {
-    let path = Path::new("./example_dir");
-
     // 1. Create a directory:
+    let path = Path::new("./example_dir");
     if !path.exists() {
         fs::create_dir(path)?;
         // This function will return an error e.g. if the parent directory
@@ -16,6 +15,7 @@ fn main() -> std::io::Result<()> {
     } else {
         println!("Directory 'example_dir' already exists.");
     }
+
     // 2. Remove it. The directory must be empty:
     fs::remove_dir(path)?;
     println!("'{}' successfully removed.", path.display());
@@ -23,6 +23,10 @@ fn main() -> std::io::Result<()> {
     // 3. Create a directory and any of its missing parents:
     let path = "./temp/my_files/subfolder";
     fs::create_dir_all(path)?; // Creates 'temp', 'temp/my_files', and 'temp/my_files/subfolder' if they don't exist.
+    // You can also use `DirBuilder`:
+    // std::fs::DirBuilder::new()
+    //     .recursive(true)
+    //     .create(path)?;
 
     // 4. Create a few files for the next step.
     // `write` creates a file if it does not exist, and will entirely replace
@@ -40,9 +44,13 @@ fn main() -> std::io::Result<()> {
         let path = entry.path();
 
         if path.is_dir() {
-            println!("- (Directory) {}", path.display());
+            print!("- (Directory) {} ", path.display());
         } else {
-            println!("- (File)      {}", path.display());
+            print!("- (File)      {} ", path.display());
+        }
+        // Show permissions:
+        if let Ok(metadata) = entry.metadata() {
+            println!("{:?}", metadata.permissions());
         }
     }
 
