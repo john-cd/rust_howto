a62c1247b52c345c41a82b5eedf151cff9aa98d7
John CD <john-cd@users.noreply.github.com>
2025-08-21 23:22:40 -0700

    clean up tempfile.md and .rs - close #361

--- bk/crates/cats/filesystem/examples/tempfile/tempfile.rs
+++ bk/crates/cats/filesystem/examples/tempfile/tempfile.rs
@@ -1,46 +1,86 @@
 #![allow(dead_code)]
 // ANCHOR: example
-//! Demonstrates the use of the `tempfile` crate for creating temporary files
-//! and directories.
-
+use std::fs::File;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
 use std::io::Write;
 
 use tempfile::Builder;
+use tempfile::NamedTempFile;
+use tempfile::TempDir;
+use tempfile::spooled_tempfile;
 use tempfile::tempdir;
 use tempfile::tempfile;
 
 fn main() -> std::io::Result<()> {
-    // 1. Create a temporary file (inside of `env::temp_dir()`). It will be
-    //    automatically removed by the OS when the last handle to it is closed.
-    let mut temp_file = tempfile()?;
+    // 1. Create a temporary file (inside of `env::temp_dir()`).
+    // Use `tempfile()` when you need a real `File` but don't need to refer to
+    // it by path.
+    let mut temp_file: File = tempfile()?;
     writeln!(temp_file, "This is temporary.")?;
 
-    // The file is automatically deleted when `temp_file` goes out of scope.
+    // Seek to start:
+    temp_file.seek(SeekFrom::Start(0))?;
 
-    // 2. Create a temporary directory (inside of `env::temp_dir()`).
-    let temp_dir = tempdir()?;
-    let temp_file_path = temp_dir.path().join("temporary_file");
-    let mut temp_file = std::fs::File::create(temp_file_path)?;
+    // Read it:
+    let mut buf = String::new();
+    temp_file.read_to_string(&mut buf).unwrap();
+    assert_eq!(buf, "This is temporary.\n");
+
+    // The temporary file will be automatically removed by the OS when the last
+    // handle to it is closed / goes out of scope. The above doesn't rely on
+    // Rust destructors being run, so will (almost) never fail to clean up
+    // the temporary file.
+
+    // 2. Use `spooled_tempfile()` when you need an in-memory buffer that will
+    //    ultimately be backed by a temporary file if it gets too large. This
+    //    variant is also secure/reliable in the presence of a pathological
+    //    temporary file cleaner.
+    let mut file = spooled_tempfile(15);
+    writeln!(file, "short")?;
+    assert!(!file.is_rolled());
+    // As a result of the following write call, the size of the data will exceed
+    // `max_size` (15), so it will be written to a temporary file on disk,
+    // and the in-memory buffer will be dropped:
+    writeln!(file, "much longer than the max size")?;
+    assert!(file.is_rolled());
+
+    // 3. Create a temporary directory (inside of `env::temp_dir()`).
+    // Use `tempdir()` when you need a temporary directory that will be
+    // recursively deleted on drop.
+    let temp_dir: TempDir = tempdir()?;
+    let temp_file_path = temp_dir.path().join("temporary_file.txt");
+    let mut temp_file = File::create(temp_file_path)?;
     writeln!(temp_file, "This is also temporary.")?;
 
-    // By closing the `TempDir` explicitly, we can check that it has
+    // `TempDir` rely on Rust destructors for cleanup. Destructors may fail to
+    // run if the process exits through an unhandled signal interrupt, or if the
+    // instance is declared statically, among other possible reasons.
+    // a. Make sure that handles to files inside the directory are dropped
+    // before the `TempDir` goes out of scope.
+    drop(temp_file);
+    // b. By closing the `TempDir` explicitly, we can check that it has
     // been deleted successfully. If we don't close it explicitly,
     // the directory will still be deleted when `tmp_dir` goes out
     // of scope, but we won't know whether deleting the directory
     // succeeded.
-    drop(temp_file);
     temp_dir.close()?;
 
-    // 3. Create a named temporary file Note: NamedTempFile rely on Rust
-    //    destructors for cleanup. This means that the file will be deleted when
-    //    the variable goes out of scope. They may fail to run if the process
-    //    exits through an unhandled signal interrupt.
+    // 4. Use `NamedTempFile::new()` when you need a named temporary file that
+    //    can be refered to its path. `NamedTempFile` also rely on Rust
+    //    destructors for cleanup, which may fail. Read the documentation and
+    //    prefer `tempfile()` in most cases.
+    let temp_file0 = NamedTempFile::new()?;
+    println!("Temporary file created at: {:?}", temp_file0.path());
+
+    // Use `Builder` to create a new temporary file (or directory) with custom
+    // options.
     let mut temp_file = Builder::new()
         .prefix("my-temporary-file")
         .suffix(".txt")
         .rand_bytes(6)
         .tempfile()?;
-    // or simpler: let mut temp_file = tempfile::NamedTempFile::new()?;
 
     writeln!(temp_file, "Hello, named temporary file!")?;
 
@@ -48,16 +88,24 @@ fn main() -> std::io::Result<()> {
     println!("Temporary file created at: {:?}", temp_file.path());
 
     // Read the content back from the named temporary file.
+    // `NamedTempFile::reopen()` guarantees that the re-opened file is the same
+    // file, even in the presence of pathological temporary file cleaners.
+    // This function is also useful when you need multiple independent handles
+    // to the same file (useful for consumer/producer patterns).
     let mut file = temp_file.reopen()?;
     let mut content = String::new();
-    std::io::Read::read_to_string(&mut file, &mut content)?;
+    Read::read_to_string(&mut file, &mut content)?;
 
-    // Print the content of the named temporary file
+    drop(temp_file);
+
+    // Print the content of the named temporary file:
     println!("Content of temporary file: {content}");
+
+    drop(file);
 
     Ok(())
 }
-// Adapted from <https://docs.rs/tempfile>
+// Examples adapted from <https://docs.rs/tempfile>
 // ANCHOR_END: example
 
 #[test]
--- bk/src/categories/filesystem/tempfile.md
+++ bk/src/categories/filesystem/tempfile.md
@@ -6,7 +6,9 @@
 
 [![tempfile~website][c~tempfile~website~badge]][c~tempfile~website] [![tempfile][c~tempfile~docs~badge]][c~tempfile~docs] [![tempfile~crates.io][c~tempfile~crates.io~badge]][c~tempfile~crates.io] [![tempfile~github][c~tempfile~github~badge]][c~tempfile~github] [![tempfile~lib.rs][c~tempfile~lib.rs~badge]][c~tempfile~lib.rs]{{hi:tempfile}}{{hi:Filesystem}}{{hi:Tempfile}}
 
-[`tempfile`][c~tempfile~docs]↗{{hi:tempfile}} supports both temporary files and temporary directories.
+The following example demonstrates the use of the [`tempfile`][c~tempfile~docs]↗ crate for creating temporary files and directories that are automatically deleted when no longer referenced.
+
+Note that temporary files and directories have subtle security and resource leaking risks. Caveat lector!
 
 ```rust,editable
 {{#include ../../../crates/cats/filesystem/examples/tempfile/tempfile.rs:example}}
@@ -20,5 +22,4 @@
 {{#include ../../refs/link-refs.md}}
 
 <div class="hidden">
-[tempfile: write](https://github.com/john-cd/rust_howto/issues/361)
 </div>
