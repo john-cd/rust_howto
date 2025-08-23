4c9c790879b15fc6f19012a9aa2cdfdd471afc7b
John CD <john-cd@users.noreply.github.com>
2025-08-22 17:37:15 -0700

    add canonicalize example

--- bk/crates/cats/filesystem/examples/paths/canonicalize.rs
+++ bk/crates/cats/filesystem/examples/paths/canonicalize.rs
@@ -1,6 +1,18 @@
 #![allow(dead_code)]
 // ANCHOR: example
-fn main() -> anyhow::Result<()> {
+use std::fs;
+use std::path::PathBuf;
+
+fn main() -> std::io::Result<()> {
+    let path = "./temp/examples";
+    fs::create_dir_all(path)?;
+
+    // Return the canonical, absolute form of a path with all intermediate components normalized and symbolic links resolved.
+    // The path must exist.
+    let normalized_path: PathBuf =
+        fs::canonicalize("./temp/examples/../examples")?;
+    println!("Canonical path: {}", normalized_path.display());
+
     Ok(())
 }
 // ANCHOR_END: example
