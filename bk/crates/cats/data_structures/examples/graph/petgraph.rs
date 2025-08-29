722bbff4750241b0c218ff2f5ed1c68b862fefb7
John CD <john-cd@users.noreply.github.com>
2025-08-29 09:07:53 -0700

    polish graph WIP #1346

--- bk/crates/cats/data_structures/examples/graph/petgraph.rs
+++ bk/crates/cats/data_structures/examples/graph/petgraph.rs
@@ -1,17 +1,5 @@
 #![allow(dead_code)]
 // ANCHOR: example
-//! This example demonstrates the basic usage of the `petgraph` crate for
-//! creating and manipulating graphs in Rust.
-//!
-//! It covers creating a graph, adding nodes and edges, traversing the graph,
-//! and using different graph types.
-//!
-//! `petgraph` provides several graph types (each differing in the tradeoffs
-//! taken in their internal representation), algorithms on those graphs, and
-//! functionality to output graphs in graphviz format. Both nodes and edges can
-//! have arbitrary associated data, and edges may be either directed or
-//! undirected.
-
 use petgraph::Directed;
 use petgraph::Undirected;
 use petgraph::algo::dijkstra;
@@ -20,29 +8,29 @@ use petgraph::dot::Dot;
 use petgraph::graph::Graph;
 
 fn main() {
-    // Create a new directed graph.
+    // Create a new directed graph:
     let mut graph = Graph::<&str, &str, Directed>::new();
 
-    // Add nodes to the graph.
+    // Add nodes to the graph:
     let a = graph.add_node("Node A");
     let b = graph.add_node("Node B");
     let c = graph.add_node("Node C");
     let d = graph.add_node("Node D");
 
-    // Add edges between nodes.
+    // Add edges between nodes:
     graph.add_edge(a, b, "Edge AB");
     graph.add_edge(b, c, "Edge BC");
     graph.add_edge(c, d, "Edge CD");
     graph.add_edge(d, a, "Edge DA");
 
-    // Find the shortest path from `b` using `1` as the cost for every edge.
+    // Find the shortest path from `b` using `1` as the cost for all edges:
     let node_map = dijkstra(&graph, b, Some(d), |_| 1);
     println!("{node_map:?}");
 
-    // Print the graph in DOT format.
+    // Print the graph in DOT format:
     println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
 
-    // Traverse the graph.
+    // Traverse the graph:
     println!("\nTraversing the graph:");
     for node in graph.node_indices() {
         println!("Node: {:?}", graph[node]);
@@ -51,16 +39,16 @@ fn main() {
         }
     }
 
-    // Create an undirected graph.
+    // Create an undirected graph:
     let mut undirected_graph =
         Graph::<&str, &str, Undirected>::new_undirected();
 
-    // Add nodes to the undirected graph.
+    // Add nodes to the undirected graph:
     let e = undirected_graph.add_node("Node E");
     let f = undirected_graph.add_node("Node F");
     let g = undirected_graph.add_node("Node G");
 
-    // Add edges to the undirected graph.
+    // Add edges to the undirected graph:
     undirected_graph.add_edge(e, f, "Edge EF");
     undirected_graph.add_edge(e, g, "Edge EG");
 
--- bk/src/categories/data-structures/graph.md
+++ bk/src/categories/data-structures/graph.md
@@ -9,7 +9,10 @@
 
 [![petgraph][c~petgraph~docs~badge]][c~petgraph~docs] [![petgraph~crates.io][c~petgraph~crates.io~badge]][c~petgraph~crates.io] [![petgraph~repo][c~petgraph~repo~badge]][c~petgraph~repo] [![petgraph~lib.rs][c~petgraph~lib.rs~badge]][c~petgraph~lib.rs]{{hi:petgraph}}{{hi:Graph}}{{hi:Unionfind}}{{hi:Graph algorithms}} [![cat~data-structures][cat~data-structures~badge]][cat~data-structures]{{hi:Data structures}}
 
-`petgraph` is a graph data structure library that provides graph types and graph algorithms.
+`petgraph` is a graph data structure library. `petgraph` provides several graph types (each differing in the tradeoffs taken in their internal representation), algorithms on those graphs, and functionality to output graphs in graphviz format. Both nodes and edges can have arbitrary associated data, and edges may be either directed or undirected.
+
+This example demonstrates the basic usage of the `petgraph` crate for creating and manipulating graphs in Rust.
+It covers creating a graph, adding nodes and edges, traversing the graph, and using different graph types:
 
 ```rust,editable,noplayground
 {{#include ../../../crates/cats/data_structures/examples/graph/petgraph.rs:example}}
@@ -20,4 +23,5 @@
 
 <div class="hidden">
 [write](https://github.com/john-cd/rust_howto/issues/1346)
+cover pathfinding ?
 </div>
