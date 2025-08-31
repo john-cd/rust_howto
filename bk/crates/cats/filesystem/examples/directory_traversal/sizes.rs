eb2e739bb70c48dc9ac6a431d813dfacf4ee15bd
John CD <john-cd@users.noreply.github.com>
2025-08-31 12:44:28 -0700

    minor formatting

--- bk/crates/cats/filesystem/examples/directory_traversal/sizes.rs
+++ bk/crates/cats/filesystem/examples/directory_traversal/sizes.rs
@@ -5,7 +5,8 @@ use walkdir::WalkDir;
 /// Calculates the total size of all files within a directory tree (within a
 /// given depth).
 fn main() {
-    // Create a new WalkDir iterator starting from the current directory (".").
+    // Create a new `WalkDir` iterator starting from the current directory
+    // (".").
     let total_size = WalkDir::new(".")
         .min_depth(1) // Set the minimum depth to 1 (exclude the root directory itself).
         .max_depth(3)
