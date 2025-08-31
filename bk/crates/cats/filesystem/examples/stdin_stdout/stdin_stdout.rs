1d9242c8df817be5d3675e707a49cf952bda2d00
John CD <john-cd@users.noreply.github.com>
2025-08-31 12:44:05 -0700

    write sdin_stdout.md and example

--- bk/crates/cats/filesystem/examples/stdin_stdout/stdin_stdout.rs
+++ bk/crates/cats/filesystem/examples/stdin_stdout/stdin_stdout.rs
@@ -1,6 +1,34 @@
 #![allow(dead_code)]
 // ANCHOR: example
-fn main() -> anyhow::Result<()> {
+use std::io;
+use std::io::Write;
+
+/// Retrieve user input from the standard input:
+fn get_and_print_input() -> Result<String, io::Error> {
+    println!("Please type your first name and press Enter:");
+    // Create a mutable `String` buffer:
+    let mut user_input = String::new();
+    // Get a handle to stdin and read a line into the buffer:
+    io::stdin().read_line(&mut user_input)?;
+    Ok(user_input)
+}
+
+fn main() -> io::Result<()> {
+    // Instead of using `print*!` to write to the standard output,
+    // you may get an explicit handle...
+    let stdout = io::stdout();
+    // ...and lock the handle if you want exclusive access:
+    let mut handle = stdout.lock();
+    // Write a byte slice to the locked handle:
+    handle.write_all(b"This is written to the standard output.\n")?;
+    // The lock is automatically released when `handle` goes out of scope.
+
+    // The same applies to the standard error of the current process:
+    let mut stderr = io::stderr();
+    // We could lock `stderr` as well.
+    stderr.write_all(b"There was an error.\n")?;
+    // This is equivalent to `eprintln!`.
+
     Ok(())
 }
 // ANCHOR_END: example
--- bk/src/categories/filesystem/stdin_stdout.md
+++ bk/src/categories/filesystem/stdin_stdout.md
@@ -1,10 +1,12 @@
-# Working with Standard Input/Output
+# Working with the Standard Input and Output
 
 {{#include stdin_stdout.incl.md}}
 
-## Work with the Standard Input and Standard Output {#stdin-stdout}
+## Read from the Standard Input and Write to the Standard Output or Standard Error {#stdin-stdout}
 
-Reading from stdin, writing to stdout/stderr, and handling command-line arguments.
+`std::io::stdin`, `stdout`, and `stderr` are handles to the standard input, standard output, and standard error streams of a process, respectively. They are your primary tools for interacting with a user via the command line. `stdin`, by default, is connected to the keyboard; `stdout` and `stderr` are connected to the terminal or console window.
+
+The following demonstrates how to work with them. You will rarely need to explicitly call `stdout` or `stderr` unless you need to lock them (to prevent output interleaving):
 
 ```rust,editable
 {{#include ../../../crates/cats/filesystem/examples/stdin_stdout/stdin_stdout.rs:example}}
@@ -14,5 +16,4 @@ Reading from stdin, writing to stdout/stderr, and handling command-line argument
 {{#include ../../refs/link-refs.md}}
 
 <div class="hidden">
-TODO write
 </div>
