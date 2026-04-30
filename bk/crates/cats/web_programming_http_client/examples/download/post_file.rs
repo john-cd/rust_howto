aee6e51c991b42768752e3184b3bab549c1dff61
john-cd <john-cd@users.noreply.github.com>
2026-04-29 19:59:43 -0700

    Update post_file.rs to comment out main function call
    Commented out the main function call and added a TODO.

--- bk/crates/cats/web_programming_http_client/examples/download/post_file.rs
+++ bk/crates/cats/web_programming_http_client/examples/download/post_file.rs
@@ -31,6 +31,7 @@ async fn test() -> anyhow::Result<()> {
     f.write_all(b"Hello").await?;
 
     // In a CI environment, we might want to skip tests that require network
-    // access. main().await?;
+    // access. 
+    // TODO main().await?;
     Ok(())
 }
